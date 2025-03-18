b078ee218e1fef0ae5d5c25558f3f79fb97117bc
bot <maximbotpy@gmail.com>
2025-03-18 21:08:36 +0300

    remove

--- back/src/app/auth/api_v1/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.app.auth.api_v1.models.auth import AuthTable
-from src.app.auth.api_v1.jwt.models.token import TokenTable
-
-__all__ = ["AuthTable", "TokenTable"]
--- back/src/app/auth/api_v1/config.py
+++
@@ -1,3 +0,0 @@
-from passlib.context import CryptContext
-
-myctx = CryptContext(schemes=["sha256_crypt", "des_crypt"], deprecated="auto")
