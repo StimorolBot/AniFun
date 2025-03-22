6ed082186cdc168a321d94e7729152ac384b0a39
bot <maximbotpy@gmail.com>
2025-03-22 21:50:20 +0300

    init

---
+++ front/src/components/ui/input/InputEmail.jsx
@@ -0,0 +1,21 @@
+import { InputMain } from "./InputMain"
+
+
+export function InputEmail({register, errors, clickRef, watch}){    
+    return(
+        <InputMain labelTitle={"Логин"} id={"email"} type={"email"} ref={clickRef} 
+            minLength={4} maxLength={30} watch={watch}
+            register={register("email", {
+                required: true,
+                minLength: {value: 4, message: "Длинна поля должна быть от 4 символов"},
+                maxLength: {value: 30, message: "Длинна поля должна быть до 30 символов"},
+                pattern: {value: /(^[a-zA-Z0-9_-]+@[mail|gmail|]+\.[ru|com]+)/, message: "Неверный формат почты"}
+            })} 
+            errorMsg={errors?.email?.message}
+        >
+            <svg className="auth__svg">
+                <use xlinkHref="/public/main.svg#login-svg"/>
+            </svg>
+        </InputMain>
+    )
+}
