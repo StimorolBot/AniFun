8edb3a0cd4122da4c850ce14676373df5f238e71
bot <maximbotpy@gmail.com>
2025-03-23 18:44:29 +0300

    remove: удалил get_user_by_token

--- back/src/utils/utils.py
+++ back/src/utils/utils.py
@@ -1,11 +1,7 @@
 import string
 import secrets
 from uuid import uuid4
-from typing import Annotated
 from datetime import datetime, UTC
-from fastapi import Cookie, HTTPException, status
-
-from src.app.auth.api_v1.jwt.jwt_token import jwt_token
 
 
 def generate_uuid() -> str:
@@ -20,9 +16,3 @@ def get_unc_now(f: str = "%d-%m-%Y, %H:%M:%S") -> datetime:
 def generate_code(code_len: int = 8) -> str:
     letters_and_digits = string.ascii_letters + string.digits + "_" + "-"
     return ''.join(secrets.choice(letters_and_digits) for _ in range(code_len))
-
-
-def get_user_by_token(access_token: Annotated[dict, Cookie()] = None) -> dict:
-    if not access_token:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Пользователь не авторизирован")
-    return jwt_token.decode(token=access_token)
