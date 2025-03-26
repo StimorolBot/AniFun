1397719f288ada51ac7c90b55defa672779a9c19
bot <maximbotpy@gmail.com>
2025-03-26 20:23:02 +0300

    init

---
+++ back/src/app/auth/social/api_v1/google/oauth.py
@@ -0,0 +1,16 @@
+from authlib.integrations.starlette_client import OAuth
+from .config import settings
+
+oauth = OAuth()
+oauth.register(
+    name="google",
+    client_id=settings.CLIENT_ID,
+    authorization_url=settings.AUTH_URI,
+    client_secret=settings.CLIENT_SECRET,
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    redirect_uri=settings.REDIRECT_URI,
+    client_kwargs={
+        "scope": "openid email profile",
+        "redirect_url": settings.REDIRECT_URI
+    }
+)
