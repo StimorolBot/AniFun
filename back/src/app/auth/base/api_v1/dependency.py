af5c4d520f8ab8bced3aeed221724a0e97dd3c1a
bot <maximbotpy@gmail.com>
2025-03-27 20:44:19 +0300

    init

---
+++ back/src/app/auth/base/api_v1/dependency.py
@@ -0,0 +1,10 @@
+from typing import Annotated
+from fastapi import Cookie, HTTPException, status, Depends
+
+from src.app.auth.base.api_v1.jwt.jwt_token import jwt_token
+
+
+def get_user_by_token(access_token: Annotated[str | None, Cookie()] = None) -> dict:
+    if not access_token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Пользователь не авторизирован")
+    return jwt_token.decode(token=access_token)
