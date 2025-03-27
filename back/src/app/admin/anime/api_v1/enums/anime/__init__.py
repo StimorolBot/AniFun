70c0b71be230a5f511f97e52be65cf93ed0a7008
bot <maximbotpy@gmail.com>
2025-03-27 21:17:10 +0300

    init

---
+++ back/src/app/admin/anime/api_v1/enums/anime/__init__.py
@@ -0,0 +1,7 @@
+from .season import Season
+from .type import Type
+from .genres import Genres
+from .age_restrictions import AgeRestrictions
+from .status import Status
+
+__all__ = ["Season", "Type", "Genres", "AgeRestrictions", "Status"]
