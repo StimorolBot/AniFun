402cd1cb0b25c129dd5799cb57e8480a5fe557f9
bot <maximbotpy@gmail.com>
2025-03-29 21:18:49 +0300

    refactor: изменил AUTH_URI, сделал его property

--- back/src/app/auth/social/api_v1/discord/config.py
+++ back/src/app/auth/social/api_v1/discord/config.py
@@ -9,9 +9,13 @@ class Settings(BaseSettings):
     CLIENT_SECRET: str
     TOKEN: str
 
-    AUTH_URI: str
     REDIRECT_URI: str
     API_ENDPOINT: str = "https://discord.com/api/v10"
+
+    @property
+    def GET_AUTH_URI(self) -> str:
+        return (f"https://discord.com/oauth2/authorize?client_id={self.CLIENT_ID}"
+                f"&response_type=code&redirect_uri={self.REDIRECT_URI}&scope=identify+email+connections")
 
     model_config = SettingsConfigDict(env_file=PATH)
 
