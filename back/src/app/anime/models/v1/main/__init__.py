14262954ff9807bbf73d776b27622c309b50e1c8
bot <maximbotpy@gmail.com>
2025-04-02 21:08:56 +0300

    refactor: перенес файл

---
+++ back/src/app/anime/models/v1/main/__init__.py
@@ -0,0 +1,5 @@
+from src.app.anime.models.v1.main.anime import AnimeTable
+from src.app.anime.models.v1.main.img import ImgTable
+from src.app.anime.models.v1.main.video import VideoTable
+
+__all__ = (AnimeTable, ImgTable, VideoTable)
