5119fa0c58e1ffbeedfd3763eac2ae70d29d6cdb
bot <maximbotpy@gmail.com>
2025-04-12 21:25:28 +0300

    refactor: перенес файлы

--- back/src/app/anime/models/v1/age_restrictions.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Mapped, mapped_column
-
-from src.model import Base
-
-
-class AgeRestrictionsTable(Base):
-    __tablename__ = "age_restrictions_table"
-
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
