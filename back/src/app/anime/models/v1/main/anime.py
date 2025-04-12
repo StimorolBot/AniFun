b5af65bc422721378c646cfdb0b67ea2922b4a97
bot <maximbotpy@gmail.com>
2025-04-12 21:39:41 +0300

    feat: добавил relationship

--- back/src/app/anime/models/v1/main/anime.py
+++ back/src/app/anime/models/v1/main/anime.py
@@ -8,17 +8,20 @@ from src.model import Base
 from src.utils.utils import generate_uuid
 
 if TYPE_CHECKING:
-    from src.app.anime.models.v1.sub.type import TypeTable
-    from src.app.anime.models.v1.sub.season import SeasonTable
-    from src.app.anime.models.v1.sub.status import StatusTable
-    from src.app.anime.models.v1.sub.age_restrictions import AgeRestrictTable
+    from src.app.anime.models.v1.sub import TypeTable
+    from src.app.anime.models.v1.sub import SeasonTable
+    from src.app.anime.models.v1.sub import StatusTable
+    from src.app.anime.models.v1.sub import AgeRestrictTable
+    from src.app.anime.models.v1.main.img import ImgTable
+    from src.app.anime.models.v1.main.genres_anime import GenresAnimeTable
+    from src.app.anime.models.v1.main.episode import EpisodeTable
 
 
 class AnimeTable(Base):
     __tablename__ = "anime_table"
 
-    uuid: Mapped[UUID] = mapped_column(primary_key=True, default=generate_uuid, unique=True, index=True)
-    title: Mapped[str] = mapped_column(unique=True)
+    uuid: Mapped[UUID] = mapped_column(primary_key=True, default=generate_uuid, unique=True)
+    title: Mapped[str] = mapped_column(unique=True, index=True)
     description: Mapped[str] = mapped_column()
     year: Mapped[int] = mapped_column()
     episodes: Mapped[int] = mapped_column()
@@ -32,3 +35,6 @@ class AnimeTable(Base):
     season_rs: Mapped["SeasonTable"] = relationship(back_populates="anime_rs")
     age_restrict_rs: Mapped["AgeRestrictTable"] = relationship(back_populates="anime_rs")
     status_rs: Mapped["StatusTable"] = relationship(back_populates="anime_rs")
+    img_rs: Mapped["ImgTable"] = relationship(back_populates="anime_rs")
+    genres_rs: Mapped["GenresAnimeTable"] = relationship(back_populates="anime_rs")
+    episode_rs: Mapped["EpisodeTable"] = relationship(back_populates="anime_rs")
