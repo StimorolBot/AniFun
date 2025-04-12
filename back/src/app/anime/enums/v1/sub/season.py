84c2a8bb4a5fbc5d8ffd10fbe250b09f3bb4ec7f
bot <maximbotpy@gmail.com>
2025-04-12 21:18:51 +0300

    refactor: перенес файлы

---
+++ back/src/app/anime/enums/v1/sub/season.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Season(Enum):
+    WINTER = "Зима"
+    SPRING = "Весна"
+    SUMMER = "Лето"
+    AUTUMN = "Осень"
---
+++ back/src/app/anime/enums/v1/sub/type.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Type(Enum):
+    TV = "ТВ сериал"
+    OVA = "OVA"
+    ONA = "ONA"
+    FILM = "Фильм"
+    SPECIAL = "Спешл"
