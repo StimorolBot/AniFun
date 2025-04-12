43b99827c985bdb7ea8ba137e1e9a9429336c6c3
bot <maximbotpy@gmail.com>
2025-04-12 21:23:16 +0300

    refactor: перенес файлы

---
+++ back/src/app/anime/enums/v1/sub/__init__.py
@@ -0,0 +1,7 @@
+from .season import Season
+from .type import Type
+from .genres import Genres
+from .restrict import Restrict
+from .status import Status
+
+__all__ = (Type, Restrict, Season, Status, Genres)
