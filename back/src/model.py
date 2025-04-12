4ab4125ef70304b75127ff72d9e480b475043abe
bot <maximbotpy@gmail.com>
2025-04-12 21:20:17 +0300

    feat: добавил метод для заполнения таблицы

--- back/src/model.py
+++ back/src/model.py
@@ -2,4 +2,7 @@ from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
-    ...
+
+    @classmethod
+    async def fill(cls, *args, **kwargs):
+        raise BaseException("[!] Метод не переопределен")
