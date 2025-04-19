371eef10dd2b40790a4617e5b3fffe9fc65213fd
bot <maximbotpy@gmail.com>
2025-04-19 21:19:40 +0300

    init

---
+++ back/src/app/anime/models/v1/main/episode.py
@@ -0,0 +1,24 @@
+from typing import TYPE_CHECKING
+from datetime import datetime
+
+from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.model import Base
+from src.utils.utils import get_unc_now
+
+if TYPE_CHECKING:
+    from src.app.anime.models.v1.main.anime import AnimeTable
+
+
+class EpisodeTable(Base):
+    __tablename__ = "episode_table"
+
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
+    title: Mapped[str] = mapped_column(ForeignKey("anime_table.title"), index=True)
+    episode: Mapped[int] = mapped_column()
+    date_add: Mapped[datetime] = mapped_column(default=get_unc_now)
+
+    anime_rs: Mapped["AnimeTable"] = relationship(back_populates="episode_rs")
+
+    __table_args__ = (UniqueConstraint("title", "episode"),)
