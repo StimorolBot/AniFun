94e69b95b6f962fe79a93a789f25e8bb849409c1
bot <maximbotpy@gmail.com>
2025-04-20 20:09:36 +0300

    refactor: перенес файл

--- back/src/app/auth/models/v1/__init__.py
+++
@@ -1,4 +0,0 @@
-from .auth import AuthTable
-from .auth_type import AuthTypeTable
-
-__all__ = ["AuthTable", "AuthTypeTable"]
---
+++ back/src/app/auth/models/v1/main/__init__.py
@@ -0,0 +1,4 @@
+from src.app.auth.models.v1.main.auth import AuthTable
+from src.app.auth.models.v1.main.token import TokenTable
+
+__all__ = (AuthTable, TokenTable)
