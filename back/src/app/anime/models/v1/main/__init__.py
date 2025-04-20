02d143e97a53e786039ef5293247335979cf3f53
bot <maximbotpy@gmail.com>
2025-04-20 20:04:00 +0300

    feat: таблицу ScheduleTable

--- back/src/app/anime/models/v1/main/__init__.py
+++ back/src/app/anime/models/v1/main/__init__.py
@@ -2,5 +2,6 @@ from src.app.anime.models.v1.main.anime import AnimeTable
 from src.app.anime.models.v1.main.img import ImgTable
 from src.app.anime.models.v1.main.episode import EpisodeTable
 from src.app.anime.models.v1.main.genres_anime import GenresAnimeTable
+from src.app.anime.models.v1.main.schedule import ScheduleTable
 
-__all__ = (AnimeTable, ImgTable, EpisodeTable, GenresAnimeTable)
+__all__ = (AnimeTable, ImgTable, EpisodeTable, GenresAnimeTable, ScheduleTable)
