e87612d636cfe0047837a689c3bec8c1e46ba0ae
bot <maximbotpy@gmail.com>
2025-04-20 20:03:46 +0300

    feat: добавил DayWeek

--- back/src/app/anime/enums/v1/sub/__init__.py
+++ back/src/app/anime/enums/v1/sub/__init__.py
@@ -3,5 +3,6 @@ from .type import Type
 from .genres import Genres
 from .restrict import Restrict
 from .status import Status
+from .day_week import DayWeek
 
-__all__ = (Type, Restrict, Season, Status, Genres)
+__all__ = (Type, Restrict, Season, Status, Genres, DayWeek)
