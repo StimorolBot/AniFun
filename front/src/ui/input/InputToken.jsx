fa2539b4ce85e3e717a504ca86aa5dd25e68d213
bot <maximbotpy@gmail.com>
2025-05-03 18:56:24 +0300

    refactor: перенес файл, увеличил длину поля с 6 до 8

---
+++ front/src/ui/input/InputToken.jsx
@@ -0,0 +1,21 @@
+import { InputMain } from "./InputMain"
+
+
+export function InputToken({register, errors, clickRef, watch, labelTitle="Токен"}){
+    
+    return(
+        <InputMain labelTitle={labelTitle} id={"email_token"} type={"text"} ref={clickRef} 
+            minLength={8} maxLength={8} watch={watch}
+            register={ register("email_token", {
+                required: true,
+                minLength: {value: 8, message: "Длинна поля должна быть 8 символов"},
+                maxLength: {value: 8, message: "Длинна поля должна быть 8 символов"},
+            })}
+            errorMsg={errors?.email_token?.message}
+        >
+            <svg className="auth__svg">
+                <use xlinkHref="/public/main.svg#token-svg"/>
+            </svg>
+        </InputMain>
+    )
+}
