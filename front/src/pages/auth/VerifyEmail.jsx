69b7771a07252cadb925ac663bc47b343facf3ac
bot <maximbotpy@gmail.com>
2025-05-03 20:34:23 +0300

    refactor: исправил импорты feat: добавил компонент TransitionLoader для анимации, добавил defaultValues для формы

--- front/src/pages/auth/VerifyEmail.jsx
+++ front/src/pages/auth/VerifyEmail.jsx
@@ -1,33 +1,41 @@
-import { useRef, useState } from "react"
+import { useRef } from "react"
 import { useForm } from "react-hook-form"
 import { useLocation, useNavigate } from "react-router-dom"
 
-import { api } from "/src/config/api"
-import { cookies } from "../../config/cookie"
-import { useFetch } from "../../components/hook/useFetch"
+import { api } from "../../api"
+import { cookies } from "../../cookie"
+import { useFetch } from "../../hook/useFetch"
 
-import { Error } from "../../components/ui/popup/Error"
-import { BtnAuth } from "../../components/ui/btn/BtnAuth"
-import { InputToken } from "../../components/ui/input/InputToken"
+
+import { BtnAuth } from "../../ui/btn/BtnAuth"
+import { InputToken } from "../../ui/input/InputToken"
 
 import { AuthBg } from "../../components/auth/AuthBg"
 import { AuthTitle } from "../../components/auth/AuthTitle"
 import { AuthWarning } from "../../components/auth/AuthWarning"
+import { TransitionLoader } from "../../transition/TransitionLoader"
+import { Error } from "../../ui/popup/Error"
 
 
 export function VerifyEmail(){
     const clickRef = useRef(false)
+    const transitionRef = useRef(null)
     const location = useLocation()
+    const token = location.state?.recaptcha_token
     const navigate = useNavigate()
-    const [errorData, setErrorData] = useState({"status_code": null, "detail": "", "is_hidden": true})
-    const {register, handleSubmit, watch, formState: {errors, isValid}, reset} = useForm({mode: "onChange"})
+    const {register, handleSubmit, watch, formState: {errors, isValid}, reset} = useForm({
+        mode: "onChange",
+        defaultValues:{
+            "email_token": ""
+        }
+    })
 
     const [request, isLoading, error] = useFetch(
         async (data, event) => {
             event.preventDefault()
             await api.post("/auth/verify-email", {
-                "recaptcha_token": location.state?.recaptcha_token,
-                "email_token": data.email_token
+                "recaptcha_token": token ? token : "", 
+                "identifier_token": data.email_token
             })
             .then((r) => {
                 cookies.set(
@@ -36,36 +44,30 @@ export function VerifyEmail(){
                 )
                 navigate("/")
             })
-            // .cath((e) => {
-            //     setErrorData({"status_code": e?.response.status,"detail": e?.response?.data?.detail, "is_hidden": false})
-            //     reset()
-            // }
-
-            // )
         }
     )
 
-    console.log(error)
-    
     return(
         <main className="auth">
             <AuthBg/>
             <div className="auth__container">
-                <AuthTitle title={"Подтверждение почты"}
-                    desc={<>
-                        Проверьте Вашу почту и найдите письмо с токеном подтверждения почты
-                        <br />
-                        Вы сможете авторизоваться в Вашу учетную запись только после подтверждения почты
-                </>}/>
-                <form className="auth__form" ref={clickRef} onSubmit={handleSubmit(request)}>
-                    <InputToken labelTitle={"Токен из письма"} register={register} errors={errors} clickRef={clickRef} watch={watch}/>
-                    <BtnAuth text={"Подтвердить почту"} isValid={isValid}/>
-                </form>
-                 <AuthWarning/>
+                <TransitionLoader transitionRef={transitionRef} isLoading={isLoading}>
+                    <div className="auth__inner transition-loader" ref={transitionRef}>
+                        <AuthTitle title={"Подтверждение почты"}
+                            desc={<>
+                                Проверьте Вашу почту и найдите письмо с токеном подтверждения почты
+                                <br />
+                                Вы сможете авторизоваться в Вашу учетную запись только после подтверждения почты
+                        </>}/>
+                        <form className="auth__form" ref={clickRef} onSubmit={handleSubmit(request)}>
+                            <InputToken labelTitle={"Токен из письма"} register={register} errors={errors} clickRef={clickRef} watch={watch}/>
+                            <BtnAuth text={"Подтвердить почту"} isValid={isValid}/>
+                        </form>
+                        <AuthWarning/>
+                    </div>
+                </TransitionLoader>
             </div>
-            <Error errorData={errorData} setErrorData={setErrorData}/>
+            <Error error={error} resetForm={reset}/>
         </main>
-
-      
     )
 }
