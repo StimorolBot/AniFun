bc70d74917d8fcafe3cd6752d5448878692ed080
bot <maximbotpy@gmail.com>
2025-05-03 18:55:42 +0300

    refactor: перенес файл

---
+++ front/src/ui/input/InputPassword.jsx
@@ -0,0 +1,25 @@
+import { InputMain } from "./InputMain"
+
+
+export function InputPassword({register, errors, clickRef, watch, id="password", labelTitle="Пароль"}){
+    
+    return(
+        <InputMain labelTitle={labelTitle} id={id} type={"password"} ref={clickRef} 
+            minLength={8} maxLength={32} watch={watch} 
+            register={ register(id, {
+                required: true,
+                minLength: {value: 8, message: "Длинна поля должна быть от 8 символов"},
+                maxLength: {value: 32, message: "Длинна поля должна быть до 32 символов"},
+            })}
+            errorMsg={errors?.[id]?.message}
+        >
+            <svg className="auth__svg">
+                {
+                    id === "password"
+                    ? <use xlinkHref="/public/main.svg#key_v3-svg"/>
+                    : <use xlinkHref="/public/main.svg#key-svg"/>
+                }
+            </svg>
+        </InputMain>
+    )
+}
