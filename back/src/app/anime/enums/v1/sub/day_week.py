41453402c1c75ddd1fc997e2999dd24ce33d15df
bot <maximbotpy@gmail.com>
2025-05-04 18:21:32 +0300

    feat: добавил enum с днями недели

---
+++ back/src/app/anime/enums/v1/sub/day_week.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class DayWeek(Enum):
+    MONDAY = "Понедельник"
+    TUESDAY = "Вторник"
+    WEDNESDAY = "Среда "
+    THURSDAY = "Четверг"
+    FRIDAY = "Пятница "
+    SATURDAY = "Суббота "
+    SUNDAY = "Воскресенье"
