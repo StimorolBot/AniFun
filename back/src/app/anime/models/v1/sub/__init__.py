64cb1c3570a172f1d60915433859b51e0ab0b839
bot <maximbotpy@gmail.com>
2025-05-08 17:00:08 +0300

    init

---
+++ back/src/app/anime/models/v1/sub/__init__.py
@@ -0,0 +1,10 @@
+from src.app.anime.models.v1.sub.type import TypeTable
+from src.app.anime.models.v1.sub.genres import GenresTable
+from src.app.anime.models.v1.sub.age_restrictions import AgeRestrictTable
+from src.app.anime.models.v1.sub.season import SeasonTable
+from src.app.anime.models.v1.sub.status import StatusTable
+
+# ?
+__all__ = (
+    TypeTable, AgeRestrictTable, SeasonTable, StatusTable, GenresTable,
+)
