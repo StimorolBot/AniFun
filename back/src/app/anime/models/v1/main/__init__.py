f68b4d4db17c567389c59d7dbbeac320820131d5
bot <maximbotpy@gmail.com>
2025-05-08 16:57:31 +0300

    feat: добавил RelationAnime

--- back/src/app/anime/models/v1/main/__init__.py
+++ back/src/app/anime/models/v1/main/__init__.py
@@ -3,5 +3,6 @@ from src.app.anime.models.v1.main.img import ImgTable
 from src.app.anime.models.v1.main.episode import EpisodeTable
 from src.app.anime.models.v1.main.genres_anime import GenresAnimeTable
 from src.app.anime.models.v1.main.schedule import ScheduleTable
+from src.app.anime.models.v1.main.relation_anime import RelationAnime
 
-__all__ = (AnimeTable, ImgTable, EpisodeTable, GenresAnimeTable, ScheduleTable)
+__all__ = (AnimeTable, ImgTable, EpisodeTable, GenresAnimeTable, ScheduleTable, RelationAnime)
