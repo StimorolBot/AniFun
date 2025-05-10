0cf6d631cf652004360203e5a9345e8907b0e6a5
bot <maximbotpy@gmail.com>
2025-05-10 16:37:28 +0300

    refactor: добавил ограничения для поля episode

--- back/src/app/anime/models/v1/main/episode.py
+++ back/src/app/anime/models/v1/main/episode.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING
 from datetime import datetime
 
-from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy import ForeignKey, UniqueConstraint, CheckConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.model import Base
@@ -14,9 +14,9 @@ if TYPE_CHECKING:
 class EpisodeTable(Base):
     __tablename__ = "episode_table"
 
-    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True, unique=True)
     title: Mapped[str] = mapped_column(ForeignKey("anime_table.title"), index=True)
-    episode: Mapped[int] = mapped_column()
+    episode: Mapped[int] = mapped_column(CheckConstraint("episode>=1"))
     date_add: Mapped[datetime] = mapped_column(default=get_unc_now)
 
     anime_rs: Mapped["AnimeTable"] = relationship(back_populates="episode_rs")
