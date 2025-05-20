0ddafdae2ab7b4fc2109f769e91728cc3bd3cd48
bot <maximbotpy@gmail.com>
2025-05-20 20:41:24 +0300

    feat: добавил анимацию при загрузке

--- front/src/components/swiper/SwiperCustom.jsx
+++ front/src/components/swiper/SwiperCustom.jsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
 import { Pagination, Navigation, Autoplay } from "swiper/modules"
+import { CSSTransition, SwitchTransition } from "react-transition-group"
 
 import { api } from "../../api"
 import { useFetch } from "../../hook/useFetch"
 
 import { SlideMain } from "./slide/SlideMain"
 import { Loader } from "../../components/loader/Loader"
-
 
 import "swiper/css"
 import "swiper/css/pagination"
@@ -16,6 +16,7 @@ import "./style/swiper_custom.sass"
 
 
 export function SwiperCustom() {
+  const transitionRef = useRef()
   const [response, setResponse] = useState([{
     "age_restrict": null, "description": null, "episodes": null, "img_rs": {"banner": null},
     "season": null, "title": null, "type": null, "uuid": null, "year": null
@@ -35,20 +36,33 @@ export function SwiperCustom() {
   
   return (
     <div className="container">
-      <Loader isLoading={isLoading}/>
-      <Swiper className="mySwiper" id="swiper-custom" slidesPerView={1} spaceBetween={30} loop={true}
-            pagination={{clickable: true}} navigation={true} modules={[Pagination, Navigation, Autoplay]}
-            autoplay={{delay: 3500, disableOnInteraction: false}}
-          >
-            {response?.map((slide, index) => {
-              return(
-                <SwiperSlide key={index}>
-                    <SlideMain slide={slide}/>
-                  </SwiperSlide>
-                )
-              })
+      <SwitchTransition mode="out-in">
+        <CSSTransition 
+          classNames="transition" 
+          key={isLoading}
+          nodeRef={transitionRef} 
+          timeout={300}
+        >
+          <div className="container-slider transition"  ref={transitionRef}>
+            {isLoading
+              ? <Loader/>
+              : <Swiper className="mySwiper" id="swiper-custom" slidesPerView={1} spaceBetween={30} loop={true}
+                pagination={{clickable: true}} navigation={true} modules={[Pagination, Navigation, Autoplay]}
+                autoplay={{delay: 3500, disableOnInteraction: false}} 
+              >
+                {response?.map((slide, index) => {
+                  return(
+                    <SwiperSlide key={index}>
+                      <SlideMain slide={slide}/>
+                    </SwiperSlide>
+                  )
+                })
+              }
+              </Swiper>
             }
-          </Swiper>
+          </div> 
+        </CSSTransition>
+      </SwitchTransition>
     </div>
   )
 }
