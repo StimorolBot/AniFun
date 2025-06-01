a68cd012e2fa0cb1a645a363c68ff2dda1c09b38
bot <maximbotpy@gmail.com>
2025-06-01 20:03:00 +0300

    remove: удалил CSSTransition

--- front/src/components/loader/Loader.jsx
+++ front/src/components/loader/Loader.jsx
@@ -1,16 +1,10 @@
 import { memo } from "react"
-import { useRef } from "react"
-import { CSSTransition } from "react-transition-group"
 
 import "./style/loader.sass"
 
 
-export const Loader = memo(({isLoading}) => {
-    const loaderRef = useRef()
-
+export const Loader = memo(() => {
     return(
-        <CSSTransition classNames="loader" nodeRef={loaderRef} in={isLoading} timeout={300} mountOnEnter unmountOnExit>
-            <span className="loader" ref={loaderRef}/>
-        </CSSTransition>
+        <span className="loader transition" />
     )
 })
