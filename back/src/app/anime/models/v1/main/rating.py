f25a591527fe53f0a0e47d9819c7fe209eaf6d67
bot <maximbotpy@gmail.com>
2025-06-01 20:01:10 +0300

    feat: добавил таблицу для оценки аниме

---
+++ back/src/app/anime/models/v1/main/rating.py
@@ -0,0 +1,18 @@
+from uuid import UUID
+
+from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.model import Base
+
+
+class RatingTable(Base):
+    """Таблица с аниме рейтингом"""
+    __tablename__ = "rating_table"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    uuid: Mapped[UUID] = mapped_column(ForeignKey("auth_table.uuid"))
+    title: Mapped[str] = mapped_column(ForeignKey("anime_table.title"))
+    star: Mapped[int] = mapped_column()
+
+    __table_args__ = (UniqueConstraint("uuid", "title", "star"),)
