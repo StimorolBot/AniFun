7ed81a20aceb41308fa88604501843cb36afc6f6
bot <maximbotpy@gmail.com>
2025-06-16 21:00:09 +0300

    feat: добавил компонент рутовой страницы с выбором действий

---
+++ front/src/pages/root/Root.jsx
@@ -0,0 +1,29 @@
+import { memo } from "react"
+import { Link } from "react-router-dom"
+
+import "./style/root.sass"
+
+
+export const Root = memo(() => {
+    return(
+        <main className="main main__r">
+            <h1 className="title-page">
+                Root page
+            </h1>
+            <nav className="root-nav">
+                <ul className="root__list">
+                    <li className="root__item">
+                        <Link to={"anime"}>
+                            Аниме
+                        </Link>
+                    </li>
+                    <li className="root__item">
+                        <Link to={"users"}>
+                            Пользователи
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </main>
+    )
+})
