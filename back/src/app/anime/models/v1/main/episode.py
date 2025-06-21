0dde28113386ede343ec09dc422ffe8f7221be32
bot <maximbotpy@gmail.com>
2025-06-21 18:09:15 +0300

    feat: добавил поля preview и schedule_rs

--- back/src/app/anime/models/v1/main/episode.py
+++ back/src/app/anime/models/v1/main/episode.py
@@ -1,24 +1,30 @@
+from uuid import UUID
 from typing import TYPE_CHECKING
 from datetime import datetime
 
-from sqlalchemy import ForeignKey, UniqueConstraint, CheckConstraint
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.model import Base
 from src.utils.utils import get_unc_now
 
+from src.utils.utils import generate_uuid
+
 if TYPE_CHECKING:
     from src.app.anime.models.v1.main.anime import AnimeTable
+    from src.app.anime.models.v1.main.schedule import ScheduleTable
 
 
 class EpisodeTable(Base):
+    """Таблица для добавления новых эпизодов аниме"""
+
     __tablename__ = "episode_table"
 
-    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True, unique=True)
+    uuid: Mapped[UUID] = mapped_column(default=generate_uuid, primary_key=True, unique=True, index=True)
     title: Mapped[str] = mapped_column(ForeignKey("anime_table.title"), index=True)
-    episode: Mapped[int] = mapped_column(CheckConstraint("episode>=1"))
+
     date_add: Mapped[datetime] = mapped_column(default=get_unc_now)
+    preview: Mapped[str] = mapped_column()
 
     anime_rs: Mapped["AnimeTable"] = relationship(back_populates="episode_rs")
-
-    __table_args__ = (UniqueConstraint("title", "episode"),)
+    schedule_rs: Mapped["ScheduleTable"] = relationship(back_populates="uuid_episode_rs")
