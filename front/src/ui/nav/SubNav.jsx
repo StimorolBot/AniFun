cfb2f25d471398a1bd30e7a5130f29da46ed6f33
bot <maximbotpy@gmail.com>
2025-06-25 21:51:49 +0300

    feat: добавил компонент с навигацией

---
+++ front/src/ui/nav/SubNav.jsx
@@ -0,0 +1,24 @@
+import { memo } from "react"
+import { Link } from "react-router-dom"
+
+import "./style/sub_nav.sass"
+
+
+export const SubNav = memo(({subNav}) => {
+
+    return(
+        <nav className="sub-nav">
+            <ul className="sub-nav__list">
+                { subNav.map((item) => {
+                    return(
+                        <li className="sub-nav__item" key={item["path"]}>
+                            <Link className="sub-nav__link" to={item["path"]}>
+                                {item["name"]}
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
+        </nav>
+    )
+})
