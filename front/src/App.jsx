b4a1dd19b1de551333ebad51cb1be2c325788ad3
bot <maximbotpy@gmail.com>
2025-10-06 19:36:58 +0300

    feat: добавил контекст для получения жанров

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react"
 import { router } from "./router"
 import { RouterProvider } from "react-router-dom"
 
+import { GenresContext } from "./context/GenresContext"
+
 
 export function App() {
+  const [genresContext, setGenresContext] = useState([])
+
   return (
-    <RouterProvider router={ router }/>
+    <GenresContext.Provider value={{genresContext, setGenresContext}}>
+      <RouterProvider router={ router }/>
+    </GenresContext.Provider>
   )
 }
