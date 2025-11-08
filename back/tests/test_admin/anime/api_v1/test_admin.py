7209182046ae5f2c5389f5ca11aec1d4e47fdda8
bot <maximbotpy@gmail.com>
2025-11-08 19:34:08 +0300

    feat: добавил тесты для обновления расписания, удаления слайдов, эпизодов refactor: исправил все остальные тесты

--- back/tests/test_admin/anime/api_v1/test_admin.py
+++ back/tests/test_admin/anime/api_v1/test_admin.py
@@ -1,12 +1,12 @@
 import os
-import pytest
 from io import BytesIO
 
+import aiofiles
+import pytest
 from fastapi import status
 from httpx import AsyncClient
 
 from tests.conftest import ac
-from src.app.admin.file_server import file_server
 
 # запуск тестов
 # pytest -vv -s tests/test_admin/anime/api_v1/test_admin.py::TestPos
@@ -17,38 +17,22 @@ absolute_path = os.path.abspath(relative_path)
 
 
 class TestPos:
-
-    @pytest.mark.parametrize(
-        "title, poster, is_origin",
-        [
-            ("title_1", "test_poster.webp", True),
-            ("title_2", "test.poster.webp", False)
-        ]
-    )
-    async def test_add_new_title(self, ac: AsyncClient, title: str, poster: str, is_origin: bool):
-        params = {
+    @pytest.mark.parametrize("title, is_origin, alias", [("title_1", True, "alias"), ("title_2", False, None)])
+    async def test_create_title(self, ac: AsyncClient, title: str, is_origin: bool, alias: str | None):
+        data = {
             "title": title,
             "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error nemo maxime laboriosam...!",
             "year": 2025,
+            "alias": alias,
             "type": "ТВ сериал",
-            "episodes": 12,
+            "total_episode": 12,
             "age_restrict": "12+",
             "status": "Онгоинг",
             "season": "Лето",
             "is_origin": is_origin,
+            "genres": ["Безумие", "Боевые искусства", "Вампиры", "Военное", "Гарем", "Драма"]
         }
-        genres = ["Безумие", "Боевые искусства", "Вампиры", "Военное", "Гарем", "Драма"]
-        file = await file_server.read(path=absolute_path, title="test_poster.webp")
-        file_io = BytesIO(file)
-        file_io.name = poster
-
-        response = await ac.post(
-            "/admin/anime/add-new-title",
-            params=params,
-            files={"img": file_io},
-            data={"genres": genres}
-        )
-
+        response = await ac.post("/admin/anime/create-title", json=data)
         assert response.status_code == status.HTTP_201_CREATED
 
     async def test_add_relation_title(self, ac: AsyncClient):
@@ -56,32 +40,49 @@ class TestPos:
         response = await ac.post("/admin/anime/add-relation-title", json=data)
         assert response.status_code == status.HTTP_201_CREATED
 
-    @pytest.mark.parametrize(
-        "title, video_name",
-        [
-            ("title_1", "test.mp4"),
-            ("title_2", "test.1.mp4")
-        ]
-    )
-    async def test_add_episode(self, ac: AsyncClient, title: str, video_name: str):
-        params = {"title": title, "episode": 1}
-        file = await file_server.read(path=absolute_path, title="test.mp4")
-        file_io = BytesIO(file)
-        file_io.name = video_name
-        response = await ac.post("/admin/anime/add-episode", params=params, files={"video": file_io})
+    async def test_add_poster(self, ac: AsyncClient):
+        params = {"title": "title_1"}
+        async with aiofiles.open(f"{absolute_path}/test.webp", "rb") as f:
+            img = await f.read()
+
+        img_io = BytesIO(img)
+        img_io.name = "test.webp"
+
+        response = await ac.post("/admin/anime/add-poster", params=params, files={"img": img_io})
+        assert response.status_code == status.HTTP_201_CREATED
+
+    @pytest.mark.parametrize("title", ["title_1", "title_2"])
+    async def test_add_episode(self, ac: AsyncClient, title: str):
+        params = {"title": title, "episode_number": 1}
+        async with aiofiles.open(f"{absolute_path}/test.mp4", "rb") as f:
+            video = await f.read()
+
+        video_io = BytesIO(video)
+        video_io.name = "test.mp4"
+
+        async with aiofiles.open(f"{absolute_path}/test.webp", "rb") as f:
+            img = await f.read()
+
+        img_io = BytesIO(img)
+        img_io.name = "test.webp"
+
+        response = await ac.post("/admin/anime/add-episode", params=params, files={"video": video_io, "img": img_io})
         assert response.status_code == status.HTTP_201_CREATED
 
     async def test_set_schedules(self, ac: AsyncClient):
         data = {
             "title": "title_1",
             "day_week": "Суббота",
-            "schedule_item": [
+            "item": [
                 {
-                    "day": 10,
-                    "month": 5,
-                    "year": 2025,
+                    "date": "2025-11-08",
                     "episode_number": 1,
                     "episode_name": "episode 1"
+                },
+                {
+                    "date": "2025-11-15",
+                    "episode_number": 2,
+                    "episode_name": "episode 2"
                 },
             ]
         }
@@ -89,20 +90,78 @@ class TestPos:
         assert response.status_code == status.HTTP_201_CREATED
 
     async def test_add_slide(self, ac: AsyncClient):
-        params = {"title": "title_1"}
-        file = await file_server.read(path=absolute_path, title="test_banner.webp")
-        file_io = BytesIO(file)
-        file_io.name = "test_banner.webp"
+        async with aiofiles.open(f"{absolute_path}/test.webp", "rb") as f:
+            img = await f.read()
 
-        response = await ac.patch("/admin/anime/add-slide", params=params, files={"img": file_io})
+        img_io = BytesIO(img)
+        img_io.name = "test.webp"
+
+        response = await ac.patch("/admin/anime/add-slide", params={"title": "title_1"}, files={"img": img_io})
         assert response.status_code == status.HTTP_200_OK
 
-    async def test_delite_slide(self, ac: AsyncClient):
-        response = await ac.patch("/admin/anime/delite-slide", params={"title": "title_1"})
+    async def test_update_title(self, ac: AsyncClient):
+        data = {
+            "title_prev": "title_2",
+            "title": "title_3",
+            "description": "new description test 123",
+            "age_restrict": "18+",
+            "genres": ["Безумие", "Боевые искусства", "Драма"]
+        }
+        response = await ac.patch("/admin/anime/update-title", json=data)
         assert response.status_code == status.HTTP_200_OK
 
-    async def test_delite_title(self, ac: AsyncClient):
-        response = await ac.delete("/admin/anime/delite-title", params={"title": "title_1"})
+    async def test_update_schedules(self, ac: AsyncClient):
+        data = {
+            "title": "title_1",
+            "day_week": "Суббота",
+            "item": [
+                {
+                    "date": "2025-11-08",
+                    "episode_number": 2,
+                    "episode_name": "episode 1",
+                    "update_fild": "episode_number"
+                },
+                {
+                    "date": "2025-11-15",
+                    "episode_number": 2,
+                    "episode_name": "episode 3",
+                    "update_fild": "episode_number"
+                },
+            ]
+        }
+        response = await ac.patch("/admin/anime/update-schedules", json=data)
+        assert response.status_code == status.HTTP_200_OK
+
+    async def test_delete_slide(self, ac: AsyncClient):
+        response = await ac.patch("/admin/anime/delete-slide", json={"title": "title_2"})
+        assert response.status_code == status.HTTP_200_OK
+
+    async def test_delete_title(self, ac: AsyncClient):
+        response = await ac.request("delete", "/admin/anime/delete-title", json={"title": "title_3"})
+        assert response.status_code == status.HTTP_200_OK
+
+    async def test_delete_relation_title(self, ac: AsyncClient):
+        data = {"title": "title_1", "relation_title": "title_2"}
+        response = await ac.request("delete", "/admin/anime/delete-relation-title", json=data)
+        assert response.status_code == status.HTTP_200_OK
+
+    async def test_delete_episode(self, ac: AsyncClient):
+        data = {
+            "title": "title_1",
+            "episode_list": [
+                {"episode_number": 1},
+                {"episode_number": 2},
+            ]
+        }
+        response = await ac.request("delete", "/admin/anime/delete-episode", json=data)
+        assert response.status_code == status.HTTP_200_OK
+
+    async def test_delete_schedules(self, ac: AsyncClient):
+        response = await ac.request("delete", "/admin/anime/delete-schedules", json={"title": "title_1"})
+        assert response.status_code == status.HTTP_200_OK
+
+    async def test_get_api_list(self, ac: AsyncClient):
+        response = await ac.get("/admin/anime/api-list")
         assert response.status_code == status.HTTP_200_OK
 
 
@@ -110,65 +169,47 @@ class TestPos:
 
 class TestNeg:
     @pytest.mark.parametrize(
-        "title, description, year, type_, episodes, age_restrict, status_, season, is_origin, genres",
+        "title, description, year",
         [  # bad title
             (
                     "Lorem, ipsum dolor sit amet." * 8,
                     "Lorem, ipsum dolor sit amet." * 8,
-                    2025, "ТВ сериал", 12, "16+", "Вышел", "Зима", True, ["Безумие"]
+                    2025
             ),
             (
                     "L$ore`",
                     "Lorem, ipsum dolor sit amet." * 8,
-                    2025, "ТВ сериал", 12, "16+", "Вышел", "Зима", True, ["Безумие"]
-            ),
-            # bad description
+                    2025
+            ),  # bad description
             (
                     "Lorem, ipsum dolor sit amet.",
                     "Lorem, ipsum dolor sit amet." * 38,
-                    2025, "ТВ сериал", 12, "16+", "Вышел", "Зима", True, ["Безумие"]
+                    2025
             ),
             (
                     "Lorem, ipsum dolor sit amet.",
                     "Lorem, ipsum dolor | sit amet.",
-                    2025, "ТВ сериал", 12, "16+", "Вышел", "Зима", True, ["Безумие"]
-            ),
-            # bad year
+                    2025
+            ),  # bad year
             (
                     "Lorem, ipsum dolor sit amet.",
                     "Lorem, ipsum dolor sit amet.",
-                    202, "ТВ сериал", 12, "16+", "Вышел", "Зима", True, ["Безумие"]
-            ),
+                    202
+            )
         ]
     )
-    async def test_add_new_title(
-            self, ac: AsyncClient,
-            title, description,
-            year, type_,
-            episodes, age_restrict,
-            status_, season,
-            is_origin, genres
-    ):
+    async def test_add_new_title(self, ac: AsyncClient, title: str, description: str, year: int):
         params = {
             "title": title,
             "description": description,
             "year": year,
-            "type": type_,
-            "episodes": 12,
-            "age_restrict": age_restrict,
-            "status": status_,
-            "season": season,
-            "is_origin": is_origin,
+            "type": "ТВ сериал",
+            "total_episode": 12,
+            "age_restrict": "16+",
+            "status": "Вышел",
+            "season": "Зима",
+            "is_origin": True,
+            "genres": ["Безумие"]
         }
-        file = await file_server.read(path=absolute_path, title="test_poster.webp")
-        file_io = BytesIO(file)
-        file_io.name = "test_poster.webp"
-
-        response = await ac.post(
-            "/admin/anime/add-new-title",
-            params=params,
-            files={"img": file_io},
-            data={"genres": genres}
-        )
-
+        response = await ac.post("/admin/anime/create-title", json=params)
         assert response.status_code == status.HTTP_400_BAD_REQUEST
