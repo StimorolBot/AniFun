87a9a98bf06744721fbe13159319bbe15a7ca869
bot <maximbotpy@gmail.com>
2025-11-09 12:58:33 +0300

    feat: добавил handler для обработки ошибок

---
+++ back/exception_handler.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
+from src.utils.logger import server_log
+
+
+def exception_handler(app: FastAPI):
+    @app.exception_handler(status.HTTP_404_NOT_FOUND)
+    def handler_page_not_found(request: Request, exp):
+        server_log.debug("Не удалось найти страницу %s", request.url)
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content="Не удалось найти страницу."
+        )
+
+    @app.exception_handler(RequestValidationError)
+    def handler_unprocessable_entity(request: Request, exc: RequestValidationError):
+        server_log.warning("Необрабатываемая сущность: %s", exc)
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content="Не удалось обработать запрос."
+        )
+
+    @app.exception_handler(status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def handler_server_error(request: Request, exp):
+        server_log.error("Ошибка сервера %s", exp)
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content="Сервер временно недоступен, пожалуйста, повторите попытку позже."
+        )
