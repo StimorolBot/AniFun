74b940e2a9b6f4b58e911d4b7fdbf603de7169b6
bot <maximbotpy@gmail.com>
2025-11-13 21:30:05 +0300

    feat: добавил проверку для paramName

--- front/src/ui/input/CustomSelect.jsx
+++ front/src/ui/input/CustomSelect.jsx
@@ -3,26 +3,34 @@ import Select from "react-select"
 import "./style/custom_select.sass"
 
 
-export function CustomSelect({value, setValue, options, ...props}){    
+export function CustomSelect({value, setValue, options, className="custom-select",  paramName=undefined, ...props}){    
     
     const getValue = () => {
         if (value){
-            return props?.isMulti 
-                ? options.filter(v => value.indexOf(v.value) >= 0) 
-                : options.find(v => v.value == value)
+            if (paramName && props?.isMulti )
+                return props?.isMulti 
+                    ? options?.filter(v => value[paramName]?.indexOf(v.value) >= 0) 
+                    : options?.find(v => v[paramName].value == value)
+            else
+                return props?.isMulti 
+                    ? options.filter(v => value?.indexOf(v.value) >= 0) 
+                    : options?.find(v => v.value == value)
         }
         else
             return props.isMulti ? [] : ""
     }
 
-    const onChange = (newValue) => {
-        setValue(props?.isMulti ?  newValue.map(v => v.value) : newValue)
+    const onChange = (newValue) =>{
+        if (paramName)
+            setValue(props?.isMulti ? s => ({...s, [paramName] : newValue.map(v => v.value)}) : s => ({...s, [paramName] : newValue.value}))
+        else
+            setValue(props?.isMulti ?  newValue.map(v => v.value) : newValue)
     }
 
     return(
         <Select
-            className="custom-select"
-            classNamePrefix="custom-select"
+            className={className}
+            classNamePrefix={className}
             value={getValue()} 
             onChange={onChange} 
             options={options}
