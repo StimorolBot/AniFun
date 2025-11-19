215cea58c827420504fcb923d0ae40fd86c0c233
bot <maximbotpy@gmail.com>
2025-11-19 20:34:48 +0300

    refactor: отсортировал импорты, переименовал роут, вернул 2 токена

--- back/src/app/auth/oauth/api_v1/router.py
+++ back/src/app/auth/oauth/api_v1/router.py
@@ -1,43 +1,39 @@
-import json
 import base64
+import json
 from binascii import Error as ErrorBinascii
 
 import httpx
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from fastapi import APIRouter, Request, status, HTTPException, Depends
 from authlib.integrations.base_client.errors import MismatchingStateError
+from fastapi import APIRouter, Depends, HTTPException, Request, status
+from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.responses import JSONResponse
-
-from src.app.auth.social.api_v1.google.oauth import oauth
-from src.app.auth.social.api_v1.google.config import settings as google_settings
-from src.app.auth.social.api_v1.discord.config import settings as discord_settings
-
-from src.utils.crud import crud
-from src.utils.logger import auth_log
-
-from src.database.session import get_async_session
-from src.app.auth.utils.v1.create_user import create_user
 
 from src.app.auth.base.api_v1.jwt.jwt_token import jwt_token
 from src.app.auth.base.api_v1.jwt.token_type import TokenType
-
 from src.app.auth.enums.v1.auth_type import AuthType
 from src.app.auth.models.v1.main.auth import AuthTable
+from src.app.auth.models.v1.main.token import TokenTable
+from src.app.auth.oauth.api_v1.discord.config import \
+    settings as discord_settings
+from src.app.auth.oauth.api_v1.google.config import settings as google_settings
+from src.app.auth.oauth.api_v1.google.oauth import oauth
+from src.app.auth.oauth.api_v1.telegram.schemas import OauthTelegram
+from src.app.auth.utils.v1.create_user import create_user
+from src.database.session import get_async_session
+from src.utils.crud import crud
+from src.utils.logger import auth_log
 
-from src.app.auth.social.api_v1.telegram.schemas import OauthTelegram
-
-auth_social_router = APIRouter(prefix="/auth/social", tags=["auth-social"])
+oauth_router = APIRouter(prefix="/auth/oauth2", tags=["auth-oauth2"])
 
 
-@auth_social_router.get("/login-google", status_code=status.HTTP_200_OK, summary="Вход через google")
-async def login_google(request: Request) -> str:
+@oauth_router.get("/uri-google", status_code=status.HTTP_200_OK, summary="Получить google uri")
+async def get_redirect_uri_google(request: Request) -> str:
     redirect_uri = google_settings.REDIRECT_URI
     uri = await oauth.google.authorize_redirect(request, redirect_uri)
     return uri._headers["location"]
 
 
-@auth_social_router.get("/auth-google", summary="Авторизация через google")
+@oauth_router.get("/auth-google", summary="Авторизация через google")
 async def auth_google(request: Request, session: AsyncSession = Depends(get_async_session)):
     try:
         token = await oauth.google.authorize_access_token(request)
@@ -50,7 +46,7 @@ async def auth_google(request: Request, session: AsyncSession = Depends(get_asyn
         )
 
         if not data_table:
-            access_token = await create_user(
+            tokens = await create_user(
                 session=session,
                 data={
                     "user_name": user_data["given_name"], "identifier": user_data["email"],
@@ -58,12 +54,19 @@ async def auth_google(request: Request, session: AsyncSession = Depends(get_asyn
                 }
             )
             await session.commit()
-            return JSONResponse(status_code=status.HTTP_200_OK, content=access_token)
+            return JSONResponse(status_code=status.HTTP_200_OK, content=tokens)
+
+        tokens = jwt_token.create_tokens(sub=data_table.uuid, auth_type=AuthType.GOOGLE.value)
 
         await crud.update(session=session, table=AuthTable, data={"is_active": True}, identifier=data_table.identifier)
-        access_token = jwt_token.create(token_type=TokenType.ACCESS.value, token_data={"sub": data_table.uuid})
+        await crud.update(
+            session=session,
+            table=TokenTable,
+            data={"refresh_token": tokens["refresh_token"]},
+            uuid=data_table.uuid
+        )
 
-        return JSONResponse(status_code=status.HTTP_200_OK, content={"access_token": access_token})
+        return JSONResponse(status_code=status.HTTP_200_OK, content=tokens)
     except MismatchingStateError as e:
         auth_log.warning("При попытке входа в учетную запись возникла ошибка: %s", e)
         raise HTTPException(
@@ -72,7 +75,12 @@ async def auth_google(request: Request, session: AsyncSession = Depends(get_asyn
         )
 
 
-@auth_social_router.get("/auth-discord", summary="Авторизация через discord")
+@oauth_router.get("/uri-discord", status_code=status.HTTP_200_OK, summary="Получить discord uri")
+async def get_redirect_uri_discord():
+    return discord_settings.GET_AUTH_URI
+
+
+@oauth_router.get("/auth-discord", summary="Авторизация через discord")
 async def auth_discord(code: str, session: AsyncSession = Depends(get_async_session)):
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
     data = {
@@ -125,7 +133,7 @@ async def auth_discord(code: str, session: AsyncSession = Depends(get_async_sess
         return JSONResponse(status_code=status.HTTP_200_OK, content={"access_token": access_token})
 
 
-@auth_social_router.post("/auth-telegram", summary="Авторизация через telegram")
+@oauth_router.post("/auth-telegram", summary="Авторизация через telegram")
 async def auth_telegram(data: OauthTelegram, session: AsyncSession = Depends(get_async_session)):
     try:
         base64_string_data = base64.b64decode(data.data_bas64 + '=' * (-len(data.data_bas64) % 4)).decode()
