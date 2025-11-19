51e8fbd8b9906e0e24aa2b1ea390867ceaf662e1
bot <maximbotpy@gmail.com>
2025-11-19 20:28:53 +0300

    feat: добавил Helement и AlertResponse

--- front/src/pages/auth/ResetPassword.jsx
+++ front/src/pages/auth/ResetPassword.jsx
@@ -1,6 +1,9 @@
-import { useRef } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { useRef, useState } from "react"
+
+import { Helmet } from "react-helmet"
 import { useForm } from "react-hook-form"
+import { Link, useNavigate } from "react-router-dom"
+import { CSSTransition, SwitchTransition } from "react-transition-group"
 
 import { api } from "../../api"
 import { useFetch } from "../../hook/useFetch"
@@ -11,12 +14,13 @@ import { BtnAuth } from "../../ui/btn/BtnAuth"
 
 import { InputToken } from "../../ui/input/InputToken"
 import { InputPassword } from "../../ui/input/InputPassword"
-import { TransitionLoader } from "../../transition/TransitionLoader"
-import { Error } from "../../ui/alert/Error"
+import { AlertResponse } from "../../ui/alert/AlertResponse"
+import { Loader } from "../../components/loader/Loader"
 
 
 export function ResetPassword(){
     const navigate = useNavigate()
+    const [updateAlert, setUpdateAlert] = useState()
     const clickRef = useRef(false)
     const transitionRef = useRef(null)
     const {register, handleSubmit, watch, formState: {errors, isValid}, reset} = useForm({
@@ -38,44 +42,70 @@ export function ResetPassword(){
         }
     )
 
-    return(
+    return(<>
+        <Helmet>
+            <title>Сброс пароля</title>
+        </Helmet>
         <main className="auth">
             <AuthBg/>
+            <h1 className="title-page">
+                Сброс пароля
+            </h1>
             <div className="auth__container">
-                <TransitionLoader transitionRef={transitionRef} isLoading={isLoading}>
-                    <div className="auth__inner transition-loader" ref={transitionRef}>
-                        <AuthTitle title={"Новый пароль"}
-                            desc={<>
-                                Вы можете создать новый пароль для Вашей учетной записи
-                                <br />
-                                Проверьте Вашу почту и найдите письмо с токеном восстановления пароля
-                            </>}
-                        />
-                        <form className="auth__form" ref={clickRef} onSubmit={handleSubmit(request)}>
-                            <InputToken register={register} errors={errors} clickRef={clickRef} watch={watch}/>
-                            <InputPassword register={register} errors={errors} clickRef={clickRef} watch={watch}/>
-                            <InputPassword 
-                                id={"password_confirm"} labelTitle={"Повторите пароль"} register={register} 
-                                errors={errors} clickRef={clickRef} watch={watch}
+                <SwitchTransition mode="out-in">
+                    <CSSTransition 
+                        classNames="transition" 
+                        key={isLoading}
+                        nodeRef={transitionRef} 
+                        timeout={300}
+                    >
+                    {isLoading
+                        ? <Loader/>
+                        : <div className="auth__inner transition-loader" ref={transitionRef}>
+                            <AuthTitle title={"Новый пароль"}
+                                desc={<>
+                                    Вы можете создать новый пароль для Вашей учетной записи
+                                    <br />
+                                    Проверьте Вашу почту и найдите письмо с токеном восстановления пароля
+                                </>}
                             />
-                            <BtnAuth text={"Сохранить новый пароль"} isValid={isValid}/>
-                        </form>
-                        <ul className="auth__link-list">
-                            <li>
-                                <Link className="auth__link" to={"/auth/login"}>
-                                    Авторизация
-                                </Link>
-                            </li>
-                            <li>
-                                <Link className="auth__link" to={"/auth/reset-password-token"}>
-                                    Повторно выслать письмо с токеном
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </TransitionLoader>
+                            <form className="auth__form" ref={clickRef} onSubmit={handleSubmit(request)}>
+                                <InputToken register={register} errors={errors} clickRef={clickRef} watch={watch}/>
+                                <InputPassword register={register} errors={errors} clickRef={clickRef} watch={watch}/>
+                                <InputPassword 
+                                    id={"password_confirm"} labelTitle={"Повторите пароль"} register={register} 
+                                    errors={errors} clickRef={clickRef} watch={watch}
+                                />
+                                <BtnAuth isValid={isValid} callback={() => setUpdateAlert(Date.now())}>
+                                    Сохранить новый пароль
+                                </BtnAuth>
+                            </form>
+                            <ul className="auth__link-list">
+                                <li>
+                                    <Link className="auth__link" to={"/auth/login"}>
+                                        Авторизация
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link className="auth__link" to={"/auth/reset-password-token"}>
+                                        Повторно выслать письмо с токеном
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    }
+                    </CSSTransition>
+                </SwitchTransition>
             </div>
-            <Error error={error} resetForm={reset}/>
-        </main>  
-    )
+            {isLoading === false &&
+                <AlertResponse
+                    msg={error?.response?.data} 
+                    statusCode={error?.status} 
+                    update={updateAlert}
+                    setResponse={reset}
+                    prefix={error?.response && "error"}
+                />
+             }
+        </main>
+    </>)
 }
--- front/src/pages/auth/ResetPasswordToken.jsx
+++ front/src/pages/auth/ResetPasswordToken.jsx
@@ -1,6 +1,9 @@
-import { useRef } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { useRef, useState } from "react"
+
+import { Helmet } from "react-helmet"
 import { useForm } from "react-hook-form"
+import { Link, useNavigate } from "react-router-dom"
+import { CSSTransition, SwitchTransition } from "react-transition-group"
 
 import { api } from "../../api"
 import { useFetch } from "../../hook/useFetch"
@@ -8,18 +11,19 @@ import { useFetch } from "../../hook/useFetch"
 import { BtnAuth } from "../../ui/btn/BtnAuth"
 import { AuthBg } from "../../components/auth/AuthBg"
 import { AuthTitle } from "../../components/auth/AuthTitle"
-import { AuthSocial } from "../../components/auth/AuthSocial"
 import { AuthWarning } from "../../components/auth/AuthWarning"
 
-import { Error } from "../../ui/alert/Error"
 import { InputEmail } from "../../ui/input/InputEmail"
-import { TransitionLoader } from "../../transition/TransitionLoader"
+import { AlertResponse } from "../../ui/alert/AlertResponse"
+import { OAuth } from "../../components/auth/OAuth"
+import { Loader } from "../../components/loader/Loader"
 
 
 export function ResetPasswordToken(){
     const clickRef = useRef(null)
-    const navigate = useNavigate()
     const transitionRef = useRef(null)
+    const navigate = useNavigate()
+    const [updateAlert, setUpdateAlert] = useState()
     const {register, handleSubmit, watch, formState: {errors, isValid}, reset} = useForm({
         mode: "onChange",
         defaultValues:{
@@ -38,36 +42,64 @@ export function ResetPasswordToken(){
     )
     
     return(
+        <>
+        <Helmet>
+            <title>Восстановление пароля</title>
+        </Helmet>
         <main className="auth">
             <AuthBg/>
+            <h1 className="title-page">
+                Восстановление пароля
+            </h1>
             <div className="auth__container">
-                <TransitionLoader transitionRef={transitionRef} isLoading={isLoading}>
-                    <div className="auth__inner transition-loader" ref={transitionRef}>
-                        <AuthTitle title={"Восстановление пароля"} 
-                            desc={<>
-                                Вы можете восстановить забытый пароль и сгенировать новый.
-                                <br/>
-                                Введите Вашу почту, на которую вы зарегистрировали аккаунт, мы пришлем 
-                                Вам письмо с дальнейшими инструкциями.
-                            </>}
-                        />
-                        <form className="auth__form" ref={clickRef} onSubmit={handleSubmit(request)}>
-                            <InputEmail register={register} errors={errors} clickRef={clickRef} watch={watch}/>
-                            <BtnAuth text={"Восстановить пароль"} isValid={isValid}/>
-                        </form>
-                        <AuthSocial/>
-                        <ul className="auth__link-list">
-                            <li>
-                                <Link className="auth__link" to={"/auth/reset-password"}>
-                                    У меня уже есть токен
-                                </Link>
-                            </li>
-                        </ul>
-                        <AuthWarning/>
-                    </div>
-                </TransitionLoader>          
+                <SwitchTransition mode="out-in">
+                    <CSSTransition 
+                        classNames="transition" 
+                        key={isLoading}
+                        nodeRef={transitionRef} 
+                        timeout={300}
+                    >
+                    {isLoading
+                        ? <Loader/>
+                        : <div className="auth__inner transition-loader" ref={transitionRef}>
+                            <AuthTitle title={"Восстановление пароля"} 
+                                desc={<>
+                                    Вы можете восстановить забытый пароль и сгенировать новый.
+                                    <br/>
+                                    Введите Вашу почту, на которую вы зарегистрировали аккаунт, мы пришлем 
+                                    Вам письмо с дальнейшими инструкциями.
+                                </>}
+                            />
+                            <form className="auth__form" ref={clickRef} onSubmit={handleSubmit(request)}>
+                                <InputEmail register={register} errors={errors} clickRef={clickRef} watch={watch}/>
+                                <BtnAuth isValid={isValid} callback={() => setUpdateAlert(Date.now())}>
+                                    Восстановить пароль
+                                </BtnAuth>
+                            </form>
+                            <OAuth/>                    
+                            <ul className="auth__link-list">
+                                <li>
+                                    <Link className="auth__link" to={"/auth/reset-password"}>
+                                        У меня уже есть токен
+                                    </Link>
+                                </li>
+                            </ul>
+                            <AuthWarning/>
+                        </div>
+                    }
+                    </CSSTransition>
+                </SwitchTransition>
             </div>
-            <Error error={error} resetForm={reset}/>
+            {isLoading === false &&
+                <AlertResponse
+                    msg={error?.response?.data} 
+                    statusCode={error?.status} 
+                    update={updateAlert}
+                    setResponse={reset}
+                    prefix={error?.response && "error"}
+                />
+            }
         </main>
+        </>
     )
 }
