489e39dd2dd5e94fbbd5f3055babb7828fea6d92
bot <maximbotpy@gmail.com>
2025-11-23 20:47:33 +0300

    feat: Loader, и <p>, если нет эпизодов

--- front/src/components/section/TitleVideo.jsx
+++ front/src/components/section/TitleVideo.jsx
@@ -4,64 +4,67 @@ import { api } from "../../api"
 import { useFetch } from "../../hook/useFetch"
 
 import { TitleVideoItem } from "../cards/TitleVideoItem"
-import { BtnFilterDate } from "../../ui/btn/BtnFilterDate"
 import { ProgressEpisode } from "../../ui/progressbar/ProgressEpisode"
-
 import { InputSearchTitle } from "../../ui/input/InputSearchTitle"
+import { Loader } from "../loader/Loader"
 
 import "./style/title_video.sass"
 
 
-export const TitleVideo = ({title, lastEpisode}) => {
+export const TitleVideo = ({title, lastEpisode, currentSlide}) => {
     const [titleSearch, setTitleSearch] = useState("")
     const [response, setResponse] = useState([{
-        "schedule":{
-            "date": null,
-            "day_week": null,
-            "episode_name": null,
-            "episode_number": null,
-            "uuid_episode": null
-        },
-        "preview": null,
+        "uuid": "",
+        "date_add": "",
+        "episode_number": "",
+        "episode_name": "",
+        "preview": ""
     }])
 
-
     const [request, isLoading, error] = useFetch(
-        async (title) => {
-            await api.get(`anime/releases/release/${title}/episodes/videos`, {params:{"title": title}})
+        async () => {
+            await api.get(`anime/releases/release/${title}/episodes/released`, {params:{"title": title}})
             .then((r) => {setResponse(r.data)})
-            }
-        )
+        }
+    )
 
     useEffect(() => {(
         async () => {
-            await request(title)
+            if (currentSlide.request === "episode")
+                await request(title)
         })()
-    }, [title])
+    }, [currentSlide.request, title])
     
     return(
-        <section className="title-video">
-            <div className="title-search__container">
-                <InputSearchTitle
-                    value={titleSearch} 
-                    setValue={setTitleSearch} 
-                    placeholder={"Введите название или номер серии"}
-                />
-                <BtnFilterDate/>               
-            </div>
-            <ProgressEpisode lastEpisode={lastEpisode}/>
-            <ul className="title-video__list">
-                {
-                    titleSearch === ""
-                    ? response.map((item, index) => {
-                        return <TitleVideoItem item={item} key={index}/> 
-                    })
-                    :response.filter(f => (f.schedule.episode_name.includes(titleSearch)) || (f.schedule.episode_number == titleSearch))
-                    .map((item, index) => {
-                        return <TitleVideoItem item={item} key={index}/> 
-                    })
-                }
-            </ul>
+        <section className="title-release__slide" style={{"marginLeft": currentSlide.marginLeft}}>
+            {isLoading
+                ? <Loader/>
+                : response.length > 1
+                    ?<>
+                        <div className="title-release__container">
+                            <InputSearchTitle
+                                value={titleSearch} 
+                                setValue={setTitleSearch} 
+                                placeholder={"Введите название или номер серии"}
+                            />
+                        </div>
+                        <ProgressEpisode lastEpisode={lastEpisode}/>
+                        <ul className="title-video__list">
+                            {titleSearch === ""
+                                ? response.map((item, index) => {
+                                    return <TitleVideoItem item={item} key={index}/> 
+                                })
+                                : response.filter(f => (f.episode_name?.includes(titleSearch)) || (f.episode_number == titleSearch))
+                                    .map((item, index) => {
+                                        return <TitleVideoItem item={item} key={index}/> 
+                                })
+                            }   
+                        </ul>
+                    </>
+                    :<p className="title-release__empty-continuation">
+                        В скором времени мы добавим новые эпизоды для "{title}".
+                    </p>
+            }
         </section>
     )
 }
