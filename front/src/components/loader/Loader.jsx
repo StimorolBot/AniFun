ecb9e61d6d6d2bf1e0a11c8c44ecb3005456361e
bot <maximbotpy@gmail.com>
2025-11-24 20:59:16 +0300

    feat: добавил data для размера лодера

--- front/src/components/loader/Loader.jsx
+++ front/src/components/loader/Loader.jsx
@@ -3,8 +3,8 @@ import { memo } from "react"
 import "./style/loader.sass"
 
 
-export const Loader = memo(() => {
+export const Loader = memo(({size="default"}) => {
     return(
-        <span className="loader transition" />
+        <span className="loader transition" data-size-loader={size}/>
     )
 })
