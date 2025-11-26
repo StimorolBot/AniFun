d5800508c83876eb1af6d804b4686580f684287c
bot <maximbotpy@gmail.com>
2025-11-26 22:29:20 +0300

    feat: добавил колонку с date_add и колонкам title, relation_title onupdate="CASCADE", ondelete="CASCADE"

--- back/src/app/anime/models/v1/main/relation_anime.py
+++ back/src/app/anime/models/v1/main/relation_anime.py
@@ -1,14 +1,22 @@
-from sqlalchemy import ForeignKey, UniqueConstraint
+from datetime import datetime
+
+from sqlalchemy import ForeignKey, UniqueConstraint, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.model import Base
+from src.utils.utils import get_unc_now
 
 
 class RelationAnime(Base):
     __tablename__ = "relation_anime"
 
     id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
-    title: Mapped[str] = mapped_column(ForeignKey("anime_table.title"))
-    relation_title: Mapped[str] = mapped_column(ForeignKey("anime_table.title"))
+    title: Mapped[str] = mapped_column(
+        ForeignKey("anime_table.title", onupdate="CASCADE", ondelete="CASCADE")
+    )
+    date_add: Mapped[datetime] = mapped_column(default=get_unc_now, server_default=func.now())
+    relation_title: Mapped[str] = mapped_column(
+        ForeignKey("anime_table.title", onupdate="CASCADE", ondelete="CASCADE")
+    )
 
     __table_args__ = (UniqueConstraint("title", "relation_title"),)
