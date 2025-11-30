b8b0e74086b30a5223a1e304a2665523075b5f67
bot <maximbotpy@gmail.com>
2025-11-30 21:22:34 +0300

    отсортировал импорты

--- back/src/app/anime/models/v1/main/__init__.py
+++ back/src/app/anime/models/v1/main/__init__.py
@@ -1,9 +1,17 @@
 from src.app.anime.models.v1.main.anime import AnimeTable
-from src.app.anime.models.v1.main.img import ImgTable
+from src.app.anime.models.v1.main.comment import (CommentTable,
+                                                  ResponseCommentTable)
 from src.app.anime.models.v1.main.episode import EpisodeTable
 from src.app.anime.models.v1.main.genres_anime import GenresAnimeTable
-from src.app.anime.models.v1.main.schedule import ScheduleTable
-from src.app.anime.models.v1.main.relation_anime import RelationAnime
+from src.app.anime.models.v1.main.img import ImgTable
 from src.app.anime.models.v1.main.rating import RatingTable
+from src.app.anime.models.v1.main.relation_anime import RelationAnime
+from src.app.anime.models.v1.main.schedule import ScheduleTable
 
-__all__ = (AnimeTable, ImgTable, EpisodeTable, GenresAnimeTable, ScheduleTable, RelationAnime, RatingTable)
+__all__ = (
+    AnimeTable, ImgTable,
+    EpisodeTable, GenresAnimeTable,
+    ScheduleTable, RelationAnime,
+    RatingTable, CommentTable,
+    ResponseCommentTable
+)
