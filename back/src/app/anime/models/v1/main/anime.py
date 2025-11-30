b324eeff0add5d4e7984c1eb1f9978646b2e7650
bot <maximbotpy@gmail.com>
2025-11-30 21:24:10 +0300

    feat: добавил total_episode, date_add, server_default для date_add и is_origin

--- back/src/app/anime/models/v1/main/anime.py
+++ back/src/app/anime/models/v1/main/anime.py
@@ -1,21 +1,19 @@
+from datetime import datetime
+from typing import TYPE_CHECKING, List
 from uuid import UUID
-from typing import List
-from typing import TYPE_CHECKING
 
-from sqlalchemy import ForeignKey, CheckConstraint
+from sqlalchemy import CheckConstraint, ForeignKey, func, true
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.model import Base
-from src.utils.utils import generate_uuid
+from src.utils.utils import generate_uuid, get_unc_now
 
 if TYPE_CHECKING:
-    from src.app.anime.models.v1.sub import TypeTable
-    from src.app.anime.models.v1.sub import SeasonTable
-    from src.app.anime.models.v1.sub import StatusTable
-    from src.app.anime.models.v1.sub import AgeRestrictTable
-    from src.app.anime.models.v1.main.img import ImgTable
-    from src.app.anime.models.v1.main.genres_anime import GenresAnimeTable
     from src.app.anime.models.v1.main.episode import EpisodeTable
+    from src.app.anime.models.v1.main.genres_anime import GenresAnimeTable
+    from src.app.anime.models.v1.main.img import ImgTable
+    from src.app.anime.models.v1.sub import (AgeRestrictTable, SeasonTable,
+                                             StatusTable, TypeTable)
 
 
 class AnimeTable(Base):
@@ -23,11 +21,13 @@ class AnimeTable(Base):
 
     uuid: Mapped[UUID] = mapped_column(primary_key=True, default=generate_uuid, unique=True)
     title: Mapped[str] = mapped_column(unique=True, index=True)
-    alias: Mapped[str] = mapped_column()
+    alias: Mapped[str] = mapped_column(unique=True)
     year: Mapped[int] = mapped_column(CheckConstraint("year>1970"))
     description: Mapped[str] = mapped_column()
+    total_episode: Mapped[int] = mapped_column()
 
-    is_origin: Mapped[bool] = mapped_column(default=True)
+    date_add: Mapped[datetime] = mapped_column(default=get_unc_now, server_default=func.now())
+    is_origin: Mapped[bool] = mapped_column(default=True, server_default=true())
 
     type: Mapped[str] = mapped_column(ForeignKey("type_table.type"))
     season: Mapped[str] = mapped_column(ForeignKey("season_table.season"))
@@ -38,6 +38,9 @@ class AnimeTable(Base):
     season_rs: Mapped["SeasonTable"] = relationship(back_populates="anime_rs")
     age_restrict_rs: Mapped["AgeRestrictTable"] = relationship(back_populates="anime_rs")
     status_rs: Mapped["StatusTable"] = relationship(back_populates="anime_rs")
-    img_rs: Mapped["ImgTable"] = relationship(back_populates="anime_rs")
-    genres_rs: Mapped[List["GenresAnimeTable"]] = relationship(back_populates="anime_rs")
+    img_rs: Mapped["ImgTable"] = relationship(back_populates="anime_rs", cascade="all, delete-orphan")
+    genres_rs: Mapped[List["GenresAnimeTable"]] = relationship(
+        back_populates="anime_rs",
+        foreign_keys="[GenresAnimeTable.title]"
+    )
     episode_rs: Mapped["EpisodeTable"] = relationship(back_populates="anime_rs")
