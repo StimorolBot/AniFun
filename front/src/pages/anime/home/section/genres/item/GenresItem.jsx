2272113645bc2c3c95277b568ce4485d189ab709
bot <maximbotpy@gmail.com>
2025-12-07 14:23:33 +0300

    refactor: перенес файл

--- front/src/pages/anime/home/section/genres/item/GenresItem.jsx
+++ front/src/pages/anime/home/section/genres/item/GenresItem.jsx
@@ -1,30 +1,27 @@
 import { memo } from "react"
 import { Link } from "react-router-dom"
+import { getPostfix } from "../../../../../../utils/utils"
 
-import { getPostfix } from "../../utils/getPostfix"
-
-import "./style/genres_item.sass"
+import "./style.sass"
 
 
 export const GenresItem = memo(({item, ...props}) => {
     
     return(
         <li className="genres__item" {...props}>
-            <Link className="genres__item-link" to={`anime/genres/${item["alias"]}`}>
+            <Link className="genres__item-link" to={`anime/genres/${item.alias}`}>
                 <div className="genres__bg-container">
-                    <img className="genres__bg" src={`data:image/webp;base64,${item["poster"]}`} loading="lazy" alt="genres-bg" />                            
+                    <img className="genres__bg" src={`data:image/webp;base64,${item.poster}`} loading="lazy" alt="genres-bg" />                            
                 </div>
                 <ul className="genres__desc-list">
                     <li className="genres__desc-item genres__desc_title">
-                        {item["genres"]}
+                        {item.genres}
                     </li>
                     <li className="genres__desc-item">
-                        {`${item["genres_count"]} ${getPostfix("релиз", item["genres_count"])}`}
+                        {`${item.genres_count} ${getPostfix("релиз", item.genres_count)}`}
                     </li>
                 </ul>
             </Link>
         </li>
     )
 })
-
-
