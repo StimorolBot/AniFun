76b8acbd44c8ae02d26b688a1bfbce2000e0e008
bot <maximbotpy@gmail.com>
2025-12-07 15:01:44 +0300

    feat: добавил компонент для фильтрации аниме

---
+++ front/src/components/filter/FilterTitle.jsx
@@ -0,0 +1,35 @@
+import { memo } from "react"
+
+import "./style/filter_title.sass"
+
+
+export const FilterTitle = memo(({filterList, filterParams, setFilterParams, keyFilter}) => {
+
+    const addFilterParams = (value) => {
+        if (filterParams.includes(value))
+            setFilterParams(s => ({...s, [keyFilter]: s[keyFilter].filter(p => p !== value)}))
+        else
+            setFilterParams(s => ({...s, [keyFilter]: [...s[keyFilter], value]}))       
+    }
+
+    return(
+        <ul className="filter-title__list">
+            {filterList?.map((item, index) => {
+                return(
+                    <li className="filter-title__item" key={index}>
+                        <input
+                            id={`filter-type-${item}`} 
+                            type="checkbox" 
+                            value={item} 
+                            name="filter-title"
+                            onChange={e => addFilterParams(e.target.value)}
+                        />
+                        <label htmlFor={`filter-type-${item}`}>
+                            {item} 
+                        </label>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+})
