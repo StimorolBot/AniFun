b1b4ed91974d3982f1108d8b2406bba6130ffca0
bot <maximbotpy@gmail.com>
2025-12-08 22:06:11 +0300

    refactor: перенес файл и переименовал файл

--- front/src/pages/anime/title/section/about/AboutTitle.jsx
+++ front/src/pages/anime/title/section/about/AboutTitle.jsx
@@ -1,21 +1,18 @@
-
-import { useContext } from "react"
 import { Link } from "react-router-dom"
 
-import { SubNav } from "../../ui/nav/SubNav"
-import { Rating } from "../rating/Rating"
-import { LinkWatch } from "../../ui/link/LinkWatch"
-import { BtnAddFavorite } from "../../ui/btn/BtnAddFavorite"
+import { SubNav } from "../../../../../ui/nav/SubNav"
+import { Rating } from "./../../mini_app/rating/Rating"
 
-import { getPostfix } from "../../utils/utils"
-import { GenresContext } from "../../context/GenresContext"
+import { getPostfix } from "../../../../../utils/utils"
 
-import "./style/title_data.sass"
+import { BtnSwitch } from "../../../../../ui/btn/BtnSwitch"
+import { BtnDefault } from "../../../../../ui/btn/BtnDefault"
+
+import "./style.sass"
 
 
-export const TitleData = ({response, subNav}) => {
-    const {_, setGenresContext} = useContext(GenresContext)
-    
+export const AboutTitle = ({response, subNav}) => {
+
     return(
         <section className="title-data__section">
             <SubNav subNav={subNav}/>
@@ -25,13 +22,13 @@ export const TitleData = ({response, subNav}) => {
                         <img className="title-data__img" src={`data:image/webp;base64,${response?.poster}`} alt="poster"/>
                     </div>
                     <div className="title-data__info">
-                        <Rating title={response.data.title}/>
+                        <Rating title={response.title}/>
                         <h2 className="title-data__anime-title">
-                            {response.data.title}
+                            {response.title}
                         </h2>
                         <div className="title-data__age-shed">
                             <p className="title-data__age">
-                                {response.data.age_restrict}
+                                {response.age_restrict}
                             </p>
                             {response?.day_week &&
                                 <p className="title-data__shed">
@@ -42,24 +39,23 @@ export const TitleData = ({response, subNav}) => {
                         <dl className="title-data__list">
                             <div className="title-data__item">
                                 <dt>Тип:</dt>
-                                <dd>{response.data.type}</dd>
+                                <dd>{response.type}</dd>
                             </div>
                             <div className="title-data__item">
                                 <dt>Статус:</dt>
-                                <dd>{response.data.status}</dd>
+                                <dd>{response.status}</dd>
                             </div>
                             <div className="title-data__item">
                                 <dt>Сезон:</dt>
-                                <dd>{response.data.season}</dd>
+                                <dd>{response.season}</dd>
                             </div>
                             <ul className="title-data__tag-list">
                                 <li>Жанры:</li>
                                 {response.genres.map(genre => {
                                     return (
                                         <Link 
-                                            to={"/anime/catalog"} 
+                                            to={`/anime/genres/${genre}`} 
                                             key={genre} 
-                                            onClick={() => setGenresContext(genre)}
                                         > 
                                             <li className="title-data__tag-item">
                                                 {genre}
@@ -70,26 +66,31 @@ export const TitleData = ({response, subNav}) => {
                             </ul>
                             <div className="title-data__item">
                                 <dt>Год выхода:</dt>
-                                <dd>{response.data.year}</dd>
+                                <dd>{response.year}</dd>
                             </div>
                             <div className="title-data__item">
                                 <dt>Всего эпизодов:</dt>
                                 <dd>
-                                    {`${response.data.total_episode} `} 
-                                    {getPostfix("эпизод", response.data.total_episode)}
+                                    {`${response.total_episode} `} 
+                                    {getPostfix("эпизод", response.total_episode)}
                                 </dd>
                             </div>
                             <div className="title-data__bottom">
-                                <LinkWatch alias={`/anime/video/episode/${response?.uuid}}`}>
-                                    Смотреть с 1 эпизода
-                                </LinkWatch>
-                                <BtnAddFavorite/>
+                                <Link>
+
+                                </Link>
+                                <BtnSwitch>
+
+                                </BtnSwitch>
+                                <BtnDefault>
+
+                                </BtnDefault>
                             </div>
                         </dl>
                     </div>
                 </div>
                 <p className="title-data__anime-description">
-                    {response.data.description}
+                    {response.description}
                 </p>
             </div>
         </section>
