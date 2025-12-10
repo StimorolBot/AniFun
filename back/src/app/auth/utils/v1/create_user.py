dc4e9487681fb7fb8879ca30cbf81a5e4b7a983c
bot <maximbotpy@gmail.com>
2025-12-10 22:22:41 +0300

    feat: добавил создание аватара пользователя refactor: вынес создание токенов в отдельную функцию

--- back/src/app/auth/utils/v1/create_user.py
+++ back/src/app/auth/utils/v1/create_user.py
@@ -1,37 +1,25 @@
-from datetime import timedelta
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.utils.crud import crud
-from src.utils.logger import auth_log
+from src.app.auth.base.api_v1.jwt.jwt_token import jwt_token
 from src.app.auth.models.v1.main.auth import AuthTable
 from src.app.auth.models.v1.main.token import TokenTable
-
-from src.app.auth.base.api_v1.jwt.config import jwt_setting
-from src.app.auth.base.api_v1.jwt.jwt_token import jwt_token
-from src.app.auth.base.api_v1.jwt.token_type import TokenType
+from src.app.user.models.v1.avatar import AvatarTable
+from src.utils.crud import crud
+from src.utils.logger import auth_log
 
 
 async def create_user(session: AsyncSession, data: dict) -> dict:
     user_created = await crud.create(session=session, table=AuthTable, data=data)
-
     await session.flush()
 
-    access_token = jwt_token.create(
-        token_type=TokenType.ACCESS.value,
-        token_data={"sub": user_created.uuid, "auth_type": data["auth_type"]}
-    )
-
-    refresh_token = jwt_token.create(
-        token_type=TokenType.REFRESH.value,
-        token_data={"sub": user_created.uuid, "auth_type": data["auth_type"]},
-        expire_timedelta=timedelta(days=jwt_setting.refresh_token_expire_days)
-    )
+    tokens = jwt_token.create_tokens(sub=user_created.uuid, auth_type=data["auth_type"])
+    await crud.create(session=session, table=AvatarTable, data={"uuid": user_created.uuid})
 
     await crud.create(
         session=session, table=TokenTable,
-        data={"uuid": user_created.uuid, "refresh_token": refresh_token}
+        data={"uuid": user_created.uuid, "refresh_token": tokens["refresh_token"]}
     )
     await session.commit()
 
     auth_log.info("Создание учетной записи: %s", data["identifier"])
-    return {"access_token": access_token}
+    return tokens
