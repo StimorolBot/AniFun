17dd6676f2c63a2ee33290a100f8ed8ef0c5c10e
bot <maximbotpy@gmail.com>
2025-12-10 20:37:44 +0300

    refactor: переименовал таблицу

--- back/src/app/anime/models/v1/sub/season.py
+++ back/src/app/anime/models/v1/sub/season.py
@@ -1,24 +1,26 @@
 from typing import TYPE_CHECKING
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from src.app.anime.enums.v1.sub.season import Season
 from src.model import Base
 from src.utils.crud import crud
-from src.app.anime.enums.v1.sub.season import Season
 
 if TYPE_CHECKING:
     from src.app.anime.models.v1.main.anime import AnimeTable
 
 
-class SeasonTable(Base):
-    __tablename__ = "season_table"
+class SeasonSubTable(Base):
+    __tablename__ = "season_sub_table"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     season: Mapped[str] = mapped_column(unique=True)
+    alias: Mapped[str] = mapped_column(unique=True)
 
     anime_rs: Mapped["AnimeTable"] = relationship(back_populates="season_rs")
 
     @classmethod
     async def fill(cls, session: AsyncSession):
         for data in Season:
-            await crud.create(session=session, table=cls, data={"season": data.value})
+            await crud.create(session=session, table=cls, data={"season": data.value, "alias": data.name.lower()})
