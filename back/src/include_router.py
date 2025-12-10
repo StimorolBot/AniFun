e1bb8f3e1d8d86a62e0d3eedd88c98ce217a031a
bot <maximbotpy@gmail.com>
2025-12-10 22:26:25 +0300

    refactor: переименовал releases_router -> title_router

--- back/src/include_router.py
+++ back/src/include_router.py
@@ -4,10 +4,10 @@ from src.app.admin.page.anime.api_v1.router import admin_anime_router
 from src.app.admin.page.user.api_v1.router import admin_user_router
 from src.app.anime.page.home.api_v1.router import home_router
 from src.app.anime.page.release.api_v1.router import release_router
-from src.app.anime.page.releases.api_v1.router import releases_router
-from src.app.anime.page.releases.mini_app.comment.api_v1.router import \
+from src.app.anime.page.title.api_v1.router import title_router
+from src.app.anime.page.title.mini_app.comment.api_v1.router import \
     comment_router
-from src.app.anime.page.releases.mini_app.rating.api_v1.router import \
+from src.app.anime.page.title.mini_app.rating.api_v1.router import \
     rating_router
 from src.app.anime.page.video.api_v1.router import video_router
 from src.app.auth.base.api_v1.router import auth_router
@@ -24,7 +24,7 @@ def include_router(app: FastAPI):
 
     app.include_router(home_router)
     app.include_router(release_router)
-    app.include_router(releases_router)
+    app.include_router(title_router)
 
     app.include_router(comment_router)
     app.include_router(rating_router)
