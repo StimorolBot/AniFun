44cdea96311c6507bb304b9ffe3f44a1bfa7b236
bot <maximbotpy@gmail.com>
2025-12-10 20:38:48 +0300

    refactor: переименовал таблицы

--- back/src/app/anime/models/v1/sub/__init__.py
+++ back/src/app/anime/models/v1/sub/__init__.py
@@ -1,10 +1,9 @@
-from src.app.anime.models.v1.sub.type import TypeTable
-from src.app.anime.models.v1.sub.genres import GenresTable
-from src.app.anime.models.v1.sub.age_restrictions import AgeRestrictTable
-from src.app.anime.models.v1.sub.season import SeasonTable
-from src.app.anime.models.v1.sub.status import StatusTable
+from src.app.anime.models.v1.sub.age_restrictions import AgeRestrictSubTable
+from src.app.anime.models.v1.sub.genres import GenresSubTable
+from src.app.anime.models.v1.sub.season import SeasonSubTable
+from src.app.anime.models.v1.sub.status import StatusSubTable
+from src.app.anime.models.v1.sub.type import TypeSubTable
 
-# ?
 __all__ = (
-    TypeTable, AgeRestrictTable, SeasonTable, StatusTable, GenresTable,
+    TypeSubTable, AgeRestrictSubTable, SeasonSubTable, StatusSubTable, GenresSubTable,
 )
