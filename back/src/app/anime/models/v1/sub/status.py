27080cf15b867e3a6198153095c6c17da6721a90
bot <maximbotpy@gmail.com>
2025-12-10 20:37:30 +0300

    refactor: переименовал таблицу

--- back/src/app/anime/models/v1/sub/status.py
+++ back/src/app/anime/models/v1/sub/status.py
@@ -1,25 +1,26 @@
 from typing import TYPE_CHECKING
-from sqlalchemy.ext.asyncio import AsyncSession
 
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from src.app.anime.enums.v1.sub.status import Status
 from src.model import Base
 from src.utils.crud import crud
-from src.app.anime.enums.v1.sub.status import Status
 
 if TYPE_CHECKING:
     from src.app.anime.models.v1.main.anime import AnimeTable
 
 
-class StatusTable(Base):
-    __tablename__ = "status_table"
+class StatusSubTable(Base):
+    __tablename__ = "status_sub_table"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     status: Mapped[str] = mapped_column(unique=True)
+    alias: Mapped[str] = mapped_column(unique=True)
 
     anime_rs: Mapped["AnimeTable"] = relationship(back_populates="status_rs")
 
     @classmethod
     async def fill(cls, session: AsyncSession):
         for data in Status:
-            await crud.create(session=session, table=cls, data={"status": data.value})
+            await crud.create(session=session, table=cls, data={"status": data.value, "alias": data.name.lower()})
