c9451649cb9bcb36a2894914921add62996710ea
bot <maximbotpy@gmail.com>
2025-12-10 20:36:35 +0300

    refactor: переименовал таблицу

--- back/src/app/anime/models/v1/sub/genres.py
+++ back/src/app/anime/models/v1/sub/genres.py
@@ -1,28 +1,29 @@
 import os
 from typing import TYPE_CHECKING
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from src.app.anime.enums.v1.sub.genres import Genres
 from src.model import Base
 from src.utils.crud import crud
 from src.utils.utils import get_base64
-from src.app.anime.enums.v1.sub.genres import Genres
 
 if TYPE_CHECKING:
-    from src.app.anime.models.v1.main.genres_anime import GenresAnimeTable
+    from src.app.anime.models.v1.main.genres_anime import GenresTable
 
 
-class GenresTable(Base):
+class GenresSubTable(Base):
     """Таблица с перечнем всех жанров"""
 
-    __tablename__ = "genres_table"
+    __tablename__ = "genres_sub_table"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     genres: Mapped[str] = mapped_column(unique=True)
     poster: Mapped[str] = mapped_column()
     alias: Mapped[str] = mapped_column()
 
-    genres_anime_rs: Mapped["GenresAnimeTable"] = relationship(back_populates="genres_rs")
+    genres_anime_rs: Mapped["GenresTable"] = relationship(back_populates="genres_rs")
 
     @classmethod
     async def fill(cls, session: AsyncSession):
