a616ca23a89283b7036cfbd3a000665a6f0a5444
bot <maximbotpy@gmail.com>
2025-12-10 20:36:53 +0300

    refactor: переименовал таблицу

--- back/src/app/anime/models/v1/sub/age_restrictions.py
+++ back/src/app/anime/models/v1/sub/age_restrictions.py
@@ -3,16 +3,16 @@ from typing import TYPE_CHECKING
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from src.app.anime.enums.v1.sub.restrict import Restrict
 from src.model import Base
 from src.utils.crud import crud
-from src.app.anime.enums.v1.sub.restrict import Restrict
 
 if TYPE_CHECKING:
     from src.app.anime.models.v1.main.anime import AnimeTable
 
 
-class AgeRestrictTable(Base):
-    __tablename__ = "age_restrict_table"
+class AgeRestrictSubTable(Base):
+    __tablename__ = "age_r_sub_table"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     restrict: Mapped[str] = mapped_column(unique=True)
