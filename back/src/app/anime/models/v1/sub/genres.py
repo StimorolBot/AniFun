d19fc9a4518b0675aaa6165b2f3dc8fa359808f2
bot <maximbotpy@gmail.com>
2025-12-14 21:52:45 +0300

    refactor: изменил заполнение поля alias

--- back/src/app/anime/models/v1/sub/genres.py
+++ back/src/app/anime/models/v1/sub/genres.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.app.anime.enums.v1.sub.genres import Genres
 from src.model import Base
 from src.utils.crud import crud
+from src.utils.logger import server_log
 from src.utils.utils import get_base64
 
 if TYPE_CHECKING:
@@ -19,18 +20,30 @@ class GenresSubTable(Base):
     __tablename__ = "genres_sub_table"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    genres: Mapped[str] = mapped_column(unique=True)
+    genre: Mapped[str] = mapped_column(unique=True)
     poster: Mapped[str] = mapped_column()
-    alias: Mapped[str] = mapped_column()
+    alias: Mapped[str] = mapped_column(unique=True)
 
-    genres_anime_rs: Mapped["GenresTable"] = relationship(back_populates="genres_rs")
+    genres_anime_rs: Mapped["GenresTable"] = relationship(
+        back_populates="genres_rs",
+        foreign_keys="[GenresTable.genre_alias]"
+    )
 
     @classmethod
     async def fill(cls, session: AsyncSession):
         path = f"{os.getcwd()}\\..\\front\\public\\.genres"
         poster_base64 = await get_base64(path)
         for data in Genres:
-            await crud.create(
-                session=session, table=cls,
-                data={"genres": data.value, "poster": poster_base64[data.value], "alias": data.name.lower()}
-            )
+            try:
+                await crud.create(
+                    session=session, table=cls,
+                    data={
+                        "genre": data.value["label"],
+                        "poster": poster_base64[data.value["label"]],
+                        "alias": data.value["value"]}
+                )
+            except KeyError:
+                server_log.error(
+                    "Не удалось найти файл с именем %s.",
+                    data.value["label"]
+                )
