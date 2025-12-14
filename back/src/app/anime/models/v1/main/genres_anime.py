6634f00aa2698b5b2e02d12d5f0cb5a40e7b2190
bot <maximbotpy@gmail.com>
2025-12-14 21:53:27 +0300

    feat: добавил поля genre_alias и alias

--- back/src/app/anime/models/v1/main/genres_anime.py
+++ back/src/app/anime/models/v1/main/genres_anime.py
@@ -7,19 +7,27 @@ from src.model import Base
 
 if TYPE_CHECKING:
     from src.app.anime.models.v1.main.anime import AnimeTable
-    from src.app.anime.models.v1.sub.genres import GenresTable
+    from src.app.anime.models.v1.sub.genres import GenresSubTable
 
 
-class GenresAnimeTable(Base):
-    __tablename__ = "genres_anime_table"
+class GenresTable(Base):
+    """Основная таблица с жанрами"""
+
+    __tablename__ = "genres_table"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    title: Mapped[str] = mapped_column(ForeignKey("anime_table.title"))
-    genres: Mapped[str] = mapped_column(ForeignKey("genres_table.genres"), unique=False)
+    title: Mapped[str] = mapped_column(
+        ForeignKey("anime_table.title", onupdate="CASCADE", ondelete="CASCADE"),
+        index=True
+    )
+    alias: Mapped[str] = mapped_column(
+        ForeignKey("anime_table.alias", onupdate="CASCADE", ondelete="CASCADE"),
+    )
+    genre: Mapped[str] = mapped_column(ForeignKey("genres_sub_table.genre"), unique=False)
+    genre_alias: Mapped[str] = mapped_column(ForeignKey("genres_sub_table.alias"))
 
-    anime_rs: Mapped["AnimeTable"] = relationship(back_populates="genres_rs")
-    genres_rs: Mapped["GenresTable"] = relationship(back_populates="genres_anime_rs")
-
+    anime_rs: Mapped["AnimeTable"] = relationship(back_populates="genres_rs", foreign_keys=[title])
+    genres_rs: Mapped["GenresSubTable"] = relationship(back_populates="genres_anime_rs", foreign_keys=[genre_alias])
 
     # сделать уникальным по нескольким столбцам
-    __table_args__ = (UniqueConstraint("title", "genres"),)
+    __table_args__ = (UniqueConstraint("title", "genre", "alias"),)
