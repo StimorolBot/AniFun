029b327fb8f0b39639f579fb70acf3fd0f68039f
bot <maximbotpy@gmail.com>
2025-12-15 22:06:54 +0300

    refactor: переименовал os_log -> server_log

--- back/src/utils/table.py
+++ back/src/utils/table.py
@@ -1,16 +1,16 @@
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.utils.logger import os_log
+from src.utils.logger import server_log
 
 
 async def fill_table(session: AsyncSession, table_list):
     for model in table_list:
-        os_log.debug(f"Проверка заполнения таблицы: '{model.__tablename__}'")
+        server_log.debug(f"Проверка заполнения таблицы: '{model.__tablename__}'")
         try:
             await model.fill(session)
             await session.commit()
-            os_log.debug(f"Таблица '{model.__tablename__}' заполнена")
+            server_log.debug(f"Таблица '{model.__tablename__}' заполнена")
         except IntegrityError as e:
             await session.rollback()
-            os_log.info(e)
+            server_log.info(e)
