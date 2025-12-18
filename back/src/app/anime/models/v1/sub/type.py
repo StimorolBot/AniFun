325aa690d973df9678bcfb6ae5c15ccce5d31a56
bot <maximbotpy@gmail.com>
2025-12-18 22:21:47 +0300

    refactor: переименовал колонки type и alias

--- back/src/app/anime/models/v1/sub/type.py
+++ back/src/app/anime/models/v1/sub/type.py
@@ -1,5 +1,6 @@
 from typing import TYPE_CHECKING
 
+from sqlalchemy import Enum
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -15,12 +16,16 @@ class TypeSubTable(Base):
     __tablename__ = "type_sub_table"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    type: Mapped[str] = mapped_column(unique=True)
-    alias: Mapped[str] = mapped_column(unique=True)
+    label: Mapped[str] = mapped_column(unique=True)
+    value: Mapped[str] = mapped_column(unique=True)
 
     anime_rs: Mapped["AnimeTable"] = relationship(back_populates="type_rs")
 
     @classmethod
     async def fill(cls, session: AsyncSession):
         for data in Type:
-            await crud.create(session=session, table=cls, data={"type": data.value, "alias": data.name.lower()})
+            await crud.create(
+                session=session,
+                table=cls,
+                data={"label": data.value["label"], "value": data.value["value"]}
+            )
