a88b5b99841e04c6568df268b0bac6d5f7505e15
bot <maximbotpy@gmail.com>
2025-12-18 22:21:16 +0300

    feat: добавил enum с английским значением для ссылок, и общий для админа

--- back/src/app/anime/enums/v1/sub/type.py
+++ back/src/app/anime/enums/v1/sub/type.py
@@ -1,9 +1,25 @@
 from enum import Enum
 
 
+class TypeLabel(Enum):
+    TV = "тв сериал"
+    OVA = "ova"
+    ONA = "ona"
+    FILM = "фильм"
+    SPECIAL = "спешл"
+
+
+class TypeValue(Enum):
+    TV = "tv"
+    OVA = "ova"
+    ONA = "ona"
+    FILM = "film"
+    SPECIAL = "special"
+
+
 class Type(Enum):
-    TV = "ТВ сериал"
-    OVA = "OVA"
-    ONA = "ONA"
-    FILM = "Фильм"
-    SPECIAL = "Спешл"
+    TV = {"value": TypeValue.TV.value, "label": TypeLabel.TV.value}
+    OVA = {"value": TypeValue.OVA.value, "label": TypeLabel.OVA.value}
+    ONA = {"value": TypeValue.ONA.value, "label": TypeLabel.ONA.value}
+    FILM = {"value": TypeValue.FILM.value, "label": TypeLabel.FILM.value}
+    SPECIAL = {"value": TypeValue.SPECIAL.value, "label": TypeLabel.SPECIAL.value}
