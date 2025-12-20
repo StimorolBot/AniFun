82567447748cecc605dc07a42d32a240328ba84d
bot <maximbotpy@gmail.com>
2025-12-20 21:05:04 +0300

    refactor: переименовал колонки

--- back/src/app/anime/models/v1/sub/genres.py
+++ back/src/app/anime/models/v1/sub/genres.py
@@ -15,32 +15,35 @@ if TYPE_CHECKING:
 
 
 class GenresSubTable(Base):
-    """Таблица с перечнем всех жанров"""
+    """Таблица с жанрами"""
 
     __tablename__ = "genres_sub_table"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    genre: Mapped[str] = mapped_column(unique=True)
+    label: Mapped[str] = mapped_column(unique=True)
+    value: Mapped[str] = mapped_column(unique=True)
     poster: Mapped[str] = mapped_column()
-    alias: Mapped[str] = mapped_column(unique=True)
 
     genres_anime_rs: Mapped["GenresTable"] = relationship(
         back_populates="genres_rs",
-        foreign_keys="[GenresTable.genre_alias]"
+        foreign_keys="[GenresTable.value]"
     )
 
     @classmethod
     async def fill(cls, session: AsyncSession):
         path = f"{os.getcwd()}\\..\\front\\public\\.genres"
         poster_base64 = await get_base64(path)
+
         for data in Genres:
             try:
                 await crud.create(
-                    session=session, table=cls,
+                    session=session,
+                    table=cls,
                     data={
-                        "genre": data.value["label"],
-                        "poster": poster_base64[data.value["label"]],
-                        "alias": data.value["value"]}
+                        "label": data.value.get("label"),
+                        "value": data.value.get("value"),
+                        "poster": poster_base64[data.value["label"]]
+                    }
                 )
             except KeyError:
                 server_log.error(
