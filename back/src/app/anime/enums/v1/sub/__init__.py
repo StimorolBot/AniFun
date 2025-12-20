5c85485bc62b027c34a2935fa8cc57d71bb7e047
bot <maximbotpy@gmail.com>
2025-12-20 20:54:36 +0300

    feat: импортировал enum label, value

--- back/src/app/anime/enums/v1/sub/__init__.py
+++ back/src/app/anime/enums/v1/sub/__init__.py
@@ -1,9 +1,17 @@
-from .day_week import DayWeek
-from .genres import Genres
+from .day_week import DayWeek, DayWeekLabel, DayWeekValue
+from .genres import Genres, GenresLabel, GenresValue
 from .limit_episode import LimitEpisode
-from .restrict import Restrict
-from .season import Season
-from .status import Status
-from .type import Type
+from .restrict import Restrict, RestrictLabel, RestrictValue
+from .season import Season, SeasonLabel, SeasonValue
+from .status import Status, StatusLabel, StatusValue
+from .type import Type, TypeLabel, TypeValue
 
-__all__ = (Type, Restrict, Season, Status, Genres, DayWeek, LimitEpisode)
+__all__ = (
+    Type, TypeValue, TypeLabel,
+    Season, SeasonValue, SeasonLabel,
+    Restrict, RestrictValue, RestrictLabel,
+    Status, StatusValue, StatusLabel,
+    Genres, GenresValue, GenresLabel,
+    DayWeek, DayWeekValue, DayWeekLabel,
+    LimitEpisode
+)
