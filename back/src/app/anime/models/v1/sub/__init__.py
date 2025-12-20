865a94ed0c823b12841231b60e45e0632fe03af0
bot <maximbotpy@gmail.com>
2025-12-20 20:55:09 +0300

    feat: добавил вспомогательную таблицу DayWeekSubTable

--- back/src/app/anime/models/v1/sub/__init__.py
+++ back/src/app/anime/models/v1/sub/__init__.py
@@ -1,9 +1,10 @@
 from src.app.anime.models.v1.sub.age_restrictions import AgeRestrictSubTable
+from src.app.anime.models.v1.sub.day_week import DayWeekSubTable
 from src.app.anime.models.v1.sub.genres import GenresSubTable
 from src.app.anime.models.v1.sub.season import SeasonSubTable
 from src.app.anime.models.v1.sub.status import StatusSubTable
 from src.app.anime.models.v1.sub.type import TypeSubTable
 
 __all__ = (
-    TypeSubTable, AgeRestrictSubTable, SeasonSubTable, StatusSubTable, GenresSubTable,
+    TypeSubTable, AgeRestrictSubTable, SeasonSubTable, StatusSubTable, GenresSubTable, DayWeekSubTable
 )
