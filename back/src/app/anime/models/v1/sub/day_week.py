5fb3443a883fb91bf57592637ccfa9f5755c88c4
bot <maximbotpy@gmail.com>
2025-12-21 20:28:50 +0300

    remove: удалил relationship

--- back/src/app/anime/models/v1/sub/day_week.py
+++ back/src/app/anime/models/v1/sub/day_week.py
@@ -7,9 +7,6 @@ from src.app.anime.enums.v1.sub.day_week import DayWeek
 from src.model import Base
 from src.utils.crud import crud
 
-if TYPE_CHECKING:
-    from src.app.anime.models.v1.main.schedule import ScheduleTable
-
 
 class DayWeekSubTable(Base):
     """Таблица с днями недели """
@@ -18,8 +15,6 @@ class DayWeekSubTable(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, unique=True)
     label: Mapped[str] = mapped_column(unique=True)
     value: Mapped[str] = mapped_column(unique=True)
-
-    schedule_rs: Mapped["ScheduleTable"] = relationship(back_populates="day_week_rs")
 
     @classmethod
     async def fill(cls, session: AsyncSession):
