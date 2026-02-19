bfeac6a6bce8d9b5ea9a3df2636d7310e8658159
bot <maximbotpy@gmail.com>
2026-02-19 20:53:58 +0300

    feat: добавил запрос для постера refactor: поправил ссылки

--- front/src/pages/anime/home/section/new_episodes/item/EpisodeItem.jsx
+++ front/src/pages/anime/home/section/new_episodes/item/EpisodeItem.jsx
@@ -1,47 +1,62 @@
-import { memo } from "react"
+import { memo, useEffect } from "react"
 import { Link } from "react-router-dom"
 
 import "./style.sass"
 
 
-export const EpisodeItem = memo(({item, ...props}) => {
-    
+export const EpisodeItem = memo(({item, getUrlImg, urlIMg, index, ...props}) => {
+
+    useEffect(() => {(
+        async () => {
+            if (item.anime.poster?.poster_uuid)
+                await getUrlImg(`/s3/anime-${item.anime.uuid}/${item.anime.poster.poster_uuid}`)
+        })()
+    }, [index])
+
     return(
         <li className="episode__item" {...props}>
-            <img className="episode__img" src={`data:image/webp;base64,${item.poster}`} alt="new-episode"/>
+            <img className="episode__img" src={urlIMg} loading="lazy" alt=" " />
             <div className="episode__description">
                 <p className="episode__number">
-                    {item.episode_number} эпизод
+                    {item.number} эпизод
                 </p>
-                <Link className="episode__title" to={`/anime/${item.alias}`}>
-                    {item.title}
+                <Link className="episode__title" to={`/anime/${item.anime.alias}`}>
+                    {item.anime.title}
                 </Link>
                 <ul className="episode__desc-list">
                     <li className="episode__desc-item">
-                        {item.year}
+                        <Link to={`/anime`} state={item.anime.year}>
+                            {item.anime.year}
+                        </Link>
                     </li>
                     <li className="episode__desc-item">
-                        {item.season}
+                        <Link to={`/anime`} state={item.anime.season.value}>
+                            {item.anime.season.label}
+                        </Link>
                     </li>
                     <li className="episode__desc-item">
-                        {item.type}
+                        <Link to={`/anime`} state={item.anime.type.value}>
+                            {item.anime.type.label}
+                        </Link>
                     </li>
                     <li className="episode__desc-item">
-                        {item.age_restrict}
+                        <Link to={`/anime`} state={item.anime.age_restrict.value}>
+                            {item.anime.age_restrict.label}
+                        </Link>
                     </li>
                 </ul>
                 <ul className="episode__desc-list">
-                    {item.genres?.slice(-2)?.map((genre, index) => {
+                    {item.anime.genres?.slice(-3)?.map((genre, index) => {
                         return(
                             <li className="episode__desc-item episode__desc-item_tag" key={index}>
-                                <Link to={`anime/genres/${genre}`}>
-                                    {genre}
+                                <Link to={`anime/genres/${genre.value}`}>
+                                    {genre.label}
                                 </Link>
                             </li>
                         )
                     })}
                 </ul>
-                <Link className="episode__link" to={`/anime/episode/${item.uuid_episode}`}>
+                <Link className="episode__link" to={`anime/${item.anime.alias}/episode/${item.uuid_episode}`}>
                     Смотреть
                 </Link>
             </div>
