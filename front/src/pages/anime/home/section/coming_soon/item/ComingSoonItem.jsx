188260b6d19fe1dfcd327aa5edd11fdb4752680b
bot <maximbotpy@gmail.com>
2026-02-19 21:26:00 +0300

    feat: добавил запрос для постера refactor: поправил ссылки

--- front/src/pages/anime/home/section/coming_soon/item/ComingSoonItem.jsx
+++ front/src/pages/anime/home/section/coming_soon/item/ComingSoonItem.jsx
@@ -1,39 +1,47 @@
+import { useEffect } from "react"
+
 import { Link } from "react-router-dom"
 
 import "./style.sass"
 
 
-export const ComingSoonItem = ({item, ...props}) => {
+export const ComingSoonItem = ({item, getUrlImg, urlIMg, schedule, ...props}) => {
     
+    useEffect(() => {(
+        async () => {
+            await getUrlImg(`/s3/anime-${item.anime.uuid}/${item.anime.poster.poster_uuid}`)
+        })()
+    }, [item.anime.alias, schedule])
+
     return(
         <li className="coming-soon__item" {...props}>
-            <Link className="coming-soon__link" to={`/anime/${item?.alias}`}>  
-                <img className="coming-soon__bg" src={`data:image/webp;base64,${item.poster}`} loading="lazy" alt="preview" />
+            <Link className="coming-soon__link" to={`/anime/${item.anime.alias}`}>  
+                <img className="coming-soon__bg" src={urlIMg} loading="lazy" alt="preview" />
                 <p className="coming-soon__title">
-                    {item.title}
+                    {item.anime.title}
                 </p>
                 <p className="coming-soon__episode">
                     {`${item.episode_number} ${"Эпизод"}`}
                 </p>
                 <ul className="coming-soon__desc-list">
                     <li className="coming-soon__desc-item">
-                        {item?.season}
+                        {item?.anime.season.label}
                     </li>
                     <li className="coming-soon__desc-item">
-                        {item?.year}
+                        {item?.anime.year}
                     </li>
                      <li className="coming-soon__desc-item">
-                        {item?.age_restrict}
+                        {item?.anime.age_restrict.label}
                     </li>
                     <li className="coming-soon__desc-item">
-                        {item?.type}
+                        {item?.anime.type.label}
                     </li>
                 </ul>
                 <ul className="coming-soon__desc-list">
-                    {item?.genres?.slice(-2)?.map((genre, index) => {
+                    {item?.anime.genres?.slice(-2)?.map((genre, index) => {
                         return(
                             <li className="coming-soon__desc-item" key={index}>
-                                {genre}
+                                {genre.label}
                             </li>
                         )
                     })}
