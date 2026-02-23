8464413c3a5aeb9f4c6eb487238a34c9086453e9
bot <maximbotpy@gmail.com>
2026-02-23 12:56:35 +0300

    refactor: исправил ссылку

--- front/src/pages/anime/home/section/genres/item/GenresItem.jsx
+++ front/src/pages/anime/home/section/genres/item/GenresItem.jsx
@@ -5,17 +5,17 @@ import { getPostfix } from "../../../../../../utils/utils"
 import "./style.sass"
 
 
-export const GenresItem = memo(({item, ...props}) => {
+export const GenresItem = memo(({item, imgData, ...props}) => {
     
     return(
         <li className="genres__item" {...props}>
-            <Link className="genres__item-link" to={`anime/genres/${item.alias}`}>
+            <Link className="genres__item-link" to={`anime/genres/${item.value}`}>
                 <div className="genres__bg-container">
-                    <img className="genres__bg" src={`data:image/webp;base64,${item.poster}`} loading="lazy" alt="genres-bg" />                            
+                    <img className="genres__bg" src={imgData} loading="lazy" alt="genres-bg" />                            
                 </div>
                 <ul className="genres__desc-list">
                     <li className="genres__desc-item genres__desc_title">
-                        {item.genres}
+                        {item.label}
                     </li>
                     <li className="genres__desc-item">
                         {`${item.genres_count} ${getPostfix("релиз", item.genres_count)}`}
