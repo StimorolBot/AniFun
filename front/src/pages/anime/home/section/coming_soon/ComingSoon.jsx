984e3787a1c718c68f7e3e423446f2bb155a1b8a
bot <maximbotpy@gmail.com>
2026-02-23 11:58:04 +0300

    refactor: переписал запросы

--- front/src/pages/anime/home/section/coming_soon/ComingSoon.jsx
+++ front/src/pages/anime/home/section/coming_soon/ComingSoon.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
+
 import Masonry from "react-masonry-css"
+import { useQueries, useQuery } from "@tanstack/react-query"
 
 import { api } from "../../../../../api"
-import { useFetch } from "../../../../../hook/useFetch"
 
 import { WrapperSection } from "../../../wrapper/WrapperSection"
 import { ComingSoonItem } from "./item/ComingSoonItem"
@@ -15,27 +16,27 @@ import "./style.sass"
 
 export const ComingSoon = () => {
     const transitionRef = useRef()
-    const [urlIMg, setUrlIMg] = useState()
     const [schedule, setSchedule] = useState("today")
-    const [response, setResponse] = useState([])
+    
+    const {data: scheduleData, isLoading, error} = useQuery({
+        queryKey: ["schedule", schedule],
+        staleTime: 1000 * 60 * 3,
+        retry: false,
+        queryFn: async () => {
+            return await api.get("/schedules", {params: {"schedule": schedule}}).then(r => r.data)
+        },
+        placeholderData: []
+    })
 
-    const [request, isLoading, _] = useFetch(
-        async () => {
-            await api.get("/schedules", {params: {"schedule":schedule}}).then((r) => setResponse(r.data))
-        }
-    )
-
-    const [getUrlImg, isLoadingImg, errorImg] = useFetch(
-        async (url) => {
-        await api.get(url).then(r => setUrlIMg(r.data))  
-        }
-    )
-
-    useEffect(() => {(
-        async () => {
-            await request()
-        })()
-    }, [schedule])
+    const imgData = useQueries({
+        queries: scheduleData?.map(item => ({
+            queryKey: ["schedule-poster", item.anime.poster.poster_uuid],
+            staleTime: 1000 * 60 * 3,
+            queryFn: async () => {
+                return await api.get(`/s3/anime-${item.anime.uuid}/${item.anime.poster.poster_uuid}`).then(r => r.data)
+            }
+        }))
+    })
 
     const breakpoints = {
         default: 4,
@@ -55,10 +56,10 @@ export const ComingSoon = () => {
                         <div className="container-coming-soon transition" ref={transitionRef}>
                             { isLoading
                                 ? <Loader/>
-                                : response.length >= 1 
+                                : scheduleData.length >= 1 
                                     ? <Masonry breakpointCols={breakpoints} className="masonry" columnClassName="masonry__column">
-                                        {response?.map((item, index) => {
-                                            return <ComingSoonItem item={item} getUrlImg={getUrlImg} urlIMg={urlIMg} schedule={schedule} key={index}/>
+                                        {scheduleData?.map((item, index) => {
+                                            return <ComingSoonItem item={item} imgData={imgData[index].data} key={index}/>
                                         })}
                                     </Masonry>
                                     :<p className="coming-soon__empty">
