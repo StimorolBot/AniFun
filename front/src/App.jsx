e1e00e8ec8f321a40c11dda34bfdfd5a6abf6887
bot <maximbotpy@gmail.com>
2026-02-24 20:08:57 +0300

    feat: подключил QueryClientProvider

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,15 +2,24 @@ import { useState } from "react"
 import { router } from "./router"
 import { RouterProvider } from "react-router-dom"
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+
 import { GenresContext } from "./context/GenresContext"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 
 export function App() {
   const [genresContext, setGenresContext] = useState([])
+  const queryClient = new QueryClient()
+
 
   return (
-    <GenresContext.Provider value={{genresContext, setGenresContext}}>
-      <RouterProvider router={ router }/>
-    </GenresContext.Provider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false}/>
+      <GenresContext.Provider value={{genresContext, setGenresContext}}>
+        <RouterProvider router={ router }/>
+      </GenresContext.Provider>
+    </QueryClientProvider>
   )
 }
