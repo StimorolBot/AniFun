1d7432de884974f678280cae8d4ae60c919c5a30
bot <maximbotpy@gmail.com>
2026-02-24 20:42:06 +0300

    refactor: разделил таблицу ImgTable на PosterTable и BannerTable

--- back/src/app/anime/models/v1/main/img.py
+++ back/src/app/anime/models/v1/main/img.py
@@ -1,4 +1,5 @@
 from typing import TYPE_CHECKING
+from uuid import UUID
 
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -9,15 +10,25 @@ if TYPE_CHECKING:
     from src.app.anime.models.v1.main.anime import AnimeTable
 
 
-class ImgTable(Base):
-    __tablename__ = "img_table"
+class PosterTable(Base):
+    __tablename__ = "poster_table"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(
         ForeignKey("anime_table.title", onupdate="CASCADE", ondelete="CASCADE"),
         index=True
     )
-    poster: Mapped[str] = mapped_column()
-    banner: Mapped[str | None] = mapped_column()
+    poster_uuid: Mapped[UUID] = mapped_column()
+    anime_rs: Mapped["AnimeTable"] = relationship(back_populates="poster_rs")
 
-    anime_rs: Mapped["AnimeTable"] = relationship(back_populates="img_rs")
+
+class BannerTable(Base):
+    __tablename__ = "banner_table"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(
+        ForeignKey("anime_table.title", onupdate="CASCADE", ondelete="CASCADE"),
+        index=True
+    )
+    uuid_banner: Mapped[UUID] = mapped_column()
+    anime_rs: Mapped["AnimeTable"] = relationship(back_populates="banner_rs")
