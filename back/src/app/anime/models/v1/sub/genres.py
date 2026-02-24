1bab168a01aa7912130a5990679dcb24b48d3923
bot <maximbotpy@gmail.com>
2026-02-24 20:36:56 +0300

    refactor: поменял тип poster на uuid remove: get_base64

--- back/src/app/anime/models/v1/sub/genres.py
+++ back/src/app/anime/models/v1/sub/genres.py
@@ -1,5 +1,5 @@
-import os
 from typing import TYPE_CHECKING
+from uuid import UUID
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -7,8 +7,6 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.app.anime.enums.v1.sub.genres import Genres
 from src.model import Base
 from src.utils.crud import crud
-from src.utils.logger import server_log
-from src.utils.utils import get_base64
 
 if TYPE_CHECKING:
     from src.app.anime.models.v1.main.genres_anime import GenresTable
@@ -22,7 +20,7 @@ class GenresSubTable(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     label: Mapped[str] = mapped_column(unique=True)
     value: Mapped[str] = mapped_column(unique=True)
-    poster: Mapped[str] = mapped_column()
+    uuid_poster: Mapped[UUID | None] = mapped_column()
 
     genres_anime_rs: Mapped["GenresTable"] = relationship(
         back_populates="genres_rs",
@@ -31,22 +29,12 @@ class GenresSubTable(Base):
 
     @classmethod
     async def fill(cls, session: AsyncSession):
-        path = f"{os.getcwd()}\\..\\front\\public\\.genres"
-        poster_base64 = await get_base64(path)
-
         for data in Genres:
-            try:
-                await crud.create(
-                    session=session,
-                    table=cls,
-                    data={
-                        "label": data.value.get("label"),
-                        "value": data.value.get("value"),
-                        "poster": poster_base64[data.value["label"]]
-                    }
-                )
-            except KeyError:
-                server_log.error(
-                    "Не удалось найти файл с именем %s.",
-                    data.value["label"]
-                )
+            await crud.create(
+                session=session,
+                table=cls,
+                data={
+                    "label": data.value.get("label"),
+                    "value": data.value.get("value"),
+                }
+            )
