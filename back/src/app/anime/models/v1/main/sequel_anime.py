f5272c1299a3f96bc243352719cfc2f9b0c21a0a
bot <maximbotpy@gmail.com>
2026-02-26 22:34:48 +0300

    переименовал таблицу

--- back/src/app/anime/models/v1/main/sequel_anime.py
+++ back/src/app/anime/models/v1/main/sequel_anime.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from uuid import UUID
 
 from sqlalchemy import ForeignKey, UniqueConstraint, func
 from sqlalchemy.orm import Mapped, mapped_column
@@ -7,16 +8,17 @@ from src.model import Base
 from src.utils.utils import get_unc_now
 
 
-class RelationAnime(Base):
-    __tablename__ = "relation_anime"
+class SequelTable(Base):
+    __tablename__ = "sequel_table"
 
     id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
+    uuid: Mapped[UUID] = mapped_column()
     title: Mapped[str] = mapped_column(
         ForeignKey("anime_table.title", onupdate="CASCADE", ondelete="CASCADE")
     )
-    date_add: Mapped[datetime] = mapped_column(default=get_unc_now, server_default=func.now())
-    relation_title: Mapped[str] = mapped_column(
+    sequel_title: Mapped[str] = mapped_column(
         ForeignKey("anime_table.title", onupdate="CASCADE", ondelete="CASCADE")
     )
+    date_add: Mapped[datetime] = mapped_column(default=get_unc_now, server_default=func.now())
 
-    __table_args__ = (UniqueConstraint("title", "relation_title"),)
+    __table_args__ = (UniqueConstraint("title", "sequel_title"),)
