ce1380300746a06336c03ee3b042a982c384f0f2
bot <maximbotpy@gmail.com>
2026-02-26 22:35:32 +0300

    переименовал таблицу

--- back/src/app/anime/models/v1/main/__init__.py
+++ back/src/app/anime/models/v1/main/__init__.py
@@ -3,17 +3,17 @@ from src.app.anime.models.v1.main.comment import (CommentTable,
                                                   ResponseCommentTable)
 from src.app.anime.models.v1.main.episode import EpisodeTable
 from src.app.anime.models.v1.main.genres_anime import GenresTable
-from src.app.anime.models.v1.main.img import ImgTable
+from src.app.anime.models.v1.main.img import BannerTable, PosterTable
 from src.app.anime.models.v1.main.rating import RatingTable
-from src.app.anime.models.v1.main.relation_anime import RelationAnime
 from src.app.anime.models.v1.main.release_day import ReleaseDayTable
 from src.app.anime.models.v1.main.schedule import ScheduleTable
+from src.app.anime.models.v1.main.sequel_anime import SequelTable
 
 __all__ = (
-    AnimeTable, ImgTable,
+    AnimeTable, PosterTable,
     EpisodeTable, GenresTable,
-    ScheduleTable, RelationAnime,
+    ScheduleTable, SequelTable,
     RatingTable, CommentTable,
     ResponseCommentTable,
-    ReleaseDayTable
+    ReleaseDayTable, BannerTable
 )
