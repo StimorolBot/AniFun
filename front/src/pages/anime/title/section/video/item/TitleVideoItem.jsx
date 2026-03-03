d27fd9d33ce0c202f602cc432554c6aeb94126d8
bot <maximbotpy@gmail.com>
2026-03-03 21:59:10 +0300

    refactor: поправил ссылку

--- front/src/pages/anime/title/section/video/item/TitleVideoItem.jsx
+++ front/src/pages/anime/title/section/video/item/TitleVideoItem.jsx
@@ -8,7 +8,7 @@ export const TitleVideoItem = memo(({item, imgData, ...props}) => {
 
     return(
         <li className="title-video__item" {...props}>
-            <Link className="title-video__link" to={`/anime/video/episode/${item.uuid}`}>
+            <Link className="title-video__link" to={`/anime/video/episode/${item.episode_uuid}`}>
                 <img className="title-video__preview" src={imgData} alt="preview" />
                 <div className="title-video__container">
                     <p className="title-video__name">
