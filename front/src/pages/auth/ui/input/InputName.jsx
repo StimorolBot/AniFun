7c2ffdae196d92c748e3a4c6967e11aab1d35afb
bot <maximbotpy@gmail.com>
2026-03-05 21:22:33 +0300

    refactor: обновил путь для svg

--- front/src/pages/auth/ui/input/InputName.jsx
+++ front/src/pages/auth/ui/input/InputName.jsx
@@ -8,14 +8,14 @@ export function InputName({register, errors, clickRef, watch}){
             minLength={4} maxLength={32} watch={watch}
             register={ register("user_name", {
                 required: true,
-                minLength: {value: 4, message: "Длинна поля должна быть от 8 символов"},
+                minLength: {value: 4, message: "Длинна поля должна быть от 4 символов"},
                 maxLength: {value: 32, message: "Длинна поля должна быть до 32 символов"},
                 pattern: {value: /^[a-zA-Z0-9_\-]+$/, message: "Неверный формат имени"}
             })}
             errorMsg={errors?.user_name?.message}
         >
             <svg className="auth__svg">
-                <use xlinkHref="/public/main.svg#login-svg"/>
+                <use xlinkHref="/public/svg/auth.svg#login-svg"/>
             </svg>
         </InputMainAuth>
     )
