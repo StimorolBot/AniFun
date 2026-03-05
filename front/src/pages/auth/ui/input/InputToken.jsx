9778355553ca45d864931347796df6f305a97b16
bot <maximbotpy@gmail.com>
2026-03-05 21:21:09 +0300

    refactor: вынес di в пропс

--- front/src/pages/auth/ui/input/InputToken.jsx
+++ front/src/pages/auth/ui/input/InputToken.jsx
@@ -1,20 +1,20 @@
 import { InputMainAuth } from "./InputMainAuth"
 
 
-export function InputToken({register, errors, clickRef, watch, labelTitle="Токен"}){
+export function InputToken({register, errors, clickRef, watch, id, labelTitle="Токен"}){
     
     return(
-        <InputMainAuth labelTitle={labelTitle} id={"email_token"} type={"text"} ref={clickRef}
+        <InputMainAuth labelTitle={labelTitle} id={id} type={"text"} ref={clickRef}
             minLength={8} maxLength={8} watch={watch}
-            register={ register("email_token", {
+            register={ register(id, {
                 required: true,
                 minLength: {value: 8, message: "Длинна поля должна быть 8 символов"},
                 maxLength: {value: 8, message: "Длинна поля должна быть 8 символов"},
             })}
-            errorMsg={errors?.email_token?.message}
+            errorMsg={errors?.[id]?.message}
         >
             <svg className="auth__svg">
-                <use xlinkHref="/public/main.svg#token-svg"/>
+                <use xlinkHref="/public/svg/auth.svg#token-svg"/>
             </svg>
         </InputMainAuth>
     )
