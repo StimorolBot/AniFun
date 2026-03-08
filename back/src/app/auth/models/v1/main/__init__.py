b749fc58626dc3191aab8b2d8efb66da4abb4bb6
bot <maximbotpy@gmail.com>
2026-03-08 23:18:58 +0300

    refactor: TokenTable -> BanTable

--- back/src/app/auth/models/v1/main/__init__.py
+++ back/src/app/auth/models/v1/main/__init__.py
@@ -1,4 +1,4 @@
 from src.app.auth.models.v1.main.auth import AuthTable
-from src.app.auth.models.v1.main.token import TokenTable
+from src.app.auth.models.v1.main.ban import BanTable
 
-__all__ = (AuthTable, TokenTable)
+__all__ = (AuthTable, BanTable)
