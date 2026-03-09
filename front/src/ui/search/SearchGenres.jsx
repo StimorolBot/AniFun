28ec29ab78b2066c99826083e9dc51369f289c5a
bot <maximbotpy@gmail.com>
2026-03-09 18:24:38 +0300

    feat: добавил запрос списка с жанрами

--- front/src/ui/search/SearchGenres.jsx
+++ front/src/ui/search/SearchGenres.jsx
@@ -1,46 +1,27 @@
+import { useQuery } from "@tanstack/react-query"
+
 import { CustomSelect } from "../../ui/input/CustomSelect"
+import { api } from "../../api"
+
+import "./style/search_genres.sass"
 
 
 export function SearchGenres({genres, setGenres, ...props}) {
-    const options  = [
-        {value: "Безумие", label: "Безумие"},
-        {value: "Боевые искусства", label: "Боевые искусства"},
-        {value: "Вампиры", label: "Вампиры"},
-        {value: "Военное", label: "Военное"},
-        {value: "Гарем", label: "Гарем"},
-        {value: "Демоны", label: "Демоны"},
-        {value: "Детектив", label: "Детектив"},
-        {value: "Детское", label: "Детское"},
-        {value: "Драма", label: "Драма"},
-        {value: "Игры", label: "Игры"},
-        {value: "Исторический", label: "Исторический"},
-        {value: "Исекай", label: "Исекай"},
-        {value: "Комедия", label: "Комедия"},
-        {value: "Магия", label: "Магия"},
-        {value: "Машины", label: "Машины"},
-        {value: "Меха", label: "Меха"},
-        {value: "Музыка", label: "Музыка"},
-        {value: "Пародия", label: "Пародия"},
-        {value: "Повседневность", label: "Повседневность"},
-        {value: "Приключения", label: "Приключения"},
-        {value: "Психологическое", label: "Психологическое"},
-        {value: "Романтика", label: "Романтика"},
-        {value: "Сверхъестественное", label: "Сверхъестественное"},
-        {value: "Спорт", label: "Спорт"},
-        {value: "Супер сила", label: "Супер сила"},
-        {value: "Триллер", label: "Триллер"},
-        {value: "Ужасы", label: "Ужасы"},
-        {value: "Фэнтези", label: "Фэнтези"},
-        {value: "Школа", label: "Школа"},
-        {value: "Экшен", label: "Экшен"},
-        {value: "Этти", label: "Этти"},
-    ]
-    
+    const {data: genresData} = useQuery({
+        queryKey: ["genres-data-list"],
+        staleTime: 1000 * 60 * 3,
+        queryFn: async () => {
+            return api.get("anime/genres/list").then(r => r.data)
+        } 
+    })
+
+
     return(   
         <CustomSelect
-            options={options}            
+            options={genresData}            
             value={genres}
             setValue={setGenres}
+            className="genres-select"
             {...props}
         />        
     )
