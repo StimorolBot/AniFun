85b728f7597bcedfce8807204d926e9886fb2663
bot <maximbotpy@gmail.com>
2026-03-09 14:32:31 +0300

    feat: подключил genres_router

--- back/src/include_router.py
+++ back/src/include_router.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from src.app.admin.page.anime.api_v1.router import admin_anime_router
 from src.app.admin.page.user.api_v1.router import admin_user_router
+from src.app.anime.page.genres.api_v1.router import genres_router
 from src.app.anime.page.home.api_v1.router import home_router
 from src.app.anime.page.release.api_v1.router import release_router
 from src.app.anime.page.title.api_v1.router import title_router
@@ -27,6 +28,7 @@ def include_router(app: FastAPI):
     app.include_router(home_router)
     app.include_router(release_router)
     app.include_router(title_router)
+    app.include_router(genres_router)
 
     app.include_router(comment_router)
     app.include_router(rating_router)
