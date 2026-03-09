83b2542b2b46360896a9f333663471f6a2c65e71
bot <maximbotpy@gmail.com>
2026-03-09 19:57:56 +0300

    feat: добавил рейтинг

--- front/src/pages/anime/release/item/ReleaseItem.jsx
+++ front/src/pages/anime/release/item/ReleaseItem.jsx
@@ -4,41 +4,44 @@ import { Link } from "react-router-dom"
 import "./style.sass"
 
 
-export const ReleaseItem = memo(({item}) => {
+export const ReleaseItem = memo(({item, imgData}) => {
 
     return(
         <li className="release__item">
            <div className="release__img-container">
-                <img src={`data:image/webp;base64,${item?.img_rs?.poster}`} alt="poster"/>
+                {item?.avg && 
+                    <div className="release__inner-rating">
+                        <div><p>{item.avg}</p></div>
+                    </div>
+                }
+                <img src={imgData} alt="poster" loading="lazy"/>
             </div>
             <div className="release__item-info">
-                <h3>
-                    {item?.title}
-                </h3>
+                <h3>{item.anime.title}</h3>
                 <div className="release__item-container">
                     <ul className="release__desc-list">
-                        {item?.genres_rs?.map((genre, index) => {
+                        {item.anime.genres?.map((genre, index) => {
                             return(
                                 <li key={index}>
-                                    <Link className="release_genre-link" to={`/anime/genres/${genre.genres}`}>
-                                        {genre.genres}
+                                    <Link className="release_genre-link" to={`/anime/genres/${genre.value}`}>
+                                        {genre.label}
                                     </Link>
                                 </li>
                             )
                         })}
                     </ul>
                     <ul className="release__desc-list">
-                        <li>{item?.year}</li>
-                        <li>{item?.season}</li>
-                        <li>{item?.type}</li>
-                        <li>{item?.age_restrict}</li>
+                        <li>{item.anime?.year}</li>
+                        <li>{item.anime?.season.label}</li>
+                        <li>{item.anime?.type.label}</li>
+                        <li>{item.anime?.age_restrict.label}</li>
                     </ul>
                 </div>
                 <p className="release__item-description">
-                    {item?.description}
+                    {item.anime.description}
                 </p>
                 <div className="release__link-container">
-                    <Link to={`/anime/${item?.alias}`}>
+                    <Link to={`/anime/${item.anime.alias}`}>
                         Смотреть
                     </Link>
                 </div>  
