47161f3496834a01c9bd4b15ae1045b3d6cfb28e
bot <maximbotpy@gmail.com>
2026-03-10 21:47:35 +0300

    feat: добавил компонент стандартной кнопки

---
+++ front/src/ui/btn/BtnDefault.jsx
@@ -0,0 +1,12 @@
+import { memo } from "react"
+
+import "./style/btn_default.sass"
+
+
+export const BtnDefault = memo (({children, callback, isStroke=true, ...props}) => {
+    return(
+        <button className="btn-default" data-stroke={isStroke} onClick={callback} {...props}>
+            {children}
+        </button>
+    )
+})
