d61ecf24f9ebb66e9a11104aeb0ce6f6bebb9b90
bot <maximbotpy@gmail.com>
2026-03-11 20:37:17 +0300

    refactor: поправил валидацию

--- back/src/app/anime/page/release/api_v1/schemas.py
+++ back/src/app/anime/page/release/api_v1/schemas.py
@@ -1,22 +1,29 @@
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, AliasChoices, Field
 
 from src.app.anime.enums.v1 import sub as sub_enums
-from src.app.anime.schemas.api_v1 import schemas
-from src.utils.valid import ValidDescription
+from src.app.anime.page.home.api_v1.schemas import ExtRespNewEpisodeDTO
+from src.utils.valid import ValidText, ValidNumber
 
 
-class ResponseReleaseDTO(schemas.ResponseAnimeDTO):
-    genres_rs: List[schemas.GenresDTO]
-    img_rs: schemas.PosterDTO
-    description: ValidDescription
+class ExtRespReleaseDTO(ExtRespNewEpisodeDTO):
+    description: ValidText[10, 1000]
+
+
+class ResponseReleaseDTO(BaseModel):
+    anime: ExtRespReleaseDTO = Field(
+        validation_alias=AliasChoices("AnimeTable", "anime")
+    )
+    avg: float | None = None
+    total_count: ValidNumber[1, 9999] | None = None
 
 
 class FilterData(BaseModel):
-    year: List[int] | None = None
-    type: List[sub_enums.Type] | None = None
-    season: List[sub_enums.Season] | None = None
-    status: List[sub_enums.Status] | None = None
-    age_restrict: List[sub_enums.Restrict] | None = None
+    title: ValidText[5, 150] | None = None
+    year: List[ValidNumber[1970, 2050]] | None = None
+    type: List[sub_enums.TypeLabel] | None = None
+    season: List[sub_enums.SeasonLabel] | None = None
+    status: List[sub_enums.StatusLabel] | None = None
+    age_restrict: List[sub_enums.RestrictLabel] | None = None
     genres: List[sub_enums.Genres] | None = None
