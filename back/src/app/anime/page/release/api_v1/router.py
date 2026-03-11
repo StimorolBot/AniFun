2c2b0c7de273e2ef2349943e2e8e2e4553cb3cf7
bot <maximbotpy@gmail.com>
2026-03-11 20:36:07 +0300

    feat: добавил подзапрос для получения рейтинга refactor: поправил get_filter_title

--- back/src/app/anime/page/release/api_v1/router.py
+++ back/src/app/anime/page/release/api_v1/router.py
@@ -1,35 +1,32 @@
 import json
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from fastapi_pagination import Page
 from fastapi_pagination.ext.sqlalchemy import paginate
-from sqlalchemy import or_, select
-from sqlalchemy.exc import ArgumentError
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
 from src.app.anime.models.v1 import main as main_table
+from src.app.anime.page.home.api_v1.query import query_main_info
 from src.app.anime.page.release.api_v1 import schemas
 from src.database.session import get_async_session
-from src.utils.logger import anime_log
+from src.app.anime.subquery.v_1 import subquery
 
-release_router = APIRouter(prefix="/anime/release", tags=["release"])
+release_router = APIRouter(prefix="/anime/releases", tags=["release"])
 
 
 @release_router.get("/", status_code=status.HTTP_200_OK, summary="Получить список релизов")
-async def get_catalog(session: AsyncSession = Depends(get_async_session)) -> Page[schemas.ResponseReleaseDTO]:
+async def get_releases(session: AsyncSession = Depends(get_async_session)) -> Page[schemas.ResponseReleaseDTO]:
+    sq = subquery.subquery_rating_all_users()
     query = (
-        select(main_table.AnimeTable)
-        .join(main_table.AnimeTable.img_rs)
-        .options(selectinload(main_table.AnimeTable.img_rs))
+        query_main_info(sq)
+        .join(sq, main_table.AnimeTable.title == sq.c.title, isouter=True)
+        .options(selectinload(main_table.AnimeTable.poster_rs))
         .join(main_table.AnimeTable.genres_rs)
-        .options(selectinload(main_table.AnimeTable.genres_rs))
-        .select_from(main_table.ImgTable)
-        .distinct()
         .order_by(main_table.AnimeTable.date_add.desc())
     )
     return await paginate(session, query)
-
 
 
 @release_router.get(
@@ -41,42 +38,53 @@ async def get_filter_title(
         data: str,
         session: AsyncSession = Depends(get_async_session)
 ) -> Page[schemas.ResponseReleaseDTO]:
-    valid_data = schemas.FilterData.model_validate(json.loads(data))
-    try:
-        query = (
-            select(main_table.AnimeTable)
-            .filter(or_(
-                main_table.AnimeTable.type.in_([i.value for i in valid_data.type])
-                if valid_data.type
-                else None,
-                main_table.AnimeTable.age_restrict.in_([i.value for i in valid_data.age_restrict])
-                if valid_data.age_restrict
-                else None,
-                main_table.AnimeTable.season.in_([i.value for i in valid_data.season])
-                if valid_data.season
-                else None,
-                main_table.AnimeTable.status.in_([i.value for i in valid_data.status])
-                if valid_data.status
-                else None,
-                main_table.AnimeTable.year.between(valid_data.year[0], valid_data.year[1])
-                if valid_data.year
-                else None,
-                main_table.GenresAnimeTable.genres.in_([i.value for i in valid_data.genres])
-                if valid_data.genres
-                else None
-            ))
-            .join(main_table.AnimeTable.img_rs)
-            .options(selectinload(main_table.AnimeTable.img_rs))
-            .join(main_table.AnimeTable.genres_rs)
-            .options(selectinload(main_table.AnimeTable.genres_rs))
-            .select_from(main_table.ImgTable)
-            .distinct()
-            .order_by(main_table.AnimeTable.date_add.desc())
+    valid_data: schemas.FilterData = schemas.FilterData.model_validate(json.loads(data))
+    filter_query_list = []
+
+    if valid_data.type:
+        filter_query_list.append(
+            main_table.AnimeTable.type.in_([i.value for i in valid_data.type])
         )
-        return await paginate(session, query)
-    except ArgumentError as e:
-        anime_log.warning("При попытке получить отфильтрованный список релизов произошла ошибка: %s", e)
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Не удалось получить список релизов, пожалуйста, повторите попытку позже."
-        ) from e
+
+    if valid_data.age_restrict:
+        filter_query_list.append(
+            main_table.AnimeTable.age_restrict.in_(
+                [i.value for i in valid_data.age_restrict]
+            )
+        )
+
+    if valid_data.season:
+        filter_query_list.append(
+            main_table.AnimeTable.season.in_([i.value for i in valid_data.season])
+        )
+
+    if valid_data.status:
+        filter_query_list.append(
+            main_table.AnimeTable.status.in_([i.value for i in valid_data.status])
+        )
+
+    if valid_data.year:
+        filter_query_list.append(
+            main_table.AnimeTable.year.between(
+                valid_data.year[0],
+                valid_data.year[1],
+            )
+        )
+
+    if valid_data.title and len(valid_data.title) > 5:
+        filter_query_list.append(
+            main_table.AnimeTable.title.ilike(f"%{valid_data.title}%")
+        )
+
+    sq = subquery.subquery_rating_all_users()
+
+    query = (
+        query_main_info(sq)
+        .join(sq, main_table.AnimeTable.title == sq.c.title, isouter=True)
+        .options(selectinload(main_table.AnimeTable.poster_rs))
+        .join(main_table.AnimeTable.genres_rs)
+        .filter(*filter_query_list)
+        .order_by(main_table.AnimeTable.date_add.desc())
+    )
+
+    return await paginate(session, query)
