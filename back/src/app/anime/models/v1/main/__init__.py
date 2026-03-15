579a7e7863573a4a57abb14db29dd5e8b97c935f
bot <maximbotpy@gmail.com>
2026-03-15 16:51:02 +0300

    feat: добавил таблицу EpisodePreviewTable

--- back/src/app/anime/models/v1/main/__init__.py
+++ back/src/app/anime/models/v1/main/__init__.py
@@ -1,7 +1,8 @@
 from src.app.anime.models.v1.main.anime import AnimeTable
 from src.app.anime.models.v1.main.comment import (CommentTable,
                                                   ResponseCommentTable)
-from src.app.anime.models.v1.main.episode import EpisodeTable
+from src.app.anime.models.v1.main.episode import (EpisodePreviewTable,
+                                                  EpisodeTable)
 from src.app.anime.models.v1.main.genres_anime import GenresTable
 from src.app.anime.models.v1.main.img import BannerTable, PosterTable
 from src.app.anime.models.v1.main.rating import RatingTable
@@ -12,6 +13,7 @@ from src.app.anime.models.v1.main.sequel_anime import SequelTable
 __all__ = (
     AnimeTable, PosterTable,
     EpisodeTable, GenresTable,
+    EpisodePreviewTable,
     ScheduleTable, SequelTable,
     RatingTable, CommentTable,
     ResponseCommentTable,
