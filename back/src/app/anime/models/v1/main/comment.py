54237622405131e3a1267bef5c71eadba46f9635
bot <maximbotpy@gmail.com>
2026-03-19 22:16:05 +0300

    remove: удалил таблицу ResponseCommentTable

--- back/src/app/anime/models/v1/main/comment.py
+++ back/src/app/anime/models/v1/main/comment.py
@@ -1,15 +1,11 @@
 from datetime import datetime
-from typing import TYPE_CHECKING, List
 from uuid import UUID
 
 from sqlalchemy import ForeignKey, UniqueConstraint, func
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column
 
 from src.model import Base
 from src.utils.utils import generate_uuid, get_unc_now
-
-if TYPE_CHECKING:
-    from src.app.auth.models.v1.main.auth import AuthTable
 
 
 class CommentTable(Base):
@@ -26,42 +22,23 @@ class CommentTable(Base):
     content: Mapped[str] = mapped_column()
     date_add: Mapped[datetime] = mapped_column(default=get_unc_now, server_default=func.now())
 
-    response_rs: Mapped[List["ResponseCommentTable"]] = relationship(back_populates="comment_rs")
+    response_comment_uuid: Mapped[UUID | None] = mapped_column(ForeignKey("comment_table.uuid"))
+    response_author_uuid: Mapped[UUID | None] = mapped_column(ForeignKey("auth_table.uuid"))
 
 
-class ResponseCommentTable(Base):
-    """Таблица для хранения ответов на комментарии"""
+class ReactionCommentTable(Base):
+    """Таблица с реакциями комментариев"""
 
-    __tablename__ = "response_commet_table"
+    __tablename__ = "reaction_comment_table"
 
-    uuid: Mapped[UUID] = mapped_column(primary_key=True, default=generate_uuid, unique=True)
+    id: Mapped[int] = mapped_column(unique=True, autoincrement=True, primary_key=True)
     title: Mapped[str] = mapped_column(
         ForeignKey("anime_table.title", onupdate="CASCADE", ondelete="CASCADE"),
         index=True
     )
-    author_uuid: Mapped[UUID] = mapped_column(ForeignKey("auth_table.uuid"))
-    content: Mapped[str] = mapped_column()
-    date_add: Mapped[datetime] = mapped_column(default=get_unc_now)
-
-    response_uuid_comment: Mapped[UUID] = mapped_column(ForeignKey("comment_table.uuid"))
-    response_uuid_author: Mapped[UUID] = mapped_column(ForeignKey("auth_table.uuid"))
-
-    comment_rs: Mapped["CommentTable"] = relationship(back_populates="response_rs")
-    auth_rs: Mapped["AuthTable"] = relationship(
-        back_populates="response_rs",
-        foreign_keys=[response_uuid_author],
-    )
-
-
-class CommentRatingTable(Base):
-    """Таблица с рейтингом комментариев"""
-
-    __tablename__ = "comment_rating_table"
-
-    id: Mapped[int] = mapped_column(unique=True, autoincrement=True, primary_key=True)
-
     uuid: Mapped[UUID] = mapped_column(ForeignKey("comment_table.uuid"))
     author_uuid: Mapped[UUID] = mapped_column(ForeignKey("auth_table.uuid"))
-    rating: Mapped[int] = mapped_column()
+    reaction_type: Mapped[str] = mapped_column()
 
-    __table_args__ = (UniqueConstraint("uuid", "author_uuid", "rating"),)
+    __table_args__ = (UniqueConstraint("uuid", "author_uuid"),)
+
