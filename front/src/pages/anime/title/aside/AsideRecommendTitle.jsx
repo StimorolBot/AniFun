3393441cc0a8a8debd2b8a686a2cf6804f35d50d
bot <maximbotpy@gmail.com>
2026-03-21 17:11:42 +0300

    refactor: изменил запросы

--- front/src/pages/anime/title/aside/AsideRecommendTitle.jsx
+++ front/src/pages/anime/title/aside/AsideRecommendTitle.jsx
@@ -1,68 +1,72 @@
-import { memo, useEffect, useState } from "react"
+import { memo } from "react"
+import { useQueries, useQuery } from "@tanstack/react-query"
 
 import { api } from "../../../../api"
-import { useFetch } from "../../../../hook/useFetch"
 
 import { AsideNewEpisodeItem } from "./item/AsideNewEpisodeItem"
 import { AsideRecTitleItem } from "./item/AsideRecTitleItem"
 import { Loader } from "../../../../components/loader/Loader"
+import { 
+    asideRecTitleCache, 
+    asideRecTitleImgCache, 
+    asideNewEpisodeCache, 
+    asideNewEpisodeImgCache 
+} from "../../../../query_key"
 
 import "./style.sass"
 
 
 export const AsideRecommendTitle = memo(() => {
-    const [responseNewEpisode, setResponseNewEpisode] = useState([{
-        "episode_number": null,    
-        "alias": null,
-        "title": null,
-        "year": null,
-        "type": null,
-        "age_restrict": null,
-        "season": null,
-        "genres":[""],
-        "poster": null
-    }])
     
-    const [responseRecTitle, setResponseRecTitle] = useState([{
-        "alias": null,
-        "title": null,
-        "year": null,
-        "type": null,
-        "age_restrict": null,
-        "season": null,
-        "poster": null
-    }])
+    const {data: recTitleData, isLoading: isLoadingRecTitle} = useQuery({
+        queryKey: [asideRecTitleCache],
+        staleTime: 1000 * 60 * 3,
+        retry: false,
+        queryFn: async () => {
+            return api.get("anime/recommend-title").then(r => r.data) 
+        },
+        placeholderData: []
+    })
 
-    const [requestNewTitle, isLoadingNewTitle, errorNewTitle] = useFetch(
-        async () => {
-            await api.get("/new-episode", {params: {"limit": 3}}).then((r) => setResponseNewEpisode(r.data))
-        }
-    )
-    
-    const [requestRecTitle, isLoadingRecTitle, errorRecTitle] = useFetch(
-        async () => {
-            await api.get("/anime/releases/recommend-title").then((r) => setResponseRecTitle(r.data))
-        }
-    )
+    const imgRecData = useQueries({
+        queries: recTitleData?.map(item => ({
+        queryKey: [asideRecTitleImgCache, item.poster_uuid],
+        staleTime: 1000 * 60 * 3,
+        queryFn: async () => {
+            return await api.get(`/s3/anime-${item.uuid}/${item.poster_uuid}`).then(r => r.data)
+        }}))
+    })
 
-    useEffect(() => {(
-        async () => {
-            await requestNewTitle()
-            await requestRecTitle()
-        })()
-    }, [])
+    const {data: newEpisodeData, isLoading: isLoadingNewEpisode} = useQuery({
+        queryKey: [asideNewEpisodeCache],
+        staleTime: 1000 * 60 * 3,
+        retry: false,
+        queryFn: async () => {
+            return api.get("/new-episode", {params: {"limit": 3}}).then(r => r.data) 
+        },
+        placeholderData: []
+    })
+
+    const imgNewEpisodeData = useQueries({
+        queries: newEpisodeData?.map(item => ({
+        queryKey: [asideNewEpisodeImgCache, item.anime.poster.poster_uuid],
+        staleTime: 1000 * 60 * 3,
+        queryFn: async () => {
+            return await api.get(`/s3/anime-${item.anime.uuid}/${item.anime.poster.poster_uuid}`).then(r => r.data)
+        }}))
+    })
     
-    
+
     return(
         <aside className="aside-rec">
             <div className="aside-rec__wrapper">
                 <div className="aside-rec__new-title">
                     <h3>Новые эпизоды</h3>
-                    {isLoadingNewTitle
+                    {isLoadingNewEpisode
                         ? <Loader/>
                         : <ul>
-                            {responseNewEpisode?.map((item, index) => {
-                            return <AsideNewEpisodeItem item={item} key={index}/>
+                            {newEpisodeData?.map((item, index) => {
+                            return <AsideNewEpisodeItem item={item} imgData={imgNewEpisodeData[index].data} key={index}/>
                         })}
                         </ul>
                     }
@@ -72,8 +76,8 @@ export const AsideRecommendTitle = memo(() => {
                     {isLoadingRecTitle
                         ? <Loader/>
                         : <ul className="rec-title__list">                     
-                            {responseRecTitle?.map((item, index) => {
-                                return <AsideRecTitleItem item={item} key={index}/>
+                            {recTitleData?.map((item, index) => {
+                                return <AsideRecTitleItem item={item} imgData={imgRecData[index].data} key={index}/>
                             })}
                         </ul>
                     }
