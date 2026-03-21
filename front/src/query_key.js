e8191d2252fcc4ce296b78bef4c759ac0b94ac9f
bot <maximbotpy@gmail.com>
2026-03-21 16:23:45 +0300

    feta: добавил ключи для хранения кеша

---
+++ front/src/query_key.js
@@ -0,0 +1,15 @@
+// HomePage
+export const sliderCache= "sliderCache"
+export const sliderBannerCache = "sliderBannerCache"
+
+export const newEpisodeCache = "newEpisodeCache"
+export const newEpisodePosterCache = "newEpisodePosterCache"
+
+export const scheduleCache = "scheduleCache"
+export const schedulePosterCache = "schedulePosterCache"
+
+export const franchisesCache = "franchisesCache"
+export const franchisesPosterCache = "franchisesPoster"
+
+export const genresCache = "genresCache"
+export const genresPosterCache = "genresPosterCache"
