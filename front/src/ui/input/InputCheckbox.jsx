e2843ef703bdd0438a5dc30836587c962f37eb7a
bot <maximbotpy@gmail.com>
2026-03-28 19:31:20 +0300

    feat: добавил компонент Checkbox

---
+++ front/src/ui/input/InputCheckbox.jsx
@@ -0,0 +1,13 @@
+import { memo } from "react"
+
+import "./style/input_checkbox.sass"
+
+
+export const InputCheckbox = memo(({id, callback}) => {
+    return(
+        <div className="input-checkbox__container">
+            <input className="input-checkbox" id={id} type="checkbox" onChange={(e) => callback(e)}/>
+            <label className="input-checkbox__label" htmlFor={id}/>
+        </div>
+    )  
+})
