ad07464a2219be8ab3e713bb7c45c003fb0bcb5f
bot <maximbotpy@gmail.com>
2026-03-29 22:20:40 +0300

    feat: добавил пропсы

--- front/src/ui/input/InputCheckbox.jsx
+++ front/src/ui/input/InputCheckbox.jsx
@@ -3,10 +3,10 @@ import { memo } from "react"
 import "./style/input_checkbox.sass"
 
 
-export const InputCheckbox = memo(({id, callback}) => {
+export const InputCheckbox = memo(({id, callback, ...props}) => {
     return(
         <div className="input-checkbox__container">
-            <input className="input-checkbox" id={id} type="checkbox" onChange={(e) => callback(e)}/>
+            <input className="input-checkbox" id={id} type="checkbox" onChange={(e) => callback(e)} {...props}/>
             <label className="input-checkbox__label" htmlFor={id}/>
         </div>
     )  
