ca2f54417ae1ba8c505bb010a8760545d10bdba8
bot <maximbotpy@gmail.com>
2026-04-04 21:33:05 +0300

    feat: добавил файл с функциями для панели управления видео

---
+++ front/src/pages/anime/video/controls/utils.js
@@ -0,0 +1,30 @@
+export const handlePip = (ref) => {
+    if (document.pictureInPictureElement)
+        document.exitPictureInPicture()
+    else
+        ref.current.requestPictureInPicture()
+}
+
+export const handleToggleMute = (volume, setVideoSettings, player) => {
+    if (volume != 0)
+        setVideoSettings(s => ({...s, "volume": 0}))
+    else
+        setVideoSettings(s => ({...s, "volume": player.volume()}))
+}
+
+export const handleKeyPress = (event, ref, setIsPlaying, player, volume, setVideoSettings, containerRef) => {
+    switch (event.code){
+        case "Space":
+            return handlerTogglePlay(ref, setIsPlaying)
+        case "KeyF":
+            return handleFullScreen(containerRef)
+        case "ArrowRight":
+            return handleSkipVideo(player, 15)
+        case "ArrowLeft":
+            return handleSkipVideo(player, -15)
+        case "KeyM":
+            return handleToggleMute(volume, setVideoSettings, player)
+        case "KeyP":
+            return handlePip(ref) 
+    }
+}
