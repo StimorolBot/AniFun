3cd6dfed1b533f91053f6bcf45a89438a20bc0b4
bot <maximbotpy@gmail.com>
2026-04-05 20:37:09 +0300

    прокинул дополнительные пропсы

--- front/src/pages/anime/video/controls/utils.js
+++ front/src/pages/anime/video/controls/utils.js
@@ -1,7 +1,7 @@
 import { handlerTogglePlay, handleFullScreen } from "../player/utils"
 
  
-export const handlePip = (ref) => {fff
+export const handlePip = (ref) => {
     if (document.pictureInPictureElement)
         document.exitPictureInPicture()
     else
@@ -15,10 +15,16 @@ export const handleToggleMute = (volume, setVideoSettings, player) => {
         setVideoSettings(s => ({...s, "volume": player.volume()}))
 }
 
-export const handleKeyPress = (event, ref, setIsPlaying, player, volume, setVideoSettings, containerRef) => {
+export const handleKeyPress = (
+    event, 
+    ref, containerRef, 
+    player, 
+    volume, uuid, alias, 
+    setVideoSettings, setIsPlaying
+) => {
     switch (event.code){
         case "Space":
-            return handlerTogglePlay(ref, setIsPlaying)
+            return handlerTogglePlay(ref, alias, uuid, setIsPlaying)
         case "KeyF":
             return handleFullScreen(containerRef)
         case "ArrowRight":
