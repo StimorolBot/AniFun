cf7dded24d3bcc366d4253f4b59884a1501e692d
bot <maximbotpy@gmail.com>
2026-04-06 21:25:17 +0300

    refactor: поправил валидацию полей

--- back/src/app/auth/base/api_v1/schemas.py
+++ back/src/app/auth/base/api_v1/schemas.py
@@ -1,8 +1,30 @@
+from typing import Annotated
+
 from fastapi import HTTPException, status
-from pydantic import BaseModel, ConfigDict, EmailStr, field_validator
+from pydantic import (BaseModel, ConfigDict, EmailStr,
+                      ValidatorFunctionWrapHandler, WrapValidator,
+                      field_validator)
 from pydantic_core.core_schema import FieldValidationInfo
 
-from src.utils.valid import ValidName, ValidPassword
+from src.utils.valid import ValidText, valid_forbidden_char, valid_len
+
+
+def valid_password(password: str, handler: ValidatorFunctionWrapHandler) -> str:
+    valid_len(password, 8, 32, "Пароль")
+    valid_forbidden_char(password, handler)
+    return password
+
+
+def valid_name(name: str, handler: ValidatorFunctionWrapHandler) -> str:
+    valid_len(name, 4, 20, "Имя")
+    if name.isalnum() is False:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Поле может содержать только буквы и цифры")
+
+    return name
+
+
+ValidName = Annotated[str, WrapValidator(valid_name)]
+ValidPassword = Annotated[str, WrapValidator(valid_password)]
 
 
 class Register(BaseModel):
@@ -22,8 +44,8 @@ class Register(BaseModel):
 
 
 class VerifyToken(BaseModel):
-    recaptcha_token: str
-    identifier_token: str
+    recaptcha_token: ValidText[127, 129]
+    identifier_token: ValidText[7, 9]
 
 
 class ResetPassword(VerifyToken):
