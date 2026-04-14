533b49b6181695fb61d27963f6d7e7a6474cd9ef
bot <maximbotpy@gmail.com>
2026-04-14 20:10:36 +0300

    feat: добавил компонент для добавления постера

---
+++ front/src/pages/root/anime/section/media/item/CreatePoster.jsx
@@ -0,0 +1,82 @@
+import { memo, useRef, useState } from "react"
+
+import { useForm } from "react-hook-form"
+import { useQuery } from "@tanstack/react-query"
+
+import { api } from "../../../../../../api"
+import { rootAnimePoster } from "../../../../query_key"
+
+import { InputTitle } from "../../../../ui/Input/InputTitle"
+import { BtnDefault } from "../../../../../../ui/btn/BtnDefault"
+
+import { Loader } from "../../../../../../components/loader/Loader"
+import { InputDragAndDrop } from "../../../../../../ui/input/InputDragAndDrop"
+import { alertHandler } from "../../../../../../utils/utils"
+
+
+export const CreatePoster = memo(({isShowAlert, setUpdateAlert, setAlertData}) => {
+    const formData = new FormData()
+    const [payloadFile, setPayloadFile] = useState()
+    
+    const {register, handleSubmit, formState: {errors, isValid}} = useForm({"mode": "onChange"})
+    
+    const formDataRef = useRef()
+    const {isFetching, refetch, error} = useQuery({
+        queryKey: [rootAnimePoster.create],
+        retry: 1,
+        enabled: false,
+        queryFn: async () => {
+            formData.append("file", payloadFile.file)
+            return api.post("/admin/anime/add-poster-title", formData,
+                {
+                    "params" : formDataRef.current,
+                    "headers": {"Content-Type": "multipart/form-data"}
+                }
+            ).then(r => r)
+        }
+    }) 
+
+    const onSubmit = async (data) => {
+        formDataRef.current = data
+        const response = await refetch()
+
+        await alertHandler(response, error, setAlertData, setUpdateAlert)
+    }  
+
+    return(
+        <form
+            className="root-anime__item"
+            id="root-anime-add-poster"
+            onSubmit={handleSubmit(onSubmit)}    
+        >
+            <h2 className="root-anime__title">
+                Добавить постер
+            </h2>
+            <InputTitle
+                id={"root-poster-create"}
+                placeholder="Название"
+                required
+                errorMsg={errors?.title?.message}
+                register={register}
+                param={"title"}
+            />
+            <InputDragAndDrop
+                id={"root-anime-add-poster-file"}
+                setPayloadFile={setPayloadFile}
+                paramName={"file"}
+            />
+            <div className="root-anime__btn-container">
+                {isFetching
+                    ? <Loader size={"small"}/>
+                    : <BtnDefault
+                        type="submit"
+                        form="root-anime-add-poster"
+                        disabled={!isValid || isShowAlert}
+                    >
+                        Добавить
+                    </BtnDefault>
+                }
+            </div>
+        </form>
+    )
+}) 
