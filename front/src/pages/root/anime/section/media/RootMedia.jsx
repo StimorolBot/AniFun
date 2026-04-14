3e5c7e2f84108eb2333cc7b78719b62048450580
bot <maximbotpy@gmail.com>
2026-04-14 20:06:14 +0300

    feat: добавил компонент для редактирования постеров/баннеров

---
+++ front/src/pages/root/anime/section/media/RootMedia.jsx
@@ -0,0 +1,47 @@
+import { memo } from "react"
+import Masonry from "react-masonry-css"
+
+import { CreateBanner } from "./item/CreateBanner"
+import { CreatePoster } from "./item/CreatePoster"
+import { UpdateBanner } from "./item/UpdateBanner"
+import { UpdatePoster } from "./item/UpdatePoster"
+import { DeleteBanner } from "./item/DeleteBanner"
+ 
+
+export const RootMedia = memo(({isShowAlert, setUpdateAlert, setAlertData, breakpoints}) => {
+    return(
+        <section className="root-anime__section">
+            <Masonry
+                className="masonry-root"
+                breakpointCols={breakpoints} 
+                columnClassName="masonry__column-root"
+            >    
+                <CreateBanner
+                    isShowAlert={isShowAlert}
+                    setUpdateAlert={setUpdateAlert} 
+                    setAlertData={setAlertData}
+                />
+                <CreatePoster
+                    isShowAlert={isShowAlert}
+                    setUpdateAlert={setUpdateAlert} 
+                    setAlertData={setAlertData}
+                />
+                <UpdateBanner
+                    isShowAlert={isShowAlert}
+                    setUpdateAlert={setUpdateAlert} 
+                    setAlertData={setAlertData}
+                />
+                <UpdatePoster
+                    isShowAlert={isShowAlert}
+                    setUpdateAlert={setUpdateAlert} 
+                    setAlertData={setAlertData}
+                />
+                <DeleteBanner
+                    isShowAlert={isShowAlert}
+                    setUpdateAlert={setUpdateAlert} 
+                    setAlertData={setAlertData}
+                />
+            </Masonry>
+        </section>
+    )
+})
