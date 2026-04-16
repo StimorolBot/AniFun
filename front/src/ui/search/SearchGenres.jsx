6ace77cf7c35811df2ea8c5a60509c0e949e094c
bot <maximbotpy@gmail.com>
2026-04-16 20:07:44 +0300

    применил prettier

--- front/src/ui/search/SearchGenres.jsx
+++ front/src/ui/search/SearchGenres.jsx
@@ -1,28 +1,29 @@
 import { useQuery } from "@tanstack/react-query"
 
 import { CustomSelect } from "../../ui/input/CustomSelect"
+
 import { api } from "../../api"
 
 import "./style/search_genres.sass"
 
+export const SearchGenres = ({ genres, setGenres, ...props }) => {
+	const { data: genresData } = useQuery({
+		queryKey: ["genres-data-list"],
+		staleTime: 1000 * 60 * 3,
+		queryFn: async () => {
+			return api.get("anime/genres/list").then((r) => r.data)
+		},
+	})
 
-export function SearchGenres({genres, setGenres, ...props}) {
-    const {data: genresData} = useQuery({
-        queryKey: ["genres-data-list"],
-        staleTime: 1000 * 60 * 3,
-        queryFn: async () => {
-            return api.get("anime/genres/list").then(r => r.data)
-        } 
-    })
-
-
-    return(   
-        <CustomSelect
-            options={genresData}            
-            value={genres}
-            setValue={setGenres}
-            className="genres-select"
-            {...props}
-        />        
-    )
+	return (
+		<CustomSelect
+			options={genresData}
+			value={genres}
+			setValue={setGenres}
+			className="genres-select"
+			isMulti={true}
+			paramName={"genres"}
+			{...props}
+		/>
+	)
 }
