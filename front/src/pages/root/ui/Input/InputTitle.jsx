5455097bac8658568528a0d03e5415293c1954cf
bot <maximbotpy@gmail.com>
2026-04-16 20:05:20 +0300

    применил prettier

--- front/src/pages/root/ui/Input/InputTitle.jsx
+++ front/src/pages/root/ui/Input/InputTitle.jsx
@@ -2,23 +2,27 @@ import { memo } from "react"
 
 import { InputDefault } from "../../../../ui/input/InputDefault"
 
-
-export const InputTitle = memo(({id, errorMsg, register, param, ...props}) => {
-    return(
-        <InputDefault
-            id={id}
-            minLength={3} 
-            maxLength={150}
-            {...props}
-            errorMsg={errorMsg}
-            register={register(param, {
-                "minLength": {"value": 3, "message": "Название от 3 символов"},
-                "maxLength": {"value": 150, "message": "Название до 150 символов"},
-                "pattern": {
-                    "value": /(^[a-zA-Zа-яёА-ЯЁ0-9][a-zA-Zа-яёА-ЯЁ0-9\s]*$)/, 
-                    "message": "Некорректное название"
-                }
-            })}
-        />
-    )
-})
+export const InputTitle = memo(
+	({ id, errorMsg, register, param, ...props }) => {
+		return (
+			<InputDefault
+				id={id}
+				minLength={3}
+				maxLength={150}
+				{...props}
+				errorMsg={errorMsg}
+				register={register(param, {
+					minLength: { value: 3, message: "Название от 3 символов" },
+					maxLength: {
+						value: 150,
+						message: "Название до 150 символов",
+					},
+					pattern: {
+						value: /(^[a-zA-Zа-яёА-ЯЁ0-9][a-zA-Zа-яёА-ЯЁ0-9\s]*$)/,
+						message: "Некорректное название",
+					},
+				})}
+			/>
+		)
+	},
+)
