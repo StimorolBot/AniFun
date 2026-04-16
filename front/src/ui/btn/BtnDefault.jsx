92279efd10d33922c9da16f3925ec43114b46405
bot <maximbotpy@gmail.com>
2026-04-16 20:02:10 +0300

    применил prettier

--- front/src/ui/btn/BtnDefault.jsx
+++ front/src/ui/btn/BtnDefault.jsx
@@ -2,11 +2,17 @@ import { memo } from "react"
 
 import "./style/btn_default.sass"
 
-
-export const BtnDefault = memo (({children, callback, isStroke=true, ...props}) => {
-    return(
-        <button className="btn-default" data-stroke={isStroke} onClick={callback} {...props}>
-            {children}
-        </button>
-    )
-})
+export const BtnDefault = memo(
+	({ children, callback, isStroke = true, ...props }) => {
+		return (
+			<button
+				className="btn-default"
+				data-stroke={isStroke}
+				onClick={callback}
+				{...props}
+			>
+				{children}
+			</button>
+		)
+	},
+)
