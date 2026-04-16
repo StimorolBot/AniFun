e0d229196dea65689e732b1af9eb7567cb8daa2a
bot <maximbotpy@gmail.com>
2026-04-16 20:16:47 +0300

    применил prettier

--- front/src/pages/root/anime/section/sequel/item/CreateSequel.jsx
+++ front/src/pages/root/anime/section/sequel/item/CreateSequel.jsx
@@ -1,73 +1,82 @@
 import { memo, useRef } from "react"
-
 import { useForm } from "react-hook-form"
+
 import { useQuery } from "@tanstack/react-query"
 
-import { api } from "../../../../../../api"
-
-import { InputTitle } from "../../../../ui/Input/InputTitle"
-import { alertHandler } from "../../../../../../utils/utils"
-import { Loader } from "../../../../../../components/loader/Loader"
 import { BtnDefault } from "../../../../../../ui/btn/BtnDefault"
 
+import { InputTitle } from "../../../../ui/Input/InputTitle"
 
-export const CreateSequel = memo(({isShowAlert, setUpdateAlert, setAlertData}) => {
-    const formDataRef = useRef()
-    const {register, handleSubmit, formState: {errors, isValid}} = useForm({"mode": "onChange"})
+import { Loader } from "../../../../../../components/loader/Loader"
 
-     const {isFetching, refetch, error} = useQuery({
-        queryKey: ["rootAnimeTitle.create"],
-        retry: 1,
-        enabled: false,
-        queryFn: async () => {
-            return api.post("/admin/anime/add-sequel-title", formDataRef.current).then(r => r)
-        }
-    })
-    
-    const onSubmit = async (data) => {
-        formDataRef.current = data
-        const response = await refetch()
-        
-        await alertHandler(response, error, setAlertData, setUpdateAlert)
-    }
+import { api } from "../../../../../../api"
+import { alertHandler } from "../../../../../../utils/utils"
+import { rootAnimeSequel } from "../../../../query_key"
 
-    return(
-          <form
-            className="root-anime__item"
-            id="root-anime-form-create-sequel"
-            onSubmit={handleSubmit(onSubmit)}
-        >
-            <h2 className="root-anime__title">
-                Добавить
-            </h2>
-            <InputTitle
-                id={"root-sequel-title-create"}
-                placeholder="Текущий тайтл"
-                required
-                errorMsg={errors?.title?.message}
-                register={register}
-                param={"title"}
-            />
-            <InputTitle
-                id={"root-sequel-name-create"}
-                placeholder="Название сиквела / приквела"
-                required
-                errorMsg={errors?.sequel_title?.message}
-                register={register}
-                param={"sequel_title"}
-            />
-            <div className="root-anime__btn-container">
-                {isFetching
-                    ? <Loader size={"small"}/>
-                    : <BtnDefault
-                        type="submit"
-                        form="root-anime-form-create-sequel"
-                        disabled={!isValid || isShowAlert}
-                    >
-                        Добавить
-                    </BtnDefault>
-                }
-            </div>  
-        </form>
-    )
-})
+export const CreateSequel = memo(
+	({ isShowAlert, setUpdateAlert, setAlertData }) => {
+		const formDataRef = useRef()
+		const {
+			register,
+			handleSubmit,
+			formState: { errors, isValid },
+		} = useForm({ mode: "onChange" })
+
+		const { isFetching, refetch, error } = useQuery({
+			queryKey: [rootAnimeSequel.create],
+			retry: 1,
+			enabled: false,
+			queryFn: async () => {
+				return api
+					.post("/admin/anime/add-sequel-title", formDataRef.current)
+					.then((r) => r)
+			},
+		})
+
+		const onSubmit = async (data) => {
+			formDataRef.current = data
+			const response = await refetch()
+
+			await alertHandler(response, error, setAlertData, setUpdateAlert)
+		}
+
+		return (
+			<form
+				className="root-anime__item"
+				id="root-anime-form-create-sequel"
+				onSubmit={handleSubmit(onSubmit)}
+			>
+				<h2 className="root-anime__title">Добавить</h2>
+				<InputTitle
+					id={"root-sequel-title-create"}
+					placeholder="Текущий тайтл"
+					required
+					errorMsg={errors?.title?.message}
+					register={register}
+					param={"title"}
+				/>
+				<InputTitle
+					id={"root-sequel-name-create"}
+					placeholder="Название сиквела / приквела"
+					required
+					errorMsg={errors?.sequel_title?.message}
+					register={register}
+					param={"sequel_title"}
+				/>
+				<div className="root-anime__btn-container">
+					{isFetching ? (
+						<Loader size={"small"} />
+					) : (
+						<BtnDefault
+							type="submit"
+							form="root-anime-form-create-sequel"
+							disabled={!isValid || isShowAlert}
+						>
+							Добавить
+						</BtnDefault>
+					)}
+				</div>
+			</form>
+		)
+	},
+)
