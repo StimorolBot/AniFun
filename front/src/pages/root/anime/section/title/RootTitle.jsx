82263ea6d0d54ae398c01e1c29b23cf07c66e4c8
bot <maximbotpy@gmail.com>
2026-04-16 20:01:28 +0300

    применил prettier

--- front/src/pages/root/anime/section/title/RootTitle.jsx
+++ front/src/pages/root/anime/section/title/RootTitle.jsx
@@ -1,71 +1,72 @@
-import { memo} from "react"
+import { memo } from "react"
 import Masonry from "react-masonry-css"
 
 import { CreateTitle } from "./item/CreateTitle"
-import { UpdateTitle } from "./item/UpdateTitle"
 import { DeleteTitle } from "./item/DeleteTitle"
+import { UpdateTitle } from "./item/UpdateTitle"
 
 const typeList = [
-    {"value": "tv", "label": "тв сериал"},
-    {"value": "ova", "label": "ova"},
-    {"value": "ona", "label": "ona"},
-    {"value": "film", "label": "фильм"},
-    {"value": "special", "label": "спешл"},
+	{ value: "tv", label: "тв сериал" },
+	{ value: "ova", label: "ova" },
+	{ value: "ona", label: "ona" },
+	{ value: "film", label: "фильм" },
+	{ value: "special", label: "спешл" },
 ]
 
 const seasonList = [
-    {"value": "winter", "label": "зима"},
-    {"value": "spring", "label": "весна"},
-    {"value": "summer", "label": "лето"},
-    {"value": "autumn", "label": "осень"},
+	{ value: "winter", label: "зима" },
+	{ value: "spring", label: "весна" },
+	{ value: "summer", label: "лето" },
+	{ value: "autumn", label: "осень" },
 ]
 
 const statusList = [
-    {"value": "ongoing", "label": "онгоинг"},
-    {"value": "completed", "label": "вышел"},
+	{ value: "ongoing", label: "онгоинг" },
+	{ value: "completed", label: "вышел" },
 ]
 
 const ageRestrictList = [
-    {"value": "g", "label": "0+"},
-    {"value": "pg", "label": "6+"},
-    {"value": "pg-13", "label": "12+"},
-    {"value": "nc-17", "label": "16+"},
-    {"value": "r", "label": "18+"},
+	{ value: "g", label: "0+" },
+	{ value: "pg", label: "6+" },
+	{ value: "pg-13", label: "12+" },
+	{ value: "nc-17", label: "16+" },
+	{ value: "r", label: "18+" },
 ]
 
-export const RootTitle = memo(({isShowAlert, setUpdateAlert, setAlertData, breakpoints}) => {
-    
-    return(
-        <section className="root-anime__section">
-            <Masonry
-                className="masonry-root"
-                breakpointCols={breakpoints} 
-                columnClassName="masonry__column-root"
-            >        
-                <CreateTitle 
-                    typeList={typeList}
-                    seasonList={seasonList}
-                    statusList={statusList}
-                    ageRestrictList={ageRestrictList}
-                    isShowAlert={isShowAlert}
-                    setUpdateAlert={setUpdateAlert}
-                    setAlertData={setAlertData}
-                />            
-                <UpdateTitle
-                    typeList={typeList}
-                    seasonList={seasonList}
-                    statusList={statusList}
-                    ageRestrictList={ageRestrictList}
-                    isShowAlert={isShowAlert}
-                    setUpdateAlert={setUpdateAlert}
-                    setAlertData={setAlertData}
-                />
-                <DeleteTitle
-                    isShowAlert={isShowAlert}
-                    setUpdateAlert={setUpdateAlert}
-                    setAlertData={setAlertData}
-                />
-            </Masonry>
-        </section>
-    )
-})
+export const RootTitle = memo(
+	({ isShowAlert, setUpdateAlert, setAlertData, breakpoints }) => {
+		return (
+			<section className="root-anime__section">
+				<Masonry
+					className="masonry-root"
+					breakpointCols={breakpoints}
+					columnClassName="masonry__column-root"
+				>
+					<CreateTitle
+						typeList={typeList}
+						seasonList={seasonList}
+						statusList={statusList}
+						ageRestrictList={ageRestrictList}
+						isShowAlert={isShowAlert}
+						setUpdateAlert={setUpdateAlert}
+						setAlertData={setAlertData}
+					/>
+					<UpdateTitle
+						typeList={typeList}
+						seasonList={seasonList}
+						statusList={statusList}
+						ageRestrictList={ageRestrictList}
+						isShowAlert={isShowAlert}
+						setUpdateAlert={setUpdateAlert}
+						setAlertData={setAlertData}
+					/>
+					<DeleteTitle
+						isShowAlert={isShowAlert}
+						setUpdateAlert={setUpdateAlert}
+						setAlertData={setAlertData}
+					/>
+				</Masonry>
+			</section>
+		)
+	},
+)
