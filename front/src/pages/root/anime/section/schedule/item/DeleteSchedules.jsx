4b688f00bcb15f7b93b27799fa1afbd001eaa1a4
bot <maximbotpy@gmail.com>
2026-04-16 20:59:37 +0300

    feat: добавил компонент для удаления расписания

---
+++ front/src/pages/root/anime/section/schedule/item/DeleteSchedules.jsx
@@ -0,0 +1,76 @@
+import { memo, useRef } from "react"
+import { useForm } from "react-hook-form"
+
+import { useQuery } from "@tanstack/react-query"
+
+import { BtnDefault } from "../../../../../../ui/btn/BtnDefault"
+
+import { InputTitle } from "../../../../ui/Input/InputTitle"
+
+import { Loader } from "../../../../../../components/loader/Loader"
+
+import { api } from "../../../../../../api"
+import { alertHandler } from "../../../../../../utils/utils"
+import { rootAnimeSchedule } from "../../../../query_key"
+
+export const DeleteSchedules = memo(
+	({ isShowAlert, setUpdateAlert, setAlertData }) => {
+		const formDataRef = useRef()
+		const {
+			register,
+			handleSubmit,
+			resetField,
+			formState: { errors, isValid },
+		} = useForm({ mode: "onChange" })
+
+		const { isFetching, refetch, error } = useQuery({
+			queryKey: [rootAnimeSchedule.delete],
+			retry: 1,
+			enabled: false,
+			queryFn: async () => {
+				return api
+					.delete("/admin/anime/delete-schedules", {
+						params: formDataRef.current,
+					})
+					.then((r) => r)
+			},
+		})
+
+		const onSubmit = async (data) => {
+			formDataRef.current = data
+			const response = await refetch()
+			await alertHandler(response, error, setAlertData, setUpdateAlert)
+		}
+
+		return (
+			<form
+				className="root-anime__item"
+				id="root-anime-form-delete-schedule"
+				onSubmit={handleSubmit(onSubmit)}
+			>
+				<h2 className="root-anime__title">Удалить расписание</h2>
+				<InputTitle
+					id={"root-schedule-delete"}
+					placeholder="Название"
+					required
+					errorMsg={errors?.title?.message}
+					register={register}
+					param={"title"}
+				/>
+				<div className="root-anime__btn-container">
+					{isFetching ? (
+						<Loader size={"small"} />
+					) : (
+						<BtnDefault
+							type="submit"
+							form="root-anime-form-delete-schedule"
+							disabled={!isValid || isShowAlert}
+						>
+							Удалить
+						</BtnDefault>
+					)}
+				</div>
+			</form>
+		)
+	},
+)
