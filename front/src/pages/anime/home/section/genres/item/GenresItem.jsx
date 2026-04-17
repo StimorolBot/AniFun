3abe95d3a94023cccc9e3e703db2846b16f895d7
bot <maximbotpy@gmail.com>
2026-04-17 22:20:49 +0300

    применил prettier

--- front/src/pages/anime/home/section/genres/item/GenresItem.jsx
+++ front/src/pages/anime/home/section/genres/item/GenresItem.jsx
@@ -1,27 +1,39 @@
 import { memo } from "react"
 import { Link } from "react-router-dom"
+
 import { getPostfix } from "../../../../../../utils/utils"
 
 import "./style.sass"
 
-
-export const GenresItem = memo(({item, imgData, ...props}) => {
-    
-    return(
-        <li className="genres__item" {...props}>
-            <Link className="genres__item-link" to={`/anime/genres/${item.value}`}>
-                <div className="genres__bg-container">
-                    <img className="genres__bg" src={imgData} loading="lazy" alt="genres-bg" />                            
-                </div>
-                <ul className="genres__desc-list">
-                    <li className="genres__desc-item genres__desc_title">
-                        {item.label}
-                    </li>
-                    <li className="genres__desc-item">
-                        {`${item.genres_count} ${getPostfix("релиз", item.genres_count)}`}
-                    </li>
-                </ul>
-            </Link>
-        </li>
-    )
+export const GenresItem = memo(({ item, imgData, ...props }) => {
+	return (
+		<li
+			className={
+				imgData ? "genres__item" : "genres__item background-plug"
+			}
+			{...props}
+		>
+			<Link
+				className="genres__item-link"
+				to={`/anime/genres/${item.value}`}
+			>
+				<div className="genres__bg-container">
+					<img
+						className="genres__bg"
+						src={imgData}
+						loading="lazy"
+						alt="genres-bg"
+					/>
+				</div>
+				<ul className="genres__desc-list">
+					<li className="genres__desc-item genres__desc_title">
+						{item.label}
+					</li>
+					<li className="genres__desc-item">
+						{`${item.genres_count} ${getPostfix("релиз", item.genres_count)}`}
+					</li>
+				</ul>
+			</Link>
+		</li>
+	)
 })
