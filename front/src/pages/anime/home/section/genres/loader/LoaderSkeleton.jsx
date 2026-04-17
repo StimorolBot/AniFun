da756ae18e4dcab49d8bad316b81952becba89aa
bot <maximbotpy@gmail.com>
2026-04-17 22:33:57 +0300

    feat: добавил LoaderSkeleton для GenresItem

---
+++ front/src/pages/anime/home/section/genres/loader/LoaderSkeleton.jsx
@@ -0,0 +1,26 @@
+import { memo } from "react"
+import Skeleton from "react-loading-skeleton"
+
+import "./style.sass"
+
+export const LoaderSkeleton = memo(({ count }) => {
+	return (
+		<ul className="genres__list">
+			{Array(count)
+				.fill(0)
+				.map((_, index) => {
+					return (
+						<div className="genres__item" key={index}>
+							<div className="genres__item-skeleton-container">
+								<Skeleton />
+								<div className="genres__item-skeleton-item">
+									<Skeleton />
+									<Skeleton />
+								</div>
+							</div>
+						</div>
+					)
+				})}
+		</ul>
+	)
+})
