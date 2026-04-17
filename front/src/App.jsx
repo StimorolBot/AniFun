2fd00f3e5617c0899aff937fe523367f0e572d38
bot <maximbotpy@gmail.com>
2026-04-17 21:48:45 +0300

    feat: подключил SkeletonTheme

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,25 +1,27 @@
 import { useState } from "react"
-import { router } from "./router"
+import { SkeletonTheme } from "react-loading-skeleton"
 import { RouterProvider } from "react-router-dom"
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-
-
-import { GenresContext } from "./context/GenresContext"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
+import { GenresContext } from "./context/GenresContext"
+import { router } from "./router"
 
 export function App() {
-  const [genresContext, setGenresContext] = useState([])
-  const queryClient = new QueryClient()
+	const [genresContext, setGenresContext] = useState([])
+	const queryClient = new QueryClient()
 
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false}/>
-      <GenresContext.Provider value={{genresContext, setGenresContext}}>
-        <RouterProvider router={ router }/>
-      </GenresContext.Provider>
-    </QueryClientProvider>
-  )
+	return (
+		<SkeletonTheme baseColor="#101010" highlightColor="#141414">
+			<QueryClientProvider client={queryClient}>
+				<ReactQueryDevtools initialIsOpen={false} />
+				<GenresContext.Provider
+					value={{ genresContext, setGenresContext }}
+				>
+					<RouterProvider router={router} />
+				</GenresContext.Provider>
+			</QueryClientProvider>
+		</SkeletonTheme>
+	)
 }
