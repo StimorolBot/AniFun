239200a7e7dd27992576802d4f95991714f2f6e9
bot <maximbotpy@gmail.com>
2026-04-18 12:17:56 +0300

    применил prettier

--- front/src/pages/anime/home/section/franchises/item/FranchisesItem.jsx
+++ front/src/pages/anime/home/section/franchises/item/FranchisesItem.jsx
@@ -1,36 +1,42 @@
+import { memo } from "react"
 import { Link } from "react-router-dom"
-import { getPostfix } from "../../../../../../utils/utils"
+
+import { getPostfix } from "../../../../utils/utils"
 
 import "./style.sass"
 
-
-export function FranchisesItem({item, imgData, ...props}){
-
-    return(
-        <li className="franchises__item" {...props}>
-            <Link className="franchises__link" to={`anime/franchises/${item?.sequel_uuid}`}>
-                <div className="franchises__bg-container">
-                    <img className="franchises__bg" src={imgData} loading="lazy" alt="franchises" />
-                </div>
-                <div className="franchises__container">
-                    <h3 className="franchises__desc_title">
-                        {item?.title}
-                    </h3>
-                    <ul className="franchises__desc-list">
-                        <li className="franchises__desc-item">
-                            {item.start_year} - {item.end_year}
-                        </li>
-                        <li className="franchises__desc-item">
-                            <span className="point">
-                                {item.seasons_count} {getPostfix("сезон", item.seasons_count)}
-                            </span>
-                            <span className="point">
-                                {item.total_episodes} {getPostfix("эпизод", item.total_episodes)}
-                            </span>
-                        </li>
-                    </ul>
-                </div>
-            </Link>
-        </li>
-    )
-}
+export const FranchisesItem = memo(({ item, imgData, ...props }) => {
+	return (
+		<li className="franchises__item" {...props}>
+			<Link
+				className="franchises__link"
+				to={`anime/franchises/${item?.sequel_uuid}`}
+			>
+				<div className="franchises__bg-container">
+					<img
+						className="franchises__bg"
+						src={imgData}
+						loading="lazy"
+						alt="franchises"
+					/>
+				</div>
+				<div className="franchises__container">
+					<h3 className="franchises__desc_title">{item?.title}</h3>
+					<ul className="franchises__desc-list">
+						<li className="franchises__desc-item">
+							{item.start_year} - {item.end_year}
+						</li>
+						<li className="franchises__desc-item">
+							<span className="point">
+								{`${item.seasons_count} ${getPostfix("сезон", item.seasons_count)}`}
+							</span>
+							<span className="point">
+								{`${item.total_episodes} ${getPostfix("эпизод", item.total_episodes)}`}
+							</span>
+						</li>
+					</ul>
+				</div>
+			</Link>
+		</li>
+	)
+})
