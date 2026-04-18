f469adf79501567e2faccc72cd9e7a5b8f003a8a
bot <maximbotpy@gmail.com>
2026-04-18 17:52:49 +0300

    применил prettier

--- front/src/components/loader/Loader.jsx
+++ front/src/components/loader/Loader.jsx
@@ -2,9 +2,12 @@ import { memo } from "react"
 
 import "./style/loader.sass"
 
-
-export const Loader = memo(({size="default", center=false}) => {
-    return(
-        <span className="loader transition" data-size-loader={size} data-center={center}/>
-    )
+export const Loader = memo(({ size = "default", center = false }) => {
+	return (
+		<span
+			className="loader transition"
+			data-size-loader={size}
+			data-center={center}
+		/>
+	)
 })
