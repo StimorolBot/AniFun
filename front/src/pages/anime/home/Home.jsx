2e00220f56b903933825dad8f468983791e7040d
bot <maximbotpy@gmail.com>
2026-04-18 15:55:05 +0300

    применил prettier

--- front/src/pages/anime/home/Home.jsx
+++ front/src/pages/anime/home/Home.jsx
@@ -1,35 +1,32 @@
 import { Helmet } from "react-helmet"
 
-import { Header } from "../../../components/header/Header"
 import { Footer } from "../../../components/footer/Footer"
+import { Header } from "../../../components/header/Header"
 
-import { SwiperCustom } from "./section/swiper/SwiperCustom"
-import { NewEpisodes } from "./section/new_episodes/NewEpisodes"
-import { Schedule } from "./section/schedule/Schedule"
 import { Franchises } from "./section/franchises/Franchises"
 import { Genres } from "./section/genres/Genres"
+import { NewEpisodes } from "./section/new_episodes/NewEpisodes"
+import { Schedule } from "./section/schedule/Schedule"
+import { SwiperCustom } from "./section/swiper/SwiperCustom"
 
-
-export const Home = () => {
-    return(
-    <>
-        <Helmet>
-            <title>AniFun</title>
-        </Helmet>
-        <div className="wrapper">
-            <Header/>
-            <main className="main">
-                <h1 className="title-page"> 
-                    Главная страница 
-                </h1>
-                <SwiperCustom/>
-                <NewEpisodes/> 
-                <Schedule/>
-                <Franchises/>
-                <Genres/>
-            </main>
-            <Footer/>            
-        </div>
-    </>
-    )
-}
+export const Home = memo(() => {
+	return (
+		<>
+			<Helmet>
+				<title>AniFun</title>
+			</Helmet>
+			<div className="wrapper">
+				<Header />
+				<main className="main">
+					<h1 className="title-page">Главная страница</h1>
+					<SwiperCustom />
+					<NewEpisodes />
+					<Schedule />
+					<Franchises />
+					<Genres />
+				</main>
+				<Footer />
+			</div>
+		</>
+	)
+})
