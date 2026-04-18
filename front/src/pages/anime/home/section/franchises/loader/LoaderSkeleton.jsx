30c11ed9115a2159bd035848e203b837bf131153
bot <maximbotpy@gmail.com>
2026-04-18 12:19:59 +0300

    feat: добавил Skeleton для FranchisesItem

---
+++ front/src/pages/anime/home/section/franchises/loader/LoaderSkeleton.jsx
@@ -0,0 +1,38 @@
+import { memo } from "react"
+import Skeleton from "react-loading-skeleton"
+
+import "./style.sass"
+
+export const LoaderSkeleton = memo(({ count }) => {
+	return (
+		<ul className="franchises__list">
+			{Array(count)
+				.fill(0)
+				.map((_, index) => {
+					return (
+						<div className="franchises__item" key={index}>
+							<div className="franchises__item-skeleton-container">
+								<div className="franchises__item-skeleton-bg">
+									<Skeleton />
+								</div>
+								<div className="franchises__item-skeleton-item">
+									<div className="franchises__item-skeleton-img">
+										<Skeleton />
+									</div>
+									<div className="franchises__item-skeleton-desc">
+										<div className="franchises__item-skeleton-top">
+											<Skeleton />
+										</div>
+										<div className="franchises__item-bottom">
+											<Skeleton />
+											<Skeleton />
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					)
+				})}
+		</ul>
+	)
+})
