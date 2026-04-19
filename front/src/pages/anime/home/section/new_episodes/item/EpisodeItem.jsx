3c69a6b1d6680ddcf1deaf66e0a038eab0aa052c
bot <maximbotpy@gmail.com>
2026-04-19 20:32:35 +0300

    применил prettier

--- front/src/pages/anime/home/section/new_episodes/item/EpisodeItem.jsx
+++ front/src/pages/anime/home/section/new_episodes/item/EpisodeItem.jsx
@@ -3,56 +3,74 @@ import { Link } from "react-router-dom"
 
 import "./style.sass"
 
-
-export const EpisodeItem = memo(({item, imgData, ...props}) => {
-
-    return(
-        <li className="episode__item" {...props}>
-            <img className="episode__img" src={imgData} loading="lazy" alt="poster" />
-            <div className="episode__description">
-                <p className="episode__number">
-                    {item.number} эпизод
-                </p>
-                <Link className="episode__title" to={`/anime/${item.anime.alias}`}>
-                    {item.anime.title}
-                </Link>
-                <ul className="episode__desc-list">
-                    <li className="episode__desc-item point">
-                        <Link to={`/anime`} state={item.anime.year}>
-                            {item.anime.year}
-                        </Link>
-                    </li>
-                    <li className="episode__desc-item point">
-                        <Link to={`/anime`} state={item.anime.season.value}>
-                            {item.anime.season.label}
-                        </Link>
-                    </li>
-                    <li className="episode__desc-item point">
-                        <Link to={`/anime`} state={item.anime.type.value}>
-                            {item.anime.type.label}
-                        </Link>
-                    </li>
-                    <li className="episode__desc-item point">
-                        <Link to={`/anime`} state={item.anime.age_restrict.value}>
-                            {item.anime.age_restrict.label}
-                        </Link>
-                    </li>
-                </ul>
-                <ul className="episode__desc-list">
-                    {item.anime.genres?.slice(-3)?.map((genre, index) => {
-                        return(
-                            <li className="episode__desc-item episode__desc-item_tag point" key={index}>
-                                <Link to={`anime/genres/${genre.value}`}>
-                                    {genre.label}
-                                </Link>
-                            </li>
-                        )
-                    })}
-                </ul>
-                <Link className="episode__link" to={`anime/${item.anime.alias}/episode/${item.uuid_episode}`}>
-                    Смотреть
-                </Link>
-            </div>
-        </li>
-    )
+export const EpisodeItem = memo(({ item, imgData, ...props }) => {
+	return (
+		<li
+			className={
+				imgData ? "episode__item" : "episode__item  background-plug"
+			}
+			{...props}
+		>
+			<img
+				className="episode__img"
+				src={imgData}
+				loading="lazy"
+				alt="poster"
+			/>
+			<div className="episode__description">
+				<p className="episode__number">{item.number} эпизод</p>
+				<Link
+					className="episode__title"
+					to={`/anime/${item.anime.alias}`}
+				>
+					{item.anime.title}
+				</Link>
+				<ul className="episode__desc-list">
+					<li className="episode__desc-item point">
+						<Link to={`/anime`} state={item.anime.year}>
+							{item.anime.year}
+						</Link>
+					</li>
+					<li className="episode__desc-item point">
+						<Link to={`/anime`} state={item.anime.season.value}>
+							{item.anime.season.label}
+						</Link>
+					</li>
+					<li className="episode__desc-item point">
+						<Link to={`/anime`} state={item.anime.type.value}>
+							{item.anime.type.label}
+						</Link>
+					</li>
+					<li className="episode__desc-item point">
+						<Link
+							to={`/anime`}
+							state={item.anime.age_restrict.value}
+						>
+							{item.anime.age_restrict.label}
+						</Link>
+					</li>
+				</ul>
+				<ul className="episode__desc-list">
+					{item.anime.genres?.slice(-3)?.map((genre, index) => {
+						return (
+							<li
+								className="episode__desc-item episode__desc-item_tag point"
+								key={index}
+							>
+								<Link to={`anime/genres/${genre.value}`}>
+									{genre.label}
+								</Link>
+							</li>
+						)
+					})}
+				</ul>
+				<Link
+					className="episode__link"
+					to={`anime/${item.anime.alias}/episode/${item.uuid_episode}`}
+				>
+					Смотреть
+				</Link>
+			</div>
+		</li>
+	)
 })
