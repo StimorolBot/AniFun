ec49890fb416963b6e5b55edab5393a2e5a07253
bot <maximbotpy@gmail.com>
2026-04-19 19:55:29 +0300

    применил prettier

--- front/src/pages/anime/wrapper/WrapperSection.jsx
+++ front/src/pages/anime/wrapper/WrapperSection.jsx
@@ -4,28 +4,24 @@ import { CSSTransition, SwitchTransition } from "react-transition-group"
 
 import "./style.sass"
 
-
-export const WrapperSection = memo(({title, link, ref, children, value}) => {
-    
-    return(
-        <div className="section__wrapper">
-            <div className="section__inner">
-                <Link className="section__link" to={link}>
-                    <h2 className="section__title">
-                        {title}
-                    </h2>
-                </Link>
-            </div>
-            <SwitchTransition mode="out-in">
-                <CSSTransition 
-                    classNames="transition" 
-                    key={value}
-                    nodeRef={ref} 
-                    timeout={300}
-                >
-                    {children}
-                </CSSTransition>
-            </SwitchTransition>  
-        </div>
-    )
+export const WrapperSection = memo(({ title, link, ref, children, value }) => {
+	return (
+		<div className="section__wrapper">
+			<div className="section__inner">
+				<Link className="section__link" to={link}>
+					<h2 className="section__title">{title}</h2>
+				</Link>
+			</div>
+			<SwitchTransition mode="out-in">
+				<CSSTransition
+					classNames="transition"
+					key={value}
+					nodeRef={ref}
+					timeout={300}
+				>
+					{children}
+				</CSSTransition>
+			</SwitchTransition>
+		</div>
+	)
 })
