588406ab88db640ba18ebb58b21c028420666ba8
bot <maximbotpy@gmail.com>
2026-04-19 21:27:08 +0300

    применил prettier

--- front/src/ui/input/InputSearch.jsx
+++ front/src/ui/input/InputSearch.jsx
@@ -2,19 +2,15 @@ import { memo } from "react"
 
 import "./style/input_search.sass"
 
-
-export const InputSearch = memo(({setVal, val, ...props}) => {
-
-    return(
-        <input
-            className="input-search"
-            id="input-search"
-            type="search" 
-            autoComplete="off"
-            onChange={setVal}
-            placeholder="Введите название аниме..."
-            value={val}
-            {...props}
-        />
-    )
+export const InputSearch = memo(({ setVal, val, ...props }) => {
+	return (
+		<input
+			className="input-search"
+			id="input-search"
+			type="search"
+			onChange={setVal}
+			value={val}
+			{...props}
+		/>
+	)
 })
