a86b69046289653cdca4da437e63df22f8931544
bot <maximbotpy@gmail.com>
2026-04-19 20:31:57 +0300

    feat: добавил ленивую загрузку, применил prettier

--- front/src/pages/anime/home/section/schedule/Schedule.jsx
+++ front/src/pages/anime/home/section/schedule/Schedule.jsx
@@ -1,76 +1,118 @@
-import { useRef, useState } from "react"
-
+import { useEffect, useRef, useState } from "react"
 import Masonry from "react-masonry-css"
+
 import { useQueries, useQuery } from "@tanstack/react-query"
 
-import { api } from "../../../../../api"
+import { SwitchDay } from "./ui/SwitchDay"
 
-import { WrapperSection } from "../../../wrapper/WrapperSection"
 import { ScheduleItem } from "./item/ScheduleItem"
 
-import { SwitchDay } from "./ui/SwitchDay"
-import { Loader } from "../../../../../components/loader/Loader"
+import { LoaderSkeleton } from "./loader/LoaderSkeleton"
 
-import { scheduleCache, schedulePosterCache } from "../../../../../query_key"
+import { WrapperSection } from "../../../wrapper/WrapperSection"
+
+import { api } from "../../../../../api"
+import { useObserverImg } from "../../../../../hook/useObserverImgProvider"
+import { scheduleSection } from "./query_key"
 
 import "./style.sass"
 
+const breakpoints = {
+	default: 4,
+	1280: 3,
+	880: 2,
+	590: 1,
+}
 
 export const Schedule = () => {
-    const transitionRef = useRef()
-    const [schedule, setSchedule] = useState("today")
-    
-    const {data: scheduleData, isLoading} = useQuery({
-        queryKey: [scheduleCache, schedule],
-        staleTime: 1000 * 60 * 3,
-        queryFn: async () => {
-            return await api.get("/schedules", {params: {"schedule": schedule}}).then(r => r.data)
-        },
-        placeholderData: []
-    })
+	const { observe } = useObserverImg()
 
-    const imgData = useQueries({
-        queries: scheduleData?.map(item => ({
-            queryKey: [schedulePosterCache, item.anime.poster.poster_uuid],
-            staleTime: 1000 * 60 * 3,
-            queryFn: async () => {
-                return await api.get(`/s3/anime-${item.anime.uuid}/${item.anime.poster.poster_uuid}`).then(r => r.data)
-            }
-        }))
-    })
+	const sectionRef = useRef()
+	const transitionRef = useRef()
 
-    const breakpoints = {
-        default: 4,
-        1280: 3,
-        880: 2,
-        590: 1
-    }
+	const [isView, setIsView] = useState(false)
+	const [schedule, setSchedule] = useState("today")
 
-    return(
-        <section className="schedules">
-            <div className="container">
-                <WrapperSection title={"Расписание релизов"} link={"/anime/schedules"} ref={transitionRef} value={isLoading}>
-                    <>
-                        <div className="switch-day__wrapper">
-                            <SwitchDay value={schedule} setValue={setSchedule}/> 
-                        </div>
-                        <ul className="schedules__list transition" ref={transitionRef}>
-                            { isLoading
-                                ? <Loader/>
-                                : scheduleData.length >= 1 
-                                    ? <Masonry breakpointCols={breakpoints} className="masonry" columnClassName="masonry__column">
-                                        {scheduleData?.map((item, index) => {
-                                            return <ScheduleItem item={item} imgData={imgData[index].data} key={index}/>
-                                        })}
-                                    </Masonry>
-                                    :<li className="schedules__empty">
-                                        К сожалению, в ближайшее время новых серий не ожидается :( 
-                                    </li>
-                            }
-                        </ul>
-                    </>                
-                </WrapperSection>        
-            </div>
-        </section>
-    )
+	const { data: scheduleData, isFetching } = useQuery({
+		queryKey: [scheduleSection.getData, schedule],
+		staleTime: 1000 * 60 * 3,
+		enabled: isView,
+		queryFn: async () => {
+			return await api
+				.get("/schedules", { params: { schedule: schedule } })
+				.then((r) => r.data)
+		},
+		placeholderData: [],
+	})
+
+	const imgData = useQueries({
+		queries: scheduleData?.map((item) => ({
+			queryKey: [scheduleSection.getImg, item.anime.poster.poster_uuid],
+			staleTime: 1000 * 60 * 3,
+			queryFn: async () => {
+				return await api
+					.get(
+						`/s3/anime-${item.anime.uuid}/${item.anime.poster.poster_uuid}`,
+					)
+					.then((r) => r.data)
+			},
+		})),
+	})
+	useEffect(() => {
+		const el = sectionRef.current
+		if (!el) return
+
+		observe(el, () => setIsView(true))
+	}, [observe])
+
+	return (
+		<section className="schedules" ref={sectionRef}>
+			<div className="container">
+				<WrapperSection
+					title={"Расписание релизов"}
+					link={"/anime/schedules"}
+					ref={transitionRef}
+					value={isFetching}
+				>
+					<>
+						<div className="switch-day__wrapper">
+							<SwitchDay
+								value={schedule}
+								setValue={setSchedule}
+							/>
+						</div>
+						<ul
+							className="schedules__list transition"
+							ref={transitionRef}
+						>
+							{isFetching ? (
+								<LoaderSkeleton count={4} />
+							) : scheduleData.length >= 1 ? (
+								<Masonry
+									breakpointCols={breakpoints}
+									className="masonry"
+									columnClassName="masonry__column"
+								>
+									{scheduleData?.map((item, index) => {
+										return (
+											<ScheduleItem
+												item={item}
+												imgData={imgData[index].data}
+												key={index}
+											/>
+										)
+									})}
+								</Masonry>
+							) : (
+								<li className="schedules__empty">
+									К сожалению, в ближайшее время новых серий
+									не ожидается :(
+								</li>
+							)}
+						</ul>
+					</>
+				</WrapperSection>
+			</div>
+		</section>
+	)
 }
