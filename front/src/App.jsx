030baf873b303aa1ec3a0da194e41b2f7a47a6ec
bot <maximbotpy@gmail.com>
2026-04-21 00:05:56 +0300

    feat: добавил ObserverImgProvider

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,22 +6,20 @@ import { RouterProvider } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
-import { GenresContext } from "./context/GenresContext"
+import { ObserverImgProvider } from "./providers/ObserverImgProvider"
+
 import { router } from "./router"
 
-export function App() {
-	const [genresContext, setGenresContext] = useState([])
+export const App = () => {
 	const queryClient = new QueryClient()
 
 	return (
 		<SkeletonTheme baseColor="#101010" highlightColor="#141414">
 			<QueryClientProvider client={queryClient}>
 				<ReactQueryDevtools initialIsOpen={false} />
-				<GenresContext.Provider
-					value={{ genresContext, setGenresContext }}
-				>
+				<ObserverImgProvider>
 					<RouterProvider router={router} />
-				</GenresContext.Provider>
+				</ObserverImgProvider>
 			</QueryClientProvider>
 		</SkeletonTheme>
 	)
