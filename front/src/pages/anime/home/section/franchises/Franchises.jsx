3fb331b759ae872c7a20342f92cfa4bcaafde19d
bot <maximbotpy@gmail.com>
2026-04-22 17:22:34 +0300

    feat: добавил limit к основному запросу

--- front/src/pages/anime/home/section/franchises/Franchises.jsx
+++ front/src/pages/anime/home/section/franchises/Franchises.jsx
@@ -9,11 +9,17 @@ import { LoaderSkeleton } from "./loader/LoaderSkeleton"
 import { WrapperSection } from "../../../wrapper/WrapperSection"
 
 import { useObserverImg } from "../../../../../hook/useObserverImgProvider"
+import { useViewport } from "../../../../../hook/useViewport"
 
 import { api } from "../../../../../api"
 import { franchisesSection } from "./query_key"
 
 import "./style.sass"
+
+const getLimit = (w) => {
+	if (w > 1300) return 3
+	return 2
+}
 
 export const Franchises = memo(() => {
 	const { observe } = useObserverImg()
@@ -23,12 +29,17 @@ export const Franchises = memo(() => {
 
 	const [isView, setIsView] = useState(false)
 
+	const widthViewport = useViewport()
+	const limit = getLimit(widthViewport)
+
 	const { data: FranchisesData = [], isFetching } = useQuery({
-		queryKey: [franchisesSection.getData],
+		queryKey: [franchisesSection.getData, limit],
 		enabled: isView,
 		staleTime: 1000 * 60 * 3,
 		queryFn: async () => {
-			return await api.get("/franchises").then((r) => r.data)
+			return await api
+				.get("/franchises", { params: { limit: limit } })
+				.then((r) => r.data)
 		},
 	})
 
@@ -61,9 +72,15 @@ export const Franchises = memo(() => {
 				>
 					<div className="transition" ref={transitionRef}>
 						{isFetching ? (
-							<LoaderSkeleton count={3} />
+							<LoaderSkeleton count={limit} />
 						) : (
-							<ul className="franchises__list">
+							<ul
+								className={
+									FranchisesData.length == 1
+										? "franchises__list franchises__list_one-item"
+										: "franchises__list"
+								}
+							>
 								{FranchisesData?.map((item, index) => {
 									return (
 										<FranchisesItem
