bea0f10134efd9d3a7d313e3d6384e6de67e43db
bot <maximbotpy@gmail.com>
2026-04-24 01:10:24 +0300

    feat: добавил валидацию

--- front/src/ui/input/InputSearch.jsx
+++ front/src/ui/input/InputSearch.jsx
@@ -2,15 +2,31 @@ import { memo } from "react"
 
 import "./style/input_search.sass"
 
-export const InputSearch = memo(({ setVal, val, ...props }) => {
+export const InputSearch = memo(({ id, register, errorMsg, ...props }) => {
 	return (
-		<input
-			className="input-search"
-			id="input-search"
-			type="search"
-			onChange={setVal}
-			value={val}
-			{...props}
-		/>
+		<div className="input-search__container">
+			<input
+				className="input-search"
+				id={id}
+				type="search"
+				minLength={5}
+				maxLength={150}
+				{...props}
+				{...register("title", {
+					minLength: { value: 5, message: "Название от 5 символов" },
+					maxLength: {
+						value: 150,
+						message: "Название до 150 символов",
+					},
+					pattern: {
+						value: /(^[a-zA-Zа-яёА-ЯЁ0-9][a-zA-Zа-яёА-ЯЁ0-9\s]*$)/,
+						message: "Некорректное название",
+					},
+				})}
+			/>
+			<label className="input-search__label" htmlFor={id}>
+				{errorMsg}
+			</label>
+		</div>
 	)
 })
