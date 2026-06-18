7e55da4095d6c3406497dfd9139d82817daf8045
bot <maximbotpy@gmail.com>
2026-06-18 21:19:40 +0300

    feat: Добавил иконку с лого

---
+++ front/src/ui/icon/Logo.jsx
@@ -0,0 +1,65 @@
+export const Logo = () => {
+	return (
+		<svg
+			viewBox="0 0 553 328"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M184.5 0L0 282.5L79 283L204 92.5L200.5 164.5L266.5 136L273.161 0H184.5Z"
+				fill="#FFFAFA"
+			/>
+			<path
+				d="M172 180.5L135.5 235.5H195L193 282.5H257.5L263 180.5H172Z"
+				fill="#FFFAFA"
+			/>
+			<g filter="url(#filter0_d_1_23)">
+				<path
+					d="M286 167L277 320L350 245L353 170L438 170.5L479.5 106L359 105.5L286 167Z"
+					fill="#D53032"
+				/>
+				<path
+					d="M293.5 29.5L288.5 141L360 82.634V65H507.5L548.5 0H322L293.5 29.5Z"
+					fill="#D53032"
+				/>
+			</g>
+			<defs>
+				<filter
+					id="filter0_d_1_23"
+					x="273"
+					y="0"
+					width="279.5"
+					height="328"
+					filterUnits="userSpaceOnUse"
+					colorInterpolationFilters="sRGB"
+				>
+					<feFlood floodOpacity="0" result="BackgroundImageFix" />
+					<feColorMatrix
+						in="SourceAlpha"
+						type="matrix"
+						values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+						result="hardAlpha"
+					/>
+					<feOffset dy="4" />
+					<feGaussianBlur stdDeviation="2" />
+					<feComposite in2="hardAlpha" operator="out" />
+					<feColorMatrix
+						type="matrix"
+						values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+					/>
+					<feBlend
+						mode="normal"
+						in2="BackgroundImageFix"
+						result="effect1_dropShadow_1_23"
+					/>
+					<feBlend
+						mode="normal"
+						in="SourceGraphic"
+						in2="effect1_dropShadow_1_23"
+						result="shape"
+					/>
+				</filter>
+			</defs>
+		</svg>
+	)
+}
