c19c6cbea6eae3b87e51cdea47b0ade34e338519
bot <maximbotpy@gmail.com>
2026-07-06 20:03:11 +0300

    init

---
+++ front/src/ui/icon/Watch.jsx
@@ -0,0 +1,17 @@
+export const Watch = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="32"
+			height="32"
+			viewBox="0 0 20 20"
+		>
+			<path
+				fillRule="evenodd"
+				d="M10 18a8 8 0 1 0 0-16a8 8 0 0 0 0 16m1-12a1 1 0 1 0-2 0v4a1 1 0 0 0 
+				.293.707l2.828 2.829a1 1 0 1 0 1.415-1.415L11 9.586z"
+				clipRule="evenodd"
+			/>
+		</svg>
+	)
+}
