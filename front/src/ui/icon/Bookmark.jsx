a11f69a94cb59cdc8d682da490ab591dbf92c38f
bot <maximbotpy@gmail.com>
2026-07-06 20:00:07 +0300

    init

---
+++ front/src/ui/icon/Bookmark.jsx
@@ -0,0 +1,12 @@
+export const Bookmark = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="32"
+			height="32"
+			viewBox="0 0 20 20"
+		>
+			<path d="M5 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v14l-5-2.5L5 18z" />
+		</svg>
+	)
+}
