c850f74b94892bdb87a7c95ec88534896389b41a
bot <maximbotpy@gmail.com>
2026-07-08 20:36:05 +0300

    feat: скилетон для TitleItem

---
+++ front/src/pages/root/ui/skeleton/TitleSkeleton.jsx
@@ -0,0 +1,99 @@
+import Skeleton from "react-loading-skeleton"
+import { Link } from "react-router-dom"
+
+import { Edit } from "../../../../ui/icon/Edit"
+import { Remove } from "../../../../ui/icon/Remove"
+
+import { BtnDefault } from "../../../../ui/btn/BtnDefault"
+
+export const TitleSkeleton = ({ count }) => {
+	const heightSkeleton = 25
+	const widthSkeleton = 100
+	return (
+		<>
+			{Array.from({ length: count }).map((_, index) => {
+				return (
+					<tr className="root-title__item">
+						<td>
+							<div className="root-title__img-container">
+								<Skeleton style={{ height: 100, width: 70 }} />
+							</div>
+						</td>
+						<td>
+							<div>
+								<Skeleton style={{ width: 150 }} />
+								<Skeleton
+									style={{
+										width: widthSkeleton,
+										marginTop: 5,
+									}}
+								/>
+							</div>
+						</td>
+						<td>
+							<Skeleton
+								style={{
+									height: heightSkeleton,
+									width: widthSkeleton,
+								}}
+							/>
+						</td>
+						<td>
+							<Skeleton
+								style={{
+									height: heightSkeleton,
+									width: widthSkeleton,
+								}}
+							/>
+						</td>
+
+						<td>
+							<Skeleton
+								style={{
+									height: heightSkeleton,
+									width: widthSkeleton,
+								}}
+							/>
+						</td>
+						<td>
+							<p>
+								<Skeleton
+									style={{
+										height: heightSkeleton,
+										width: widthSkeleton,
+									}}
+								/>
+							</p>
+						</td>
+						<td>
+							<Skeleton
+								style={{
+									height: heightSkeleton,
+									width: widthSkeleton,
+								}}
+							/>
+						</td>
+						<td>
+							<div className="root-title__btn">
+								<Skeleton
+									style={{
+										height: heightSkeleton,
+										width: heightSkeleton,
+										borderRadius: "50%",
+									}}
+								/>
+								<Skeleton
+									style={{
+										height: heightSkeleton,
+										width: heightSkeleton,
+										borderRadius: "50%",
+									}}
+								/>
+							</div>
+						</td>
+					</tr>
+				)
+			})}
+		</>
+	)
+}
