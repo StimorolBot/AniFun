68232ea2cac0061c45d3f4648b7cb0c429b9e698
bot <maximbotpy@gmail.com>
2026-07-12 16:59:53 +0300

    init

---
+++ front/src/ui/icon/Close.jsx
@@ -0,0 +1,19 @@
+export const Close = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="32"
+			height="32"
+			viewBox="0 0 24 24"
+		>
+			<path
+				fill="none"
+				stroke="currentColor"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="1.5"
+				d="m5 19l7-7m0 0l7-7m-7 7L5 5m7 7l7 7"
+			/>
+		</svg>
+	)
+}
