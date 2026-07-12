dcbb800e106616e3b3866e82331bd7e380d5b1df
bot <maximbotpy@gmail.com>
2026-07-12 17:00:06 +0300

    init

---
+++ front/src/ui/icon/Back.jsx
@@ -0,0 +1,19 @@
+export const Back = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="32"
+			height="32"
+			viewBox="0 0 24 24"
+		>
+			<path
+				fill="none"
+				stroke="currentColor"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="2"
+				d="M11 18h3.75a5.25 5.25 0 1 0 0-10.5H5M7.5 4L4 7.5L7.5 11"
+			/>
+		</svg>
+	)
+}
