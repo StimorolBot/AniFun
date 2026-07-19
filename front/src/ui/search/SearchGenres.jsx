3dbb095a254186224fd24f4bcb1d4ca0b4308e3c
bot <maximbotpy@gmail.com>
2026-07-19 21:02:31 +0300

    feat: добавил Controller

--- front/src/ui/search/SearchGenres.jsx
+++ front/src/ui/search/SearchGenres.jsx
@@ -1,3 +1,5 @@
+import { Controller } from "react-hook-form"
+
 import { useQuery } from "@tanstack/react-query"
 
 import { CustomSelect } from "../../ui/input/CustomSelect"
@@ -6,24 +8,38 @@ import { api } from "../../api"
 
 import "./style/search_genres.sass"
 
-export const SearchGenres = ({ genres, setGenres, ...props }) => {
+export const SearchGenres = ({ control, ...props }) => {
 	const { data: genresData } = useQuery({
 		queryKey: ["genres-data-list"],
 		staleTime: 1000 * 60 * 3,
 		queryFn: async () => {
-			return api.get("anime/genres/list").then((r) => r.data)
+			return api.get("anime/genres/").then((r) => r.data)
 		},
 	})
 
 	return (
-		<CustomSelect
-			options={genresData}
-			value={genres}
-			setValue={setGenres}
-			className="genres-select"
-			isMulti={true}
-			paramName={"genres"}
-			{...props}
+		<Controller
+			name="genres"
+			control={control}
+			render={({ field }) => {
+				const handleChange = (selected) => {
+					field.onChange(selected ? selected.map((v) => v.value) : [])
+				}
+
+				const selectedValues = (genresData || []).filter((g) =>
+					field?.value?.includes(g.value),
+				)
+				return (
+					<CustomSelect
+						options={genresData}
+						value={selectedValues}
+						onChange={handleChange}
+						className="genres-select"
+						isMulti={true}
+						{...props}
+					/>
+				)
+			}}
 		/>
 	)
 }
