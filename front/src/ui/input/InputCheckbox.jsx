b6194593f858649ae7174e42bf6a68c97a709ecb
bot <maximbotpy@gmail.com>
2026-07-21 20:18:36 +0300

    refactor: отформатировал

--- front/src/ui/input/InputCheckbox.jsx
+++ front/src/ui/input/InputCheckbox.jsx
@@ -1,13 +1,16 @@
-import { memo } from "react"
-
 import "./style/input_checkbox.sass"
 
-
-export const InputCheckbox = memo(({id, callback, ...props}) => {
-    return(
-        <div className="input-checkbox__container">
-            <input className="input-checkbox" id={id} type="checkbox" onChange={(e) => callback(e)} {...props}/>
-            <label className="input-checkbox__label" htmlFor={id}/>
-        </div>
-    )  
-})
+export const InputCheckbox = ({ id, register, param, ...props }) => {
+	return (
+		<div className="input-checkbox__container">
+			<input
+				className="input-checkbox"
+				id={id}
+				type="checkbox"
+				{...register(param)}
+				{...props}
+			/>
+			<label className="input-checkbox__label" htmlFor={id} />
+		</div>
+	)
+}
