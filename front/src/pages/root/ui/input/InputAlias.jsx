8ecb6c6517bed7dc5cb0dc4c6e38c99a85e78614
bot <maximbotpy@gmail.com>
2026-07-21 20:26:09 +0300

    init

---
+++ front/src/pages/root/ui/input/InputAlias.jsx
@@ -0,0 +1,23 @@
+import { InputDefault } from "../../../../ui/input/InputDefault"
+
+export const InputAlias = ({ id, errorMsg, register, ...props }) => {
+	return (
+		<InputDefault
+			id={id}
+			minLength={5}
+			maxLength={150}
+			placeholder="Alias"
+			{...props}
+			errorMsg={errorMsg}
+			register={register("alias", {
+				defaultValue: null,
+				minLength: { value: 5, message: "Alias от 5 символов" },
+				maxLength: { value: 150, message: "Alias до 150 символов" },
+				pattern: {
+					value: /(^[a-zA-Z0-9][a-zA-Z0-9_-]*$)/,
+					message: "Некорректный alias",
+				},
+			})}
+		/>
+	)
+}
