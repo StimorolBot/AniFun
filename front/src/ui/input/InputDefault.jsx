175211a217f0578ac15d53913f5dd2a539e0a815
bot <maximbotpy@gmail.com>
2026-07-21 20:25:43 +0300

    refactor: удалил  label

--- front/src/ui/input/InputDefault.jsx
+++ front/src/ui/input/InputDefault.jsx
@@ -1,16 +1,10 @@
-import { memo } from "react"
-
 import "./style/input_default.sass"
 
-
-export const InputDefault = memo(({id, register, errorMsg, ...props}) => {
-    return(
-        <div className="input-default__container">
-            <input className="input-default" id={id} {...register} {...props} />
-            <label htmlFor={id}/>
-            <span className="input-default__error">
-                {errorMsg}
-            </span>
-        </div>
-    )
-})
+export const InputDefault = ({ id, register, errorMsg, ...props }) => {
+	return (
+		<div className="input-default__container">
+			<input className="input-default" id={id} {...props} {...register} />
+			<span className="input-default__error">{errorMsg}</span>
+		</div>
+	)
+}
