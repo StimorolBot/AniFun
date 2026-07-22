e1bb66ae43a73aeecaf8e48eab3b600baeed722b
bot <maximbotpy@gmail.com>
2026-07-22 21:53:33 +0300

    init

---
+++ back/src/app/auth/enums/v1/__init__.py
@@ -0,0 +1,9 @@
+from src.app.auth.enums.v1.auth_provider import AuthProvider
+from src.app.auth.enums.v1.role import Role
+from src.app.auth.enums.v1.token_type import TokenType
+
+__all__ = (
+    AuthProvider,
+    Role,
+    TokenType
+)
---
+++ back/src/app/validation/__init__.py
@@ -0,0 +1,6 @@
+from src.app.validation.img import ValidRatioImg
+from src.app.validation.number import ValidNumber
+from src.app.validation.text import ValidAlias, ValidText
+from src.app.validation.uuid import ValidUUID
+
+__all__ = (ValidNumber, ValidUUID, ValidText, ValidAlias, ValidRatioImg)
