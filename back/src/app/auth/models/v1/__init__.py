a06513b768294ca8af4c6dc2fb6e05e9e62dde36
bot <maximbotpy@gmail.com>
2026-07-27 21:13:01 +0300

    feat: добавил AuthProviderTable, RoleTable

---
+++ back/src/app/auth/models/v1/__init__.py
@@ -0,0 +1,11 @@
+from src.app.auth.models.v1.auth import AuthTable
+from src.app.auth.models.v1.auth_provider import AuthProviderTable
+from src.app.auth.models.v1.ban import BanTable
+from src.app.auth.models.v1.role import RoleTable
+
+__all__ = (
+    AuthTable,
+    BanTable,
+    AuthProviderTable,
+    RoleTable
+)
--- back/src/app/auth/models/v1/main/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.app.auth.models.v1.main.auth import AuthTable
-from src.app.auth.models.v1.main.ban import BanTable
-
-__all__ = (AuthTable, BanTable)
