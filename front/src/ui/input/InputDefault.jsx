84287d98d9f5dc190a78298f98d977dad5e0fa9c
bot <maximbotpy@gmail.com>
2026-07-29 20:29:00 +0300

    feat: добавил Loader

--- front/src/ui/input/InputDefault.jsx
+++ front/src/ui/input/InputDefault.jsx
@@ -1,9 +1,28 @@
+import { Loader } from "../../components/loader/Loader"
+
 import "./style/input_default.sass"
 
-export const InputDefault = ({ id, register, errorMsg, ...props }) => {
+export const InputDefault = ({
+	id,
+	register,
+	errorMsg,
+	isLoading,
+	...props
+}) => {
 	return (
 		<div className="input-default__container">
-			<input className="input-default" id={id} {...props} {...register} />
+			<input
+				className="input-default"
+				id={id}
+				disabled={isLoading}
+				{...props}
+				{...register}
+			/>
+			{isLoading && (
+				<div className="input-default__loader">
+					<Loader size="small" />
+				</div>
+			)}
 			<span className="input-default__error">{errorMsg}</span>
 		</div>
 	)
