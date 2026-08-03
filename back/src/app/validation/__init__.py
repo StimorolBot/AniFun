c054370ff3cca9bf1af86844d43e8a752a01846d
bot <maximbotpy@gmail.com>
2026-08-03 20:48:09 +0300

    поправил импорт

--- back/src/app/validation/__init__.py
+++ back/src/app/validation/__init__.py
@@ -1,6 +1,6 @@
 from src.app.validation.img import ValidRatioImg
 from src.app.validation.number import ValidNumber
-from src.app.validation.text import ValidAlias, ValidText, ValidDescription
+from src.app.validation.text import ValidAlias, ValidDescription, ValidText
 from src.app.validation.uuid import ValidUUID
 
 __all__ = (ValidNumber, ValidUUID, ValidText, ValidAlias, ValidRatioImg, ValidDescription)
