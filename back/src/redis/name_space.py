d7d5e81a1665cfe9d1ef09d0ad8285a899198d20
bot <maximbotpy@gmail.com>
2026-08-06 22:36:34 +0300

    дал более понятные имена

--- back/src/redis/name_space.py
+++ back/src/redis/name_space.py
@@ -2,21 +2,23 @@ from enum import Enum, IntEnum
 
 
 class RedisNameSpace(Enum):
-    # home page
-    SLIDER = "slider"
-    NEW_EPISODE = "new_episode"
-    SCHEDULES = "schedules"
-    FRANCHISES = "franchises"
-    GENRES = "genres"
+    HOME_BANNER = "home:banner"
+    HOME_EPISODE = "home:episode"
+    HOME_SCHEDULE = "home:schedule"
+    HOME_FRANCHISE = "home:franchise"
+    HOME_GENRE = "home:genre"
 
-    # title page
-    ABOUT_TITLE = "about_title"
-    RECOMMEND_TITLE = "recommend_title"
-    EPISODE_TITLE = "episode_title"
-    SEQUEL = "sequel"
-    SCHEDULE = "schedule"
+    TITLE = "title"
+    TITLE_EPISODE = "title:episode"
+    TITLE_RECOMMEND = "title:recommend"
+    TITLE_SEQUEL = "title:sequel"
+    TITLE_SCHEDULE = "title:schedule"
 
 
 class Expire(IntEnum):
-    TWO = 120
-    THREE = 180
+    TWO_MINUTES = 120
+    TEN_MINUTES = 600
+    THIRTY_MINUTES = 1800
+    ONE_HOUR = 3600
+    THREE_HOURS = 10800
+    ONE_DAY = 86400
