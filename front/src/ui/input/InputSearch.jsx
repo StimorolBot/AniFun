36dfa2071cf7993367719136af97e4667516fc81
bot <maximbotpy@gmail.com>
2026-08-06 21:14:07 +0300

    feat: добавил возможность отключать валидацию при поиске

--- front/src/ui/input/InputSearch.jsx
+++ front/src/ui/input/InputSearch.jsx
@@ -1,32 +1,50 @@
-import { memo } from "react"
-
 import "./style/input_search.sass"
 
-export const InputSearch = memo(({ id, register, errorMsg, ...props }) => {
+export const InputSearch = ({
+	id,
+	register,
+	errorMsg,
+	isInnerSearch = false,
+	...props
+}) => {
 	return (
 		<div className="input-search__container">
-			<input
-				className="input-search"
-				id={id}
-				type="search"
-				minLength={5}
-				maxLength={150}
-				{...props}
-				{...register("title", {
-					minLength: { value: 5, message: "Название от 5 символов" },
-					maxLength: {
-						value: 150,
-						message: "Название до 150 символов",
-					},
-					pattern: {
-						value: /(^[a-zA-Zа-яёА-ЯЁ0-9][a-zA-Zа-яёА-ЯЁ0-9\s]*$)/,
-						message: "Некорректное название",
-					},
-				})}
-			/>
-			<label className="input-search__label" htmlFor={id}>
-				{errorMsg}
-			</label>
+			{isInnerSearch ? (
+				<input
+					className="input-search"
+					id={id}
+					type="search"
+					{...props}
+				/>
+			) : (
+				<>
+					<input
+						className="input-search"
+						id={id}
+						type="search"
+						minLength={5}
+						maxLength={150}
+						{...props}
+						{...register("title", {
+							minLength: {
+								value: 5,
+								message: "Название от 5 символов",
+							},
+							maxLength: {
+								value: 150,
+								message: "Название до 150 символов",
+							},
+							pattern: {
+								value: /(^[a-zA-Zа-яёА-ЯЁ0-9][a-zA-Zа-яёА-ЯЁ0-9\s]*$)/,
+								message: "Некорректное название",
+							},
+						})}
+					/>
+					<label className="input-search__label" htmlFor={id}>
+						{errorMsg}
+					</label>
+				</>
+			)}
 		</div>
 	)
-})
+}
