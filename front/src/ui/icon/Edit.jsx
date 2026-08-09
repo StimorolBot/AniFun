1ba88326005a07da04e9ce98fc904d545fa9d024
bot <maximbotpy@gmail.com>
2026-08-09 18:05:03 +0300

    прокинул  props

--- front/src/ui/icon/Edit.jsx
+++ front/src/ui/icon/Edit.jsx
@@ -1,10 +1,11 @@
-export const Edit = () => {
+export const Edit = ({ ...props }) => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="32"
 			height="32"
 			viewBox="0 0 24 24"
+			{...props}
 		>
 			<path
 				fill="currentColor"
--- front/src/ui/icon/Remove.jsx
+++ front/src/ui/icon/Remove.jsx
@@ -1,10 +1,11 @@
-export const Remove = () => {
+export const Remove = ({ ...props }) => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="32"
 			height="32"
 			viewBox="0 0 24 24"
+			{...props}
 		>
 			<path
 				fill="currentColor"
