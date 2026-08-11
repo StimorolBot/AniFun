9ff0d7108f3944a628007091581cf78287502bd8
bot <maximbotpy@gmail.com>
2026-08-11 21:10:55 +0300

    refactor: поменял url в запросе, svg не компоненты, button -> BtnDefault

--- front/src/components/popup/Search.jsx
+++ front/src/components/popup/Search.jsx
@@ -1,32 +1,34 @@
 import { memo, useRef } from "react"
+
 import { useForm } from "react-hook-form"
 import { CSSTransition, SwitchTransition } from "react-transition-group"
 
 import { useQuery } from "@tanstack/react-query"
 
+import { Close } from "../../ui/icon/Close"
+import { Search as SearchIcon } from "../../ui/icon/Search"
+
+import { BtnDefault } from "../../ui/btn/BtnDefault"
+
 import { InputSearch } from "../../ui/input/InputSearch"
 
 import { SearchItem } from "./item/search_item/SearchItem"
-
-import { LoaderSkeletonSearch } from "./loader/LoaderSkeletonSearch"
 
 import { useClickOutside } from "../../hook/useClickOutside"
 import { useDebounce } from "../../hook/useDebounce"
 
 import { api } from "../../api"
+import { SkeletonSearch } from "./skeleton/SkeletonSearch"
 
 import "./style/search.sass"
 
-export const Search = memo(({ ref, setIsShow }) => {
+export const Search = memo(({ ref, isShowPopup, setIsShow, storageUrl }) => {
 	const clickRef = useRef()
 	const transitionRef = useRef()
 	const {
 		register,
-		control,
-		formState: { errors, isValid },
+		formState: { isValid },
 		watch,
-		getValues,
-		reset,
 	} = useForm({
 		mode: "onChange",
 		defaultValues: {
@@ -36,47 +38,70 @@ export const Search = memo(({ ref, setIsShow }) => {
 
 	const debounceSearchVal = useDebounce(watch("title"))
 
-	const {
-		data: searchData,
-		isFetching,
-		refetch,
-	} = useQuery({
-		queryKey: [
-			"header-search-title-data",
-			debounceSearchVal?.length > 5 && isValid ? debounceSearchVal : null,
-		],
+	const { data: searchData, isLoading } = useQuery({
+		queryKey: ["search-title-data", debounceSearchVal],
 		staleTime: 1000 * 60 * 3,
 		enabled: !!(debounceSearchVal?.length >= 5 && isValid),
 		queryFn: async () => {
 			return await api
-				.get("/search-title", {
+				.get("/titles/search", {
 					params: { size: 20, page: 1, title: debounceSearchVal },
 				})
 				.then((r) => r.data)
 		},
 	})
-	useClickOutside(clickRef, setIsShow, "", true, true)
+
+	useClickOutside({
+		refs: [clickRef],
+		enabled: isShowPopup,
+		handler: () => {
+			setIsShow(false)
+			document.body.classList.remove("scroll_block")
+		},
+	})
+
+	const items = searchData?.items ?? []
+
+	const searchContent =
+		items.length > 0 ? (
+			items.map((item) => (
+				<SearchItem
+					item={item}
+					storageUrl={storageUrl}
+					key={item.uuid}
+				/>
+			))
+		) : debounceSearchVal?.length >= 5 ? (
+			<li className="search-popup__not-found">
+				<img
+					src={`${storageUrl}/stickers/72b0282a20594165a6511c8fbd8c5dfd.png`}
+					alt="Нет результата"
+				/>
+			</li>
+		) : (
+			<li className="search-popup__svg">
+				<SearchIcon style={{ fill: "currentColor" }} />
+				<p>Введите название релиза</p>
+				<p>Результаты вашего поиска появятся здесь</p>
+			</li>
+		)
 
 	return (
 		<dialog className="search-popup transition" ref={ref}>
 			<div className="search-popup__container" ref={clickRef}>
-				<button className="btn-close-popup">
-					<svg>
-						<use
-							xlinkHref="/main.svg#close-svg"
-							onClick={() => {
-								document.body.classList.remove("scroll_block")
-								setIsShow(false)
-							}}
-						/>
-					</svg>
-				</button>
+				<BtnDefault
+					callback={() => {
+						document.body.classList.remove("scroll_block")
+						setIsShow(false)
+					}}
+				>
+					<Close />
+				</BtnDefault>
 				<search>
-					<form action="/search-title" method="get">
+					<form>
 						<InputSearch
 							id={"search-title-header"}
 							register={register}
-							errorMsg={errors?.title?.message}
 							autoComplete={"off"}
 							placeholder={"Введите название аниме"}
 						/>
@@ -85,50 +110,18 @@ export const Search = memo(({ ref, setIsShow }) => {
 				<SwitchTransition mode="out-in">
 					<CSSTransition
 						classNames="transition"
-						key={isFetching}
+						key={isLoading}
 						nodeRef={transitionRef}
 						timeout={300}
 					>
-						{isFetching ? (
-							<LoaderSkeletonSearch count={4} />
+						{isLoading ? (
+							<SkeletonSearch count={3} />
 						) : (
-							<ul className="search-popup__list transition">
-								{searchData?.items ||
-								(debounceSearchVal !== null &&
-									debounceSearchVal?.length !== 0) ? (
-									searchData?.items.length > 0 ? (
-										searchData?.items?.map(
-											(item, index) => {
-												return (
-													<SearchItem
-														item={item}
-														key={index}
-													/>
-												)
-											},
-										)
-									) : (
-										<li className="search-popup__not-found transition">
-											<svg>
-												<use xlinkHref="/public/svg/header.svg#not-find-svg" />
-											</svg>
-											<p>
-												{`Не удалось найти тайтл с именем: ${debounceSearchVal}`}
-											</p>
-										</li>
-									)
-								) : (
-									<li className="search-popup__svg">
-										<svg>
-											<use xlinkHref="/public/svg/header.svg#search-doc-svg" />
-										</svg>
-										<p>Введите название релиза</p>
-										<p>
-											Результаты вашего поиска появятся
-											здесь
-										</p>
-									</li>
-								)}
+							<ul
+								className="search-popup__list transition"
+								ref={transitionRef}
+							>
+								{searchContent}
 							</ul>
 						)}
 					</CSSTransition>
