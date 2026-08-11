faeda58abd85b730638a58069b848b878b27c7cf
bot <maximbotpy@gmail.com>
2026-08-11 21:07:12 +0300

    refactor: заменил useQuery на ссылку, поправил шрифт для тайтла

--- front/src/components/popup/item/search_item/SearchItem.jsx
+++ front/src/components/popup/item/search_item/SearchItem.jsx
@@ -1,43 +1,47 @@
 import { memo } from "react"
+
 import { Link } from "react-router-dom"
 
-import { useQuery } from "@tanstack/react-query"
-
-import { api } from "../../../../api"
+import { useAutoFontSize } from "../../../../hook/useAutoFontSize"
 
 import "./style.sass"
 
-export const SearchItem = memo(({ item, ...props }) => {
-	const { data: imgData } = useQuery({
-		queryKey: ["header-search-title-img"],
-		staleTime: 1000 * 60 * 3,
-		retry: false,
-		enabled: !!item?.poster?.poster_uuid,
-		queryFn: async () => {
-			return await api
-				.get(`/s3/anime-${item.uuid}/${item.poster.poster_uuid}`)
-				.then((r) => r.data)
-		},
+export const SearchItem = memo(({ item, storageUrl, ...props }) => {
+	const titleRef = useAutoFontSize({
+		minSize: 12,
+		maxSize: 16,
+		deps: [item.title],
 	})
-
+	const subTitleRef = useAutoFontSize({
+		minSize: 12,
+		maxSize: 14,
+		deps: [item.sut_title],
+	})
 	return (
-		<li className="search-popup__item" {...props}>
-			<div className="search-popup__img-inner">
-				<img className="search-popup__img" src={imgData} alt="poster" />
-			</div>
-			<div className="search-popup__content">
-				<p className="search-popup__title">{item.title}</p>
-				<ul className="search-popup__bottom">
-					<li>{item.year}</li>
-					<li>{item.type?.label}</li>
-				</ul>
-				<Link
-					className="search-popup__link"
-					to={`/anime/${item?.alias}`}
-				>
-					Смотреть
-				</Link>
-			</div>
+		<li {...props}>
+			<Link className="search-popup__link" to={`/anime/${item?.alias}`}>
+				<div className="search-popup__img-inner">
+					<img
+						src={`${storageUrl}/anime-${item.uuid}/${item.poster_uuid}.webp`}
+						alt="постер"
+						onError={(e) => {
+							const img = e.currentTarget
+							img.onerror = null
+							img.src = `${storageUrl}/stickers/6093866d92af49f68292ba298b383eea.png`
+						}}
+						loading="lazy"
+					/>
+				</div>
+				<div className="search-popup__content">
+					<h3 ref={titleRef}>{item.title}</h3>
+					<h4 ref={subTitleRef}>{item?.sub_title}</h4>
+					<ul className="search-popup__bottom">
+						<li className="point">{item.type}</li>
+						<li className="point">{item.season}</li>
+						<li className="point">{item.year}</li>
+					</ul>
+				</div>
+			</Link>
 		</li>
 	)
 })
