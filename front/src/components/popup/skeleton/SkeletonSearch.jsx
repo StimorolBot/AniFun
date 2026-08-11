dcbd2d9bdcc72b5f734712d8e757f92c7b12fae3
bot <maximbotpy@gmail.com>
2026-08-11 21:03:48 +0300

    refactor: переименовал файл, удалил файл со стилями

---
+++ front/src/components/popup/skeleton/SkeletonSearch.jsx
@@ -0,0 +1,64 @@
+import Skeleton from "react-loading-skeleton"
+
+export const SkeletonSearch = ({ count }) => {
+	return (
+		<ul className="search-popup__list transition">
+			{Array(count)
+				.fill(0)
+				.map((_, index) => {
+					return (
+						<li className="search-popup_skeleton" key={index}>
+							<div
+								style={{
+									display: "flex",
+									justifyContent: "space-between",
+									height: 90,
+									marginTop: 20,
+								}}
+							>
+								<div
+									style={{
+										width: 65,
+										height: 80,
+									}}
+								>
+									<Skeleton
+										style={{
+											width: "100%",
+											height: "100%",
+										}}
+									/>
+								</div>
+								<div
+									className="search-popup__content"
+									style={{
+										justifyContent: "center",
+										gap: 5,
+									}}
+								>
+									<Skeleton
+										style={{
+											width: 140,
+											height: 20,
+										}}
+									/>
+									<Skeleton
+										style={{
+											width: 100,
+											height: 20,
+										}}
+									/>
+									<Skeleton
+										style={{
+											width: 150,
+											height: 20,
+										}}
+									/>
+								</div>
+							</div>
+						</li>
+					)
+				})}
+		</ul>
+	)
+}
