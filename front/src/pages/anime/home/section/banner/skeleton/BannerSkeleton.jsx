16178430c07748aac82ca2162aa65d06e0a21920
bot <maximbotpy@gmail.com>
2026-08-12 19:44:21 +0300

    refactor: переименовал файл, убрал лишние стили

---
+++ front/src/pages/anime/home/section/banner/skeleton/BannerSkeleton.jsx
@@ -0,0 +1,46 @@
+import Skeleton from "react-loading-skeleton"
+
+export const BannerSkeleton = () => {
+	return (
+		<div style={{ height: 380 }}>
+			<div className="banner">
+				<Skeleton
+					className="skeleton__background"
+					style={{
+						width: "100%",
+						height: "100%",
+						borderRadius: 20,
+					}}
+				/>
+				<div className="slide__inner">
+					<Skeleton style={{ width: 250, height: 30 }} />
+					<ul className="slide-desc__list" style={{ gap: 20 }}>
+						<Skeleton style={{ width: 70, height: 25 }} />
+						<Skeleton style={{ width: 70, height: 25 }} />
+						<Skeleton style={{ width: 70, height: 25 }} />
+					</ul>
+					<ul className="slide-desc__list" style={{ gap: 20 }}>
+						<Skeleton style={{ width: 80, height: 25 }} />
+						<Skeleton style={{ width: 80, height: 25 }} />
+						<Skeleton style={{ width: 80, height: 25 }} />
+					</ul>
+					<Skeleton
+						style={{ width: "100%", height: 90, margin: "10px 0" }}
+					/>
+					<Skeleton
+						style={{
+							position: "absolute",
+							bottom: -30,
+							width: 120,
+							height: 40,
+						}}
+					/>
+				</div>
+				<div className="banner__btn">
+					<Skeleton style={{ width: 30, height: 30 }} />
+					<Skeleton style={{ width: 30, height: 30 }} />
+				</div>
+			</div>
+		</div>
+	)
+}
