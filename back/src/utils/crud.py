9cd46aef7558cc9eaff45e2dd00117e79a58c3ec
bot <maximbotpy@gmail.com>
2026-08-15 21:47:43 +0300

    refactor: поправил аннотации

--- back/src/utils/crud.py
+++ back/src/utils/crud.py
@@ -1,31 +1,35 @@
+from typing import TypeVar
+
 from sqlalchemy import delete, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm.decl_api import DeclarativeAttributeIntercept
+from sqlalchemy.orm import DeclarativeBase
 
 from src.utils.abs_model.crud import CrudAbs
+
+T = TypeVar("T", bound=DeclarativeBase)
 
 
 class Crud(CrudAbs):
     @staticmethod
-    async def create(session: AsyncSession, table: DeclarativeAttributeIntercept, data: dict):
+    async def create(session: AsyncSession, table: type[T], data: dict):
         stmt = table(**data)
         session.add(stmt)
         return stmt
 
     @staticmethod
-    async def read(session: AsyncSession, table: DeclarativeAttributeIntercept, **kwargs) -> list:
+    async def read(session: AsyncSession, table: type[T], **kwargs) -> T | None:
         stmt = select(table).filter_by(**kwargs)
         result = await session.execute(stmt)
         return result.unique().scalar_one_or_none()
 
     @staticmethod
-    async def update(session: AsyncSession, table: DeclarativeAttributeIntercept, data: dict, **kwargs):
+    async def update(session: AsyncSession, table: type[T], data: dict, **kwargs):
         query = update(table).filter_by(**kwargs).values(**data)
         result = await session.execute(query)
         return result
 
     @staticmethod
-    async def delite(session: AsyncSession, table: DeclarativeAttributeIntercept, **kwargs):
+    async def delite(session: AsyncSession, table: type[T], **kwargs):
         stmt = delete(table).filter_by(**kwargs)
         await session.execute(stmt)
 
