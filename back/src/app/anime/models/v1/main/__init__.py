44feee0f091b4533162378dcb25f84a98418fc26
bot <maximbotpy@gmail.com>
2026-08-16 17:16:51 +0300

    refactor: переименовал таблицу SequelTable

--- back/src/app/anime/models/v1/main/__init__.py
+++ back/src/app/anime/models/v1/main/__init__.py
@@ -1,4 +1,5 @@
 from src.app.anime.models.v1.main.anime import AnimeTable
+from src.app.anime.models.v1.main.anime_relation import AnimeRelationTable
 from src.app.anime.models.v1.main.comment import (CommentTable,
                                                   ReactionCommentTable)
 from src.app.anime.models.v1.main.episode import (EpisodePreviewTable,
@@ -11,14 +12,13 @@ from src.app.anime.models.v1.main.img import BannerTable, PosterTable
 from src.app.anime.models.v1.main.rating import RatingTable
 from src.app.anime.models.v1.main.release_day import ReleaseDayTable
 from src.app.anime.models.v1.main.schedule import ScheduleTable
-from src.app.anime.models.v1.main.sequel_anime import SequelTable
 
 __all__ = (
     AnimeTable, PosterTable,
     EpisodeTable, GenresTable,
     EpisodePreviewTable, EpisodeWatchProgress,
     EpisodeVideoTable, EpisodeTimeCodeTable,
-    ScheduleTable, SequelTable,
+    ScheduleTable, AnimeRelationTable,
     RatingTable, CommentTable,
     ReactionCommentTable,
     ReleaseDayTable, BannerTable
