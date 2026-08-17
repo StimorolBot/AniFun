5b85df63a81113e2e827c9f701066b5ab2ee3891
bot <maximbotpy@gmail.com>
2026-08-17 18:51:51 +0300

    feat: подключил стили

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client"
 
 import { App } from "./App"
 
+import "./style/reset.sass"
+
+import "./style/main.sass"
+
 createRoot(document.getElementById("root")).render(<App />)
