9e8026e43620081fad5926cf4a2dab04ac8b998c
bot <maximbotpy@gmail.com>
2026-08-22 18:56:51 +0300

    init

---
+++ front/src/ui/icon/genres/genres.js
@@ -0,0 +1,65 @@
+import { Action } from "./Action"
+import { Adventures } from "./Adventures"
+import { Cars } from "./Cars"
+import { Children } from "./Children"
+import { Comedy } from "./Comedy"
+import { Demons } from "./Demons"
+import { Detective } from "./Detective"
+import { Drama } from "./Drama"
+import { Etty } from "./Etty"
+import { EverydayLife } from "./EverydayLife"
+import { Fantasy } from "./Fantasy"
+import { Games } from "./Games"
+import { Harem } from "./Harem"
+import { Historical } from "./Historical"
+import { Horrors } from "./Horrors"
+import { Isekai } from "./Isekai"
+import { Madness } from "./Madness"
+import { Magic } from "./Magic"
+import { MartialArts } from "./MartialArts"
+import { Mexa } from "./Mexa"
+import { Military } from "./Military"
+import { Music } from "./Music"
+import { Parody } from "./Parody"
+import { Psychological } from "./Psychological"
+import { Romance } from "./Romance"
+import { School } from "./School"
+import { Sport } from "./Sport"
+import { SuperPower } from "./SuperPower"
+import { Supernatural } from "./Supernatural"
+import { Thriller } from "./Thriller"
+import { Vampires } from "./Vampires"
+
+export const genres = {
+	action: Action,
+	adventures: Adventures,
+	cars: Cars,
+	children: Children,
+	comedy: Comedy,
+	demons: Demons,
+	detective: Detective,
+	drama: Drama,
+	etty: Etty,
+	"everyday-life": EverydayLife,
+	fantasy: Fantasy,
+	games: Games,
+	harem: Harem,
+	historical: Historical,
+	horrors: Horrors,
+	isekai: Isekai,
+	madness: Madness,
+	magic: Magic,
+	"martial-arts": MartialArts,
+	mexa: Mexa,
+	military: Military,
+	music: Music,
+	parody: Parody,
+	psychological: Psychological,
+	romance: Romance,
+	school: School,
+	sport: Sport,
+	"super-power": SuperPower,
+	supernatural: Supernatural,
+	thriller: Thriller,
+	vampires: Vampires,
+}
