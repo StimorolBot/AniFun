6b78a57d882fd08874c8042cca6471acf312e53c
bot <maximbotpy@gmail.com>
2026-08-22 18:44:49 +0300

    init

---
+++ front/src/ui/icon/genres/Military.jsx
@@ -0,0 +1,16 @@
+export const Military = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="1.25"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		>
+			<path stroke="none" d="M0 0h24v24H0z" />
+			<path d="M2 15a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3 3 3 0 0 1-3 3H5a3 3 0 0 1-3-3m4-3 1-5h5l3 5m6-3h-7.8" />
+		</svg>
+	)
+}
