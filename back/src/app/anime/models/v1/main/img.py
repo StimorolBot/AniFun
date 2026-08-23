47aba95f445819312f216cbba6e9a6371376bd18
bot <maximbotpy@gmail.com>
2026-08-23 20:15:21 +0300

    добавил для баннера position, date_add и is_active

--- back/src/app/anime/models/v1/main/img.py
+++ back/src/app/anime/models/v1/main/img.py
@@ -1,10 +1,12 @@
+from datetime import datetime
 from typing import TYPE_CHECKING
 from uuid import UUID
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, false, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.model import Base
+from src.utils.utils import get_unc_now
 
 if TYPE_CHECKING:
     from src.app.anime.models.v1.main.anime import AnimeTable
@@ -32,4 +34,8 @@ class BannerTable(Base):
         index=True
     )
     banner_uuid: Mapped[UUID] = mapped_column()
+    position: Mapped[int | None] = mapped_column()
+    date_add: Mapped[datetime] = mapped_column(default=get_unc_now, server_default=func.now())
+    is_active: Mapped[bool] = mapped_column(default=False, server_default=false())
+
     anime_rs: Mapped["AnimeTable"] = relationship(back_populates="banner_rs")
