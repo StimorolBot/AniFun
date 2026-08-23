7fcacaeeed8e5937e507c462c6325a6bce1c988b
bot <maximbotpy@gmail.com>
2026-08-23 18:17:28 +0300

    обновил дизайн

--- front/src/pages/anime/home/Home.jsx
+++ front/src/pages/anime/home/Home.jsx
@@ -1,13 +1,14 @@
 import { memo } from "react"
+
 import { Helmet } from "react-helmet"
 
+import { Banner } from "./section/banner/Banner"
 import { Franchises } from "./section/franchises/Franchises"
 import { Genres } from "./section/genres/Genres"
 import { NewEpisodes } from "./section/new_episodes/NewEpisodes"
 import { Schedule } from "./section/schedule/Schedule"
-import { SwiperCustom } from "./section/swiper/SwiperCustom"
 
-import "./adaptive.sass"
+import "./style.sass"
 
 export const Home = memo(() => {
 	const storageUrl = import.meta.env.VITE_STORAGE_URL
@@ -16,12 +17,21 @@ export const Home = memo(() => {
 			<Helmet>
 				<title>AniFun</title>
 			</Helmet>
-			<h1 className="title-page">Главная страница</h1>
-			<SwiperCustom storageUrl={storageUrl} />
-			<NewEpisodes storageUrl={storageUrl} />
-			<Schedule storageUrl={storageUrl} />
-			<Franchises storageUrl={storageUrl} />
-			<Genres storageUrl={storageUrl} />
+			<Banner storageUrl={storageUrl} />
+			<main className="home">
+				<div className="container">
+					<div className="home__grid">
+						<div className="home__main">
+							<NewEpisodes storageUrl={storageUrl} />
+							<Franchises storageUrl={storageUrl} />
+						</div>
+						<aside className="home__sidebar">
+							<Schedule storageUrl={storageUrl} />
+						</aside>
+					</div>
+					<Genres />
+				</div>
+			</main>
 		</>
 	)
 })
