9068c3e0f88a2c50e67ce0b30f53007f82f6b8d4
bot <maximbotpy@gmail.com>
2026-08-27 21:59:19 +0300

    refactor: заменил button на NavLink

--- front/src/pages/anime/title/nav/TitleNav.jsx
+++ front/src/pages/anime/title/nav/TitleNav.jsx
@@ -1,78 +1,73 @@
-import { memo } from "react"
+import { NavLink } from "react-router-dom"
+
+import { Msg } from "../../../../ui/icon/Msg"
+import { Relations } from "../../../../ui/icon/Relations"
+import { Video } from "../../../../ui/icon/Video"
+import { Watch } from "../../../../ui/icon/Watch"
 
 import "./style.sass"
 
-
-export const TitleNav = memo(({setCurrentSlide, currentSlide}) => {
-
-    return(
-        <nav className="title-nav">
-            <ul className="title-nav__list">
-                <li>
-                    <button 
-                        className="title-nav__btn" 
-                        onClick={(e) => {
-                            setCurrentSlide({
-                                "offset": e.target.offsetLeft, 
-                                "width": e.target.offsetWidth, 
-                                "marginLeft": "0%",
-                                "request": "episode",
-                            })}
-                        }
-                    >
-                        Эпизоды
-                    </button>
-                </li>
-                <li>
-                    <button 
-                        className="title-nav__btn"
-                        onClick={(e) => {
-                            setCurrentSlide({
-                                "offset": e.target.offsetLeft,
-                                "width": e.target.offsetWidth, 
-                                "marginLeft": "-100%",
-                                "request": "continuation",
-                            })}
-                        }
-                    >
-                        Связанное
-                    </button>
-                </li>
-                <li>
-                    <button
-                        className="title-nav__btn"
-                        onClick={(e) => {
-                            setCurrentSlide({
-                                "offset": e.target.offsetLeft, 
-                                "width": e.target.offsetWidth, 
-                                "marginLeft": "-200%",
-                                "request": "comments",
-                            })}
-                        }
-                    >
-                        Комментарии
-                    </button>
-                </li>
-                <li>
-                    <button 
-                        className="title-nav__btn"
-                        onClick={(e) => {
-                            setCurrentSlide({
-                                "offset": e.target.offsetLeft,
-                                "width": e.target.offsetWidth,
-                                "marginLeft": "-300%",
-                                "request": "schedule",
-                            })}
-                        }
-                    >
-                        Расписание
-                    </button>
-                </li>
-                <span 
-                    className="title-active-btn"
-                    style={{"width": `${currentSlide.width}px`, "transform": `translateX(${currentSlide.offset}px)`}}
-                />
-            </ul>
-        </nav>
-    )
-})
+export const TitleNav = () => {
+	return (
+		<nav>
+			<ul className="title-nav__list">
+				<li>
+					<NavLink
+						className={({ isActive }) =>
+							isActive
+								? "title-nav-link title-nav-link_active"
+								: "title-nav-link"
+						}
+						to={"episodes"}
+						end
+					>
+						<Video />
+						Эпизоды
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						className={({ isActive }) =>
+							isActive
+								? "title-nav-link title-nav-link_active"
+								: "title-nav-link"
+						}
+						to={"franchises"}
+						end
+					>
+						<Relations />
+						Порядок просмотра
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						className={({ isActive }) =>
+							isActive
+								? "title-nav-link title-nav-link_active"
+								: "title-nav-link"
+						}
+						to={"comments"}
+						end
+					>
+						<Msg />
+						Комментарии
+					</NavLink>
+				</li>
+				<li>
+					<NavLink
+						className={({ isActive }) =>
+							isActive
+								? "title-nav-link title-nav-link_active"
+								: "title-nav-link"
+						}
+						to={"schedules"}
+						end
+					>
+						<Watch />
+						Расписание
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	)
+}
