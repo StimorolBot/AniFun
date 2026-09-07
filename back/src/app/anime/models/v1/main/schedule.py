5eaa836de11a2c22c69c7c0177c012c95d3eb379
bot <maximbotpy@gmail.com>
2026-09-07 22:10:46 +0300

    удалил day_week, добавил date_release, time_release, is_released

--- back/src/app/anime/models/v1/main/schedule.py
+++ back/src/app/anime/models/v1/main/schedule.py
@@ -1,6 +1,5 @@
-from typing import TYPE_CHECKING, List
-
 import datetime
+from typing import TYPE_CHECKING
 from uuid import UUID
 
 from sqlalchemy import ForeignKey, UniqueConstraint
@@ -9,22 +8,27 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.model import Base
 
 if TYPE_CHECKING:
-    from src.app.anime.models.v1.main.episode import EpisodeTable
+    from src.app.anime.models.v1.main.anime import AnimeTable
 
+
+# pylint: disable=too-few-public-methods
 class ScheduleTable(Base):
-    """Таблица для установки расписания выхода новых эпизодов"""
+    """Таблица с расписанием выхода эпизодов"""
 
     __tablename__ = "schedule_table"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, unique=True)
-    title: Mapped[str] = mapped_column(ForeignKey("anime_table.title"), index=True)
+    title_uuid: Mapped[UUID] = mapped_column(
+        ForeignKey("anime_table.uuid", onupdate="CASCADE", ondelete="CASCADE"),
+        index=True
+    )
+
+    date_release: Mapped[datetime.date] = mapped_column()
+    time_release: Mapped[datetime.time] = mapped_column()
     episode_number: Mapped[int] = mapped_column()
     episode_name: Mapped[str] = mapped_column()
-    day_week: Mapped[str] = mapped_column()
-    date: Mapped[datetime.date] = mapped_column()
+    is_released: Mapped[bool] = mapped_column(default=False)
 
-    uuid_episode: Mapped[UUID| None] = mapped_column(ForeignKey("episode_table.uuid"))
-    uuid_episode_rs: Mapped[List["EpisodeTable"]] = relationship(back_populates="schedule_rs")
+    anime_rs: Mapped["AnimeTable"] = relationship(back_populates="schedule_rs")
 
-    __table_args__ = (UniqueConstraint("title", "episode_number", "day_week", "episode_name"),)
-
+    __table_args__ = (UniqueConstraint("title_uuid", "episode_number", "episode_name"),)
